cc8bd798485cb3dc3b65eaacc4f7a660b3fa0274
jebw <jeb@jdwilkins.co.uk>
2020-04-21 16:41:02 +0100

    Added default route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+  def home
+    render template: "pages/home"
+  end
+
   def show
     render template: "pages/#{params[:page]}"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   get "/pages/:page", to: "pages#show"
 
   get "/404", to: "errors#not_found", via: :all
