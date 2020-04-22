791a2b748d6a03f941f1c6413694d5fb3ea3f7de
jebw <jeb@jdwilkins.co.uk>
2020-04-22 13:23:01 +0100

    Removed govuk-frontend

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,10 +1,6 @@
-require.context('govuk-frontend/govuk/assets');
-
 import '../styles/application.scss';
 import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
-import { initAll } from 'govuk-frontend';
 
 Rails.start();
 Turbolinks.start();
-initAll();
