cacdffa3383b63d35bc851b1503310f0d15bd215
jebw <jeb@jdwilkins.co.uk>
2020-05-05 11:23:17 +0100

    Fixed failing test for homepage

--- spec/features/view_pages_spec.rb
+++ spec/features/view_pages_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature "View pages", type: :feature do
   scenario "Navigate to home" do
-    visit "/pages/home"
+    visit root_path
 
     expect(page).to have_text("Lorem")
   end
