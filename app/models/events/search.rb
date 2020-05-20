92c76a611db35a3a72c4c8ea1135bf82e3421b4f
jebw <jeb@jdwilkins.co.uk>
2020-05-20 16:28:14 +0100

    Added Search model

---
+++ app/models/events/search.rb
@@ -0,0 +1,23 @@
+module Events
+  class Search
+    include ActiveModel::Model
+    include ActiveModel::Attributes
+    include ActiveModel::Validations::Callbacks
+
+    DISTANCES = [30, 50, 100].freeze
+    MONTH_FORMAT = %r{\A20[234]\d-(0[1-9]|1[012])\z}.freeze
+    EVENT_TYPES = [0, 1].freeze
+
+    attribute :event_type, :integer
+    attribute :distance, :integer
+    attribute :location, :string
+    attribute :month, :string
+
+    validates :event_type, presence: true, inclusion: { in: EVENT_TYPES }
+    validates :distance, inclusion: { in: DISTANCES }, allow_nil: true
+    validates :location, presence: true, postcode: true, if: :distance
+    validates :month, presence: true, format: { with: MONTH_FORMAT }
+
+    before_validation(unless: :distance) { self.location = nil }
+  end
+end
---
+++ spec/models/events/search_spec.rb
@@ -0,0 +1,72 @@
+require "rails_helper"
+
+describe Events::Search do
+  let(:event_types) do
+    {
+      0 => "first",
+      1 => "second",
+    }
+  end
+
+  before do
+   # allow(GetIntoTeachingApi::Client).to receive(:event_types) { event_types }
+  end
+
+  context "attributes" do
+    it { is_expected.to respond_to :event_type }
+    it { is_expected.to respond_to :distance }
+    it { is_expected.to respond_to :location }
+    it { is_expected.to respond_to :month }
+  end
+
+  context "validation" do
+    context "for event type" do
+      it { is_expected.to allow_value(0).for :event_type }
+      it { is_expected.to allow_value("1").for :event_type }
+      it { is_expected.not_to allow_value("2").for :event_type }
+      it { is_expected.not_to allow_value("").for :event_type }
+    end
+
+    context "for distance" do
+      described_class::DISTANCES.each do |distance|
+        it { is_expected.to allow_value(distance.to_s).for :distance }
+      end
+
+      it { is_expected.to allow_value("").for :distance }
+      it { is_expected.not_to allow_value("foo").for :distance }
+      it { is_expected.not_to allow_value(1000).for :distance }
+    end
+
+    context "for location" do
+      context "with blank distance" do
+        subject { described_class.new distance: nil }
+
+        it { is_expected.to allow_value(nil).for :location }
+        it { is_expected.to allow_value("").for :location }
+        it { is_expected.to allow_value("random").for :location }
+      end
+
+      context "with assigned distance" do
+        subject do
+          described_class.new distance: described_class::DISTANCES.first
+        end
+
+        it { is_expected.not_to allow_value("").for :location }
+        it { is_expected.not_to allow_value(nil).for :location }
+        it { is_expected.not_to allow_value("random").for :location }
+        it { is_expected.to allow_value("MA1 2WD").for :location }
+      end
+    end
+
+    context "for month" do
+      it { is_expected.to allow_value("2020-01").for :month }
+      it { is_expected.to allow_value("2020-12").for :month }
+      it { is_expected.not_to allow_value("2020-00").for :month }
+      it { is_expected.not_to allow_value("2020-13").for :month }
+      it { is_expected.not_to allow_value("1900-01").for :month }
+      it { is_expected.not_to allow_value("foo").for :month }
+      it { is_expected.not_to allow_value("").for :month }
+      it { is_expected.not_to allow_value(nil).for :month }
+    end
+  end
+end
