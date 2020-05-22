d4bfaf0b0d39bafb9c29c714044481ab80a9dfef
maxmills <8balldigitalsolutions@gmail.com>
2020-05-22 15:55:29 +0100

    google analytics added

---
+++ app/webpacker/packs/google_analytics.js
@@ -0,0 +1,7 @@
+document.addEventListener("turbolinks:load", function (event) {
+  if (typeof gtag === "function") {
+    gtag("config", "UA-159214613-2", {
+      page_path: event.data.url,
+    });
+  }
+});
