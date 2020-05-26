fb4db39cb50e79b7826b6d4fe107ce8babea9955
jebw <jeb@jdwilkins.co.uk>
2020-05-26 13:45:48 +0100

    Fix invalid linting on some ruby code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,12 @@ class PagesController < ApplicationController
   def home
     render template: "pages/home"
   end
+
   def events
     render template: "pages/events"
   end
-  
+
   def show
-    render template: "content/#{params[:page]}", layout: 'layouts/content'
+    render template: "content/#{params[:page]}", layout: "layouts/content"
   end
 end
