846ecfb205331eb1e5118e79e963edb5a2c8de92
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2020-05-27 16:44:12 +0100

    add an event handler and a reg step 1 handler

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,18 @@ class PagesController < ApplicationController
   def home
     render template: "pages/home"
   end
+  
+  def event
+    render template: "pages/events/event"
+  end
 
   def events
-    render template: "pages/events"
+    render template: "pages/events/events"
   end
+
+  def eventregistration
+    render template: "pages/events/registration/step1"
+  end 
 
   def show
     render template: "content/#{params[:page]}", layout: "layouts/content"
