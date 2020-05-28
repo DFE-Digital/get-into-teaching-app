ab8ffa1f25405ab5a1c28f7c317b5889a66f213b
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2020-05-28 11:49:00 +0100

    stop rubocop from crying

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/404", to: "errors#not_found", via: :all
   get "/422", to: "errors#unprocessable_entity", via: :all
   get "/500", to: "errors#internal_server_error", via: :all
-  get "/events", to: "pages#events", via: :all  
+  get "/events", to: "pages#events", via: :all
   get "/eventschool", to: "pages#eventschool", via: :all
   get "/event", to: "pages#event", via: :all
   get "/event/register/1", to: "pages#eventregistration", via: :all
