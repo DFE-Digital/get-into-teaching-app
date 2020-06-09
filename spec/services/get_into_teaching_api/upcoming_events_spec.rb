e4395ffc3d19b6e464c5e46e55fc501fa8d2d0a5
jebw <jeb@jdwilkins.co.uk>
2020-06-09 11:34:50 +0100

    Refactored tests to avoid duplication

--- spec/services/get_into_teaching_api/upcoming_events_spec.rb
+++ spec/services/get_into_teaching_api/upcoming_events_spec.rb
@@ -9,33 +9,12 @@ describe GetIntoTeachingApi::UpcomingEvents do
   let(:testdata) { build_list :event_api, 1 }
 
   describe "#events" do
-    it { is_expected.to be_kind_of Array }
-    it { is_expected.to have_attributes length: 1 }
-    it { is_expected.to all respond_to :eventId }
-    it { is_expected.to all respond_to :eventName }
+    it_behaves_like "array of event entities", 1
 
-    context "event details" do
-      let(:startdate) { Date.parse testdata[0]["startDate"] }
-      subject { client.call.first }
-      it { is_expected.to be_kind_of GetIntoTeachingApi::Types::Event }
-      it { is_expected.to have_attributes eventId: testdata[0]["eventId"] }
-      it { is_expected.to have_attributes eventName: testdata[0]["eventName"] }
-      it { is_expected.to have_attributes startDate: startdate }
-      it { is_expected.to have_attributes endDate: startdate }
-    end
-
-    context "event building" do
-      subject { client.call.first.building }
-      it { is_expected.to be_kind_of GetIntoTeachingApi::Types::EventBuilding }
-      it { is_expected.to have_attributes id: testdata[0]["building"]["id"] }
-      it { is_expected.to have_attributes addressComposite: "Line 1, Line 2" }
-    end
-
-    context "event room" do
-      subject { client.call.first.room }
-      it { is_expected.to be_kind_of GetIntoTeachingApi::Types::EventRoom }
-      it { is_expected.to have_attributes id: testdata[0]["room"]["id"] }
-      it { is_expected.to have_attributes description: "Lecture Hall 1 description" }
+    describe "first entity" do
+      let(:eventdata) { testdata[0] }
+      let(:event) { client.call.first }
+      it_behaves_like "event entity"
     end
   end
 end
--- spec/support/api_support.rb
+++ spec/support/api_support.rb
@@ -22,3 +22,47 @@ shared_examples "api support" do
       body: testdata.to_json
   end
 end
+
+shared_examples "event details" do
+  describe "top level event details" do
+    let(:startdate) { Date.parse eventdata["startDate"] }
+    subject { event }
+    it { is_expected.to be_kind_of GetIntoTeachingApi::Types::Event }
+    it { is_expected.to have_attributes eventId: eventdata["eventId"] }
+    it { is_expected.to have_attributes eventName: eventdata["eventName"] }
+    it { is_expected.to have_attributes startDate: startdate }
+    it { is_expected.to have_attributes endDate: startdate }
+  end
+end
+
+shared_examples "event building" do
+  describe "event building fields" do
+    subject { event.building }
+    it { is_expected.to be_kind_of GetIntoTeachingApi::Types::EventBuilding }
+    it { is_expected.to have_attributes id: eventdata["building"]["id"] }
+    it { is_expected.to have_attributes addressComposite: "Line 1, Line 2" }
+  end
+end
+
+shared_examples "event room" do
+  describe "event room fields" do
+    subject { event.room }
+    it { is_expected.to be_kind_of GetIntoTeachingApi::Types::EventRoom }
+    it { is_expected.to have_attributes id: eventdata["room"]["id"] }
+    it { is_expected.to have_attributes description: "Lecture Hall 1 description" }
+  end
+end
+
+shared_examples "event entity" do
+  it_behaves_like "event details"
+  it_behaves_like "event building"
+  it_behaves_like "event room"
+end
+
+shared_examples "array of event entities" do |length|
+  subject { client.call }
+  it { is_expected.to be_kind_of Array }
+  it { is_expected.to have_attributes length: length }
+  it { is_expected.to all respond_to :eventId }
+  it { is_expected.to all respond_to :eventName }
+end
