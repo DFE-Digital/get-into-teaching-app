c8adc28ea3e93c9f67baa42c977b9eb13aac41e5
jebw <jeb@jdwilkins.co.uk>
2020-07-01 16:06:14 +0100

    Added feature spec for Mailing List wizard

---
+++ spec/features/mailing_list_wizard_spec.rb
@@ -0,0 +1,81 @@
+require "rails_helper"
+
+RSpec.feature "View pages", type: :feature do
+  scenario "Full journey as Student" do
+    visit mailing_list_steps_path
+
+    expect(page).to have_text "Sign up for personalised updates"
+    fill_in "First name", with: "Test"
+    fill_in "Last name", with: "User"
+    fill_in "Email address", with: "test@testuser.com"
+    select "Student"
+    click_on "Next Step"
+
+    expect(page).to have_text "What stage are you at with your degree"
+    select "Graduate or postgraduate"
+    click_on "Next Step"
+
+    expect(page).to have_text "How close are you to applying"
+    select "I'm not sure and finding out more"
+    click_on "Next Step"
+
+    expect(page).to have_text "Which subject do you want to teach"
+    select "Maths"
+    click_on "Next Step"
+
+    expect(page).to have_text "Your postcode"
+    fill_in "Your postcode", with: "TE57 1NG"
+    click_on "Next Step"
+
+    expect(page).to have_text "Your phone number"
+    fill_in "Your phone number", with: "01234567890"
+    fill_in "What would you like more information about", with: "Lorem ipsum"
+    click_on "Complete sign up"
+
+    expect(page).to have_text "Your phone number"
+    expect(page).to have_text "There is a problem"
+    expect(page).to have_text "Accept the privacy policy to continue"
+    check "Yes"
+    click_on "Complete sign up"
+
+    expect(page).to have_text "You've signed up"
+    expect(page).to have_text "What happens next"
+  end
+
+  scenario "Full journey as non Student" do
+    visit mailing_list_steps_path
+
+    expect(page).to have_text "Sign up for personalised updates"
+    fill_in "First name", with: "Test"
+    fill_in "Last name", with: "User"
+    fill_in "Email address", with: "test@testuser.com"
+    select "Looking to change career"
+    click_on "Next Step"
+
+    expect(page).to have_text "How close are you to applying"
+    select "I'm not sure and finding out more"
+    click_on "Next Step"
+
+    expect(page).to have_text "Which subject do you want to teach"
+    select "Maths"
+    click_on "Next Step"
+
+    expect(page).to have_text "Your postcode"
+    fill_in "Your postcode", with: "TE57 1NG"
+    click_on "Next Step"
+
+    expect(page).to have_text "Your phone number"
+    fill_in "Your phone number", with: "01234567890"
+    fill_in "What would you like more information about", with: "Lorem ipsum"
+    click_on "Complete sign up"
+
+    expect(page).to have_text "Your phone number"
+    expect(page).to have_text "There is a problem"
+    expect(page).to have_text "Accept the privacy policy to continue"
+    check "Yes"
+    click_on "Complete sign up"
+
+    expect(page).to have_text "You've signed up"
+    expect(page).to have_text "What happens next"
+  end
+end
