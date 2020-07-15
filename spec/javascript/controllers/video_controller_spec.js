ca9ead1cdf871437d6c6862301fa596a55885f49
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2020-07-15 16:07:28 +0100

    unit test video player stimulus controller

--- spec/javascript/controllers/video_controller_spec.js
+++ spec/javascript/controllers/video_controller_spec.js
@@ -5,8 +5,16 @@ describe('AccordionController', () => {
 
     document.body.innerHTML = 
     `
-    <body data-controller="video">
-        <div class="video-overlay" data-target="video.player" data-action="click->video#close">
+    <div data-controller="video">
+        <a href="https://www.youtube.com/watch?v=MLdrZJpK5rU" target="_blank" data-action="click->video#play" data-target="video.link">
+            <div class="content-video">
+                <img src="/assets/images/case-study-helen.png" alt="Helen's story">
+                <div class="content-video__play">
+                    <div class="icon-play"></div>
+                </div>
+            </div>
+        </a>
+        <div id="the-video-player" class="video-overlay" data-target="video.player" data-action="click->video#close">
             <div class="video-overlay__background">   
             </div>
             <div class='video-overlay__video-container'>
@@ -21,20 +29,55 @@ describe('AccordionController', () => {
                     </iframe>
                 </div>
                 <div class="video-overlay__video-container__dismiss">
-                    <div class="icon-video-close" data-action="click->video#close"></div>
+                    <div id="close-button" class="icon-video-close" data-action="click->video#close"></div>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
     `
 
     const application = Application.start() ;
     application.register('video', VideoController) ;
 
-
     describe("when first loaded", () => {
-        it("something should happen", () => {
+        it("removes the target attribute from video links", () => {
+            let vidlink = document.getElementsByTagName('a');
+            expect(vidlink[0].getAttribute('target')).toBe(null);
         })
-    })
+    });
+
+    describe("when video link is clicked", () => {
+        it("swaps out the youtube url with the embedded version", () => {
+            let vidlink = document.getElementsByTagName('a')[0];
+            vidlink.click();
+            let iframe = document.getElementsByTagName('iframe')[0];
+            let iframesrc = iframe.getAttribute('src');
+            expect(iframesrc).toContain('https://www.youtube.com/embed/');
+        })
+        it("makes the video player visible", () => {
+            let videoplayer = document.getElementById('the-video-player');
+            expect(videoplayer.style.display).toBe('flex');
+        });
+    });
+
+    describe("when the dismiss button is clicked", () => {
+        it("sets the video player display to hidden", () => {
+            let closeButton = document.getElementById('close-button');
+            closeButton.click();
+            let videoplayer = document.getElementById('the-video-player');
+            expect(videoplayer.style.display).toBe('none');
+        })
+    });
+
+    describe("when the overlay is clicked and video is open", () => {
+        it("sets the video player display to hidden", () => {
+            let vidlink = document.getElementsByTagName('a')[0];
+            vidlink.click();
+            let videoplayer = document.getElementById('the-video-player');
+            videoplayer.click();
+            expect(videoplayer.style.display).toBe('none');
+        })
+    });
+    
 
 });
