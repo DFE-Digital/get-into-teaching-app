ba8b15f85f081613482b3100b1f75caecc12f769
Ross Oliver <ross.oliver36@gmail.com>
2020-07-16 10:07:56 +0100

    Rename postcode to address_postcode
    Bring inline with the API naming to make matchback prefilling simpler.

--- app/models/mailing_list/steps/postcode.rb
+++ app/models/mailing_list/steps/postcode.rb
@@ -1,11 +1,11 @@
 module MailingList
   module Steps
     class Postcode < ::Wizard::Step
-      attribute :postcode
-      validates :postcode, presence: true, postcode: true
+      attribute :address_postcode
+      validates :address_postcode, presence: true, postcode: true
 
-      before_validation if: :postcode do
-        self.postcode = postcode.to_s.strip
+      before_validation if: :address_postcode do
+        self.address_postcode = address_postcode.to_s.strip
       end
     end
   end
