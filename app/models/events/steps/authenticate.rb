ac1d095aeff20afb910d15885370be969e12dd5b
Ross Oliver <ross.oliver36@gmail.com>
2020-07-16 09:53:30 +0100

    Verify code on Authenticate step
    When a user enters a code we first check if it is syntactically valid (present, numerical and exactly 6 digits). If it is, then the code is sent to the API to check if it is correct.
    
    We only hit the API once per code (so if the code changes, we will re-call the API to verify). This is to avoid multiple calls via `valid?`.
    
    If the code is correct, then the wizard store is updated with the response from the API. The matchback criteria will not be overriden (email, first name, last name).

--- app/models/events/steps/authenticate.rb
+++ app/models/events/steps/authenticate.rb
@@ -4,6 +4,7 @@ module Events
       attribute :timed_one_time_password
 
       validates :timed_one_time_password, presence: true, length: { is: 6 }, format: { with: /\A[0-9]*\z/, message: "can only contain numbers" }
+      validate :timed_one_time_password_is_correct, if: :timed_one_time_password_valid?
 
       before_validation if: :timed_one_time_password do
         self.timed_one_time_password = timed_one_time_password.to_s.strip
@@ -11,6 +12,42 @@ module Events
 
       def skipped?
         @store["authenticate"] == false
+      end
+
+      def save
+        prepopulate_store if valid?
+
+        super
+      end
+
+      def timed_one_time_password=(value)
+        @totp_response = nil if value != timed_one_time_password
+        super(value)
+      end
+
+    private
+
+      def timed_one_time_password_valid?
+        self.class.validators_on(:timed_one_time_password).each do |validator|
+          validator.validate_each(self, :timed_one_time_password, timed_one_time_password)
+        end
+        errors.none?
+      end
+
+      def timed_one_time_password_is_correct
+        request = GetIntoTeachingApiClient::ExistingCandidateRequest.new(
+          @store.fetch("email", "first_name", "last_name").transform_keys { |k| k.camelize(:lower).to_sym },
+        )
+        @api ||= GetIntoTeachingApiClient::TeachingEventsApi.new
+        @totp_response ||= @api.get_pre_filled_teaching_event_add_attendee(timed_one_time_password, request)
+      rescue GetIntoTeachingApiClient::ApiError
+        errors.add(:timed_one_time_password, "is not correct")
+      end
+
+      def prepopulate_store
+        attribute_map = GetIntoTeachingApiClient::TeachingEventAddAttendee.attribute_map
+        hash = attribute_map.map { |k, _v| { k => @totp_response.send(k) } }.reduce(:merge)
+        @store.persist(hash.except(:email, :first_name, :last_name))
       end
     end
   end
