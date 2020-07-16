cd6d8825a5b68f2e5dba9f158b66efa501a603c6
Ross Oliver <ross.oliver36@gmail.com>
2020-07-16 10:03:03 +0100

    Rename phone_number to telephone
    Make the attribute consistent with the naming in the API, which will make
    pre-filling the form on matchback responses easier.

--- app/models/events/steps/contact_details.rb
+++ app/models/events/steps/contact_details.rb
@@ -1,11 +1,11 @@
 module Events
   module Steps
     class ContactDetails < ::Wizard::Step
-      attribute :phone_number
-      validates :phone_number, phone_number: true, allow_blank: true
+      attribute :telephone
+      validates :telephone, telephone: true, allow_blank: true
 
-      before_validation if: :phone_number do
-        self.phone_number = phone_number.to_s.strip
+      before_validation if: :telephone do
+        self.telephone = telephone.to_s.strip
       end
     end
   end
--- app/models/mailing_list/steps/contact.rb
+++ app/models/mailing_list/steps/contact.rb
@@ -1,17 +1,17 @@
 module MailingList
   module Steps
     class Contact < ::Wizard::Step
-      attribute :phone_number
+      attribute :telephone
       attribute :more_info
       attribute :accept_privacy_policy, :boolean
 
-      validates :phone_number, phone_number: true
-      validates :more_info, presence: true, if: -> { phone_number.present? }
+      validates :telephone, telephone: true
+      validates :more_info, presence: true, if: -> { telephone.present? }
       validates :more_info, number_of_words: { less_than: 200 }
       validates :accept_privacy_policy, acceptance: true, allow_nil: false
 
-      before_validation if: :phone_number do
-        self.phone_number = phone_number.to_s.strip
+      before_validation if: :telephone do
+        self.telephone = telephone.to_s.strip
       end
 
       before_validation if: :more_info do
