634f878b836f98e32e095bb7c2e2558a36beb32a
Ross Oliver <ross.oliver36@gmail.com>
2020-07-24 08:35:13 +0100

    Clean up styling around events and sign up forms
    - Tidy up eventbox/short partial usage. Tie class names to the type of event.
    - Consolidate event/mailing list registration styles into single stylesheet.
    - Update date formatting to match designs.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,18 +1,19 @@
 module EventsHelper
-  def format_event_date(event)
+  def format_event_date(event, stacked: true)
     return if event.start_at.blank?
 
     if event.start_at.to_date == event.end_at.to_date
-      sprintf \
-        "%{startdate} at %{starttime} - %{endtime}",
-        startdate: event.start_at.to_date.to_formatted_s(:long),
+      sprintf(
+        "%{startdate} #{stacked ? '<br />' : 'at'} %{starttime} - %{endtime}",
+        startdate: event.start_at.to_date.to_formatted_s(:long_ordinal),
         starttime: event.start_at.to_formatted_s(:time),
-        endtime: event.end_at.to_formatted_s(:time)
+        endtime: event.end_at.to_formatted_s(:time),
+      ).html_safe
     else
       sprintf \
         "%{startdate} to %{enddate}",
-        startdate: event.start_at.to_formatted_s(:full),
-        enddate: event.end_at.to_formatted_s(:full)
+        startdate: event.start_at.to_formatted_s(:long_ordinal),
+        enddate: event.end_at.to_formatted_s(:long_ordinal)
     end
   end
 end
--- spec/helpers/events_helper_spec.rb
+++ spec/helpers/events_helper_spec.rb
@@ -6,15 +6,22 @@ describe EventsHelper, type: "helper" do
   let(:event) { build(:event_api, start_at: startdate, end_at: enddate) }
 
   describe "#format_event_date" do
-    subject { format_event_date event }
+    let(:stacked) { true }
+    subject { format_event_date event, stacked: stacked }
 
     context "for single day event" do
-      it { is_expected.to eql "1 June 2020 at 10:00 - 12:00" }
+      it { is_expected.to eql "June 1st, 2020 <br /> 10:00 - 12:00" }
     end
 
     context "for multi day event" do
       let(:enddate) { DateTime.new(2020, 6, 4, 14) }
-      it { is_expected.to eql "1 June 2020 10:00 to 4 June 2020 14:00" }
+      it { is_expected.to eql "June 1st, 2020 10:00 to June 4th, 2020 14:00" }
+    end
+
+    context "when not stacked" do
+      let(:stacked) { false }
+
+      it { is_expected.to eql "June 1st, 2020 at 10:00 - 12:00" }
     end
   end
 end
