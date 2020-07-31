55242017ec60131515eadcce26ad7eb8f4055cef
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2020-07-31 11:25:40 +0100

    fix linting errors

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,18 +6,18 @@ class EventsController < ApplicationController
     categories = ["Train to Teach events", "Online events"]
     @events_by_category = categories.map do |category|
       events = 5.times.collect do
-      {
-        :category => category,
-        :title => "Train to teach Coventry Online event",
-        :datetime => "11 May 2020 at 17:00 - 18:30",
-        :description => "This online event specifically for anybody looking to train to teach in Coventry will give you the opportunity to ask our experts questions about teacher training.",
-        :type => GetIntoTeachingApi::Constants::EVENT_TYPES["Train to Teach Event"],
-        :online => true,
-        :location => "Coventry",
-        :readable_id => "ttc-online-event"
-      }
+        {
+          category: category,
+          title: "Train to teach Coventry Online event",
+          datetime: "11 May 2020 at 17:00 - 18:30",
+          description: "This online event specifically for anybody looking to train to teach in Coventry will give you the opportunity to ask our experts questions about teacher training.",
+          type: GetIntoTeachingApi::Constants::EVENT_TYPES["Train to Teach Event"],
+          online: true,
+          location: "Coventry",
+          readable_id: "ttc-online-event",
+        }
       end
-      { category_name: category, events: events}
+      { category_name: category, events: events }
     end
   end
 
