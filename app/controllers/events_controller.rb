d9e254a189db69a154712b5401385d32d20f901e
Ross Oliver <ross.oliver36@gmail.com>
2020-08-05 16:02:40 +0100

    Show next 3 events when categorised
    When viewing all events categorised we only want to display the next 3 events
    for each category (except the 'all events' section, where we display all events
    in chronological order).
    
    Also hides the 'all events' section when filtering to a specific category.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
 class EventsController < ApplicationController
-  before_action :load_events, only: %i[index search]
+  before_action :load_events, :categorise_events, only: %i[index search]
+
+  CATEGORISED_EVENTS_TO_DISPLAY = 3
 
   def index
     # Events are loaded in a before_action
@@ -20,13 +22,23 @@ private
   def load_events
     @event_search = Events::Search.new(event_search_params)
     @events = @event_search.query_events
-    @events_by_type = @events.each_with_object(Hash.new { |h, k| h[k] = [] }) do |event, hash|
+    @display_all_events_section = event_search_params[:type].blank?
+  end
+
+  def categorise_events
+    hash_default_value_is_array = Hash.new { |h, k| h[k] = [] }
+
+    @events_by_type = @events.each_with_object(hash_default_value_is_array) do |event, hash|
       hash[event.type_id] << event
     end
+
+    return unless @display_all_events_section
+
+    @events_by_type.transform_values! { |events| events.first(CATEGORISED_EVENTS_TO_DISPLAY) }
   end
 
   def event_search_params
-    defaults = ActionController::Parameters.new(month: Time.zone.today.to_formatted_s(:yearmonth))
+    defaults = ActionController::Parameters.new(month: Time.zone.today.to_formatted_s(:yearmonth), type: "")
 
     (params[Events::Search.model_name.param_key] || defaults)
       .permit(:type, :distance, :postcode, :month)
