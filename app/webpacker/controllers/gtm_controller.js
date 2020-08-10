d83e009939d96063900780a4d86264b140093db6
jebw <jeb@jdwilkins.co.uk>
2020-08-10 13:36:26 +0100

    Updated to latest GTM JS code

--- app/webpacker/controllers/gtm_controller.js
+++ app/webpacker/controllers/gtm_controller.js
@@ -11,16 +11,17 @@ export default class extends AnalyticsBaseController {
   }
 
   initService() {
-    window.dataLayer = window.dataLayer || [];
-    window.gtag = function (){dataLayer.push(arguments);}
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer',this.serviceId);
 
-    var e="https://www.googletagmanager.com/gtag/js?id=" + this.serviceId ;
-    var t=document.createElement("script");t.async=true,t.src=e;var
-    r=document.getElementsByTagName("script")[0];
-    r.parentNode.insertBefore(t,r) ;
-
-    window.gtag('js', new Date());
-    window.gtag('config', this.serviceId);
+    /* this is added for simplicity but is not compatible with multiple GTM containers */
+    /* We dont support that anyway so its not an issue here */
+    window.gtag = function() {
+      window.dataLayer.push(arguments);
+    }
   }
 
   get isEnabled() {
