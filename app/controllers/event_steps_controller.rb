51024bf0b15402ca75e4656aefdd97fcee0de3d3
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2020-08-17 13:48:38 +0100

    page titles on event steps

--- app/controllers/event_steps_controller.rb
+++ app/controllers/event_steps_controller.rb
@@ -1,8 +1,10 @@
 class EventStepsController < ApplicationController
-  before_action :load_event
+  before_action :load_event 
 
   include WizardSteps
   self.wizard_class = Events::Wizard
+
+  before_action :set_page_title
 
 private
 
@@ -22,5 +24,9 @@ private
 
   def load_event
     @event = GetIntoTeachingApiClient::TeachingEventsApi.new.get_teaching_event(params[:event_id])
+  end
+
+  def set_page_title
+    @page_title = "Sign up for #{@event.name}, #{@current_step.title.downcase} step"
   end
 end
