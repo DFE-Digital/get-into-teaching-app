145330d70c72fa0fc22e470861983a843aedf4f4
jebw <jeb@jdwilkins.co.uk>
2020-08-26 14:01:29 +0100

    Dropped future events field

--- app/models/events/steps/further_details.rb
+++ app/models/events/steps/further_details.rb
@@ -5,23 +5,17 @@ module Events
 
       attribute :event_id
       attribute :privacy_policy, :boolean
-      attribute :future_events, :boolean
       attribute :mailing_list, :boolean
       attribute :address_postcode
       attribute :accepted_policy_id
 
       validates :event_id, presence: true
       validates :privacy_policy, presence: true, acceptance: true
-      validates :future_events, inclusion: [true, false]
       validates :mailing_list, inclusion: [true, false]
       validates :address_postcode, postcode: { allow_blank: true }
 
       before_validation if: :address_postcode do
         self.address_postcode = address_postcode.to_s.strip.presence
-      end
-
-      before_validation if: :already_subscribed_to_events? do
-        self.future_events = true
       end
 
       before_validation if: :already_subscribed_to_mailing_list? do
@@ -30,7 +24,6 @@ module Events
 
       def save
         if valid?
-          @store["subscribe_to_events"] = future_events == true
           @store["subscribe_to_mailing_list"] = mailing_list == true
         end
 
@@ -47,10 +40,6 @@ module Events
 
       def already_subscribed_to_mailing_list?
         @store["already_subscribed_to_mailing_list"]
-      end
-
-      def already_subscribed_to_events?
-        @store["already_subscribed_to_events"]
       end
     end
   end
