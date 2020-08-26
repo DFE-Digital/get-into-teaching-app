14ee3ba2dd2b74f6383324caa868c0c8cd74c9a9
jebw <jeb@jdwilkins.co.uk>
2020-08-26 17:02:48 +0100

    Added GovUK JS

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -2,10 +2,14 @@ require.context('../fonts', true) ;
 require.context('../images', true) ;
 
 import '../styles/application.scss';
+
 import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
+import { initAll } from 'govuk-frontend';
 
 Rails.start();
 Turbolinks.start();
+
+initAll();
 
 import "controllers";
