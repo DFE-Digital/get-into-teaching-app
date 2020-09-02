63ce565b89cc361d98af0d07b023eaa943191bc3
Ross Oliver <ross.oliver36@gmail.com>
2020-09-02 08:04:57 +0100

    Add @stimulus/polyfills for IE 11 support

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,6 +1,7 @@
 require.context('../fonts', true) ;
 require.context('../images', true) ;
 
+import '@stimulus/polyfills'
 import '../styles/application.scss';
 import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
