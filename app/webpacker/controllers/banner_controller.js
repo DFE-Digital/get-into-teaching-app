256a94f90e9eb9b8e4864a700351368e6e64f51d
Ross Oliver <ross.oliver36@gmail.com>
2020-09-08 11:09:31 +0100

    Persist banner hidden state in a Cookie
    When a user hides a banner we want to ensure it remains hidden for their
    session; to do this we set a cookie and check for the cookie on page load,
    hiding the banner if it is present.

--- app/webpacker/controllers/banner_controller.js
+++ app/webpacker/controllers/banner_controller.js
@@ -1,8 +1,35 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-    hide(e) {
-        e.preventDefault();
-        this.element.style.display = "none";
+  connect() {
+    this.hideOnLoad();
+  }
+
+  hideOnLoad() {
+    if(document.cookie.indexOf(this.cookie) != -1) {
+      this.hideBanner();
     }
+  }
+
+  hide(e) {
+    e.preventDefault();
+    this.hideBanner();
+    this.setCookie();
+  }
+
+  hideBanner() {
+    this.element.style.display = 'none';
+  }
+
+  setCookie() {
+    document.cookie = this.cookie;
+  }
+
+  get cookie() {
+    return `GiTBetaBanner${this.name}=Hidden`;
+  }
+
+  get name() {
+    return this.data.get('name');
+  }
 }
--- spec/javascript/controllers/banner_controller_spec.js
+++ spec/javascript/controllers/banner_controller_spec.js
@@ -3,19 +3,33 @@ import BannerController from 'banner_controller';
 
 describe('BannerController', () => {
   document.body.innerHTML = `
-  <div data-controller="banner" id="banner">
+  <div data-controller="banner" id="banner" data-banner-name="Name">
     <a href="#" id="hideButton" data-action="banner#hide">Hide</a>
   </div>
   ` ;
 
   const application = Application.start();
   application.register('banner', BannerController);
+  const banner = document.getElementById('banner');
 
-  describe("clicking the hide button", () => {
-    it("hides the banner", () => {
-      var hideButton = document.getElementById('hideButton');
-      var banner = document.getElementById('banner');
-      hideButton.click();
+  describe("when the cookie is not yet set", () => {
+    it("shows the banner", () => {
+      expect(banner.style.display).not.toContain('none');
+    });
+
+    describe("clicking the hide button", () => {
+      it("hides the banner", () => {
+        var hideButton = document.getElementById('hideButton');
+        hideButton.click();
+        expect(banner.style.display).toContain('none');
+      });
+    });
+  });
+
+  describe("when the cookie has been set", () => {
+    beforeEach(() => { document.cookie = "GiTBetaBannerName=Hidden"; });
+
+    it('hides the banner', () => {
       expect(banner.style.display).toContain('none');
     });
   });
