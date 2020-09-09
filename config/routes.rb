7f72943aea634e5b66fc3f0fb65676afc6532c14
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2020-09-09 14:29:36 +0100

    fix page titles

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/404", to: "errors#not_found", via: :all
   get "/422", to: "errors#unprocessable_entity", via: :all
   get "/500", to: "errors#internal_server_error", via: :all
+
   if Rails.env.rolling? || Rails.env.preprod? || Rails.env.production?
     get "/assets/*missing", to: "errors#not_found", via: :all
   end
