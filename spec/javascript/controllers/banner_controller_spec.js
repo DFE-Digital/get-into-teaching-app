662eab454ffe2dacbfca27e53da01b143d78c80b
jebw <jeb@jdwilkins.co.uk>
2020-09-15 16:51:39 +0100

    Check with cookie prefs before storing cookies

--- spec/javascript/controllers/banner_controller_spec.js
+++ spec/javascript/controllers/banner_controller_spec.js
@@ -1,3 +1,4 @@
+const Cookies = require('js-cookie') ;
 import { Application } from 'stimulus' ;
 import BannerController from 'banner_controller';
 
@@ -8,9 +9,13 @@ describe('BannerController', () => {
   </div>
   ` ;
 
-  const application = Application.start();
-  application.register('banner', BannerController);
-  const banner = document.getElementById('banner');
+  beforeEach(() => {
+    Cookies.remove('GiTBetaBannerName') ;
+
+    const application = Application.start();
+    application.register('banner', BannerController);
+    const banner = document.getElementById('banner');
+  })
 
   describe("when the cookie is not yet set", () => {
     it("shows the banner", () => {
@@ -27,7 +32,7 @@ describe('BannerController', () => {
   });
 
   describe("when the cookie has been set", () => {
-    beforeEach(() => { document.cookie = "GiTBetaBannerName=Hidden"; });
+    beforeEach(() => { Cookies.set("GiTBetaBannerName', 'Hidden") });
 
     it('hides the banner', () => {
       expect(banner.style.display).toContain('none');
