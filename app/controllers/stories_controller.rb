47033a50e42b4df3e2441e09c1d3e17a3c074161
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2020-09-24 15:30:27 +0100

    controller doesnt need additional logic

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,11 +3,7 @@ class StoriesController < ApplicationController
   rescue_from ActionView::MissingTemplate, StaticPages::InvalidTemplateName, with: :rescue_missing_template
 
   def show
-    new_stories = ["becoming-a-mum-sparked-my-interest-in-teaching"]
-    is_new_story = new_stories.any? { |story| params[:story].include?(story) }
-    layout = is_new_story ? "layouts/stories" : "layouts/stories_old"
-
-    render template: stories_template, layout: layout
+    render template: stories_template, layout: "layouts/stories"
   end
 
 private
