e1ca301d3a0e3f6a0b080551bea9234e50d4e3d4
Ross Oliver <ross.oliver36@gmail.com>
2020-10-20 12:35:25 +0100

    Whitelist CSP keys and cap entry length
    To help prevent malicious usage of the endpoint we ensure the `csp-report` only
    contains valid keys and truncate the length of all values (hopefully avoiding
    someone maliciously filling up our logstash).
    
    I thought about setting up the CSP report as a model to add further validation
    (ensure the blocked-uri is a URL, for example) but it feels like more hassle
    than it's worth.

--- app/controllers/csp_reports_controller.rb
+++ app/controllers/csp_reports_controller.rb
@@ -1,11 +1,26 @@
 class CspReportsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  CSP_KEYS = %w[
+    blocked-uri
+    disposition
+    document-uri
+    effective-directive
+    original-policy
+    referrer
+    script-sample
+    status-code
+    violated-directive
+  ].freeze
+  MAX_ENTRY_LENGTH = 2_000
+
   def create
     json = JSON.parse(request.body.read)
-    report = json["csp-report"]
+    report = (json["csp-report"] || {})
+      .slice(*CSP_KEYS)
+      .transform_values { |v| v.truncate(MAX_ENTRY_LENGTH) }
 
-    Rails.logger.error(report) if report
+    Rails.logger.error(report) unless report.empty?
 
     head :ok
   end
--- spec/requests/csp_reports_spec.rb
+++ spec/requests/csp_reports_spec.rb
@@ -19,4 +19,11 @@ describe "CSP violation reporting" do
     it { is_expected.to have_http_status(:success) }
     it { expect(Rails.logger).to_not have_received(:error) }
   end
+
+  describe "when the csp-report contains keys not in our whitelist" do
+    let(:params) { { "csp-report" => { "blocked-uri" => "https://malicious.com/script.js", "random" => "information" } } }
+
+    it { expect(Rails.logger).to_not have_received(:error).with(params["csp-report"]) }
+    it { expect(Rails.logger).to have_received(:error).with(params["csp-report"].slice("blocked-uri")).once }
+  end
 end
