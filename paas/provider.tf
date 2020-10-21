59ddd31d2b1aa2dd7ddfac37fe05fee416fa1ce2
Steven Fawcett <steve.fawcett@capgemini.com>
2020-10-21 18:15:16 +0100

    Terraform upgrade to 13.4

--- paas/provider.tf
+++ paas/provider.tf
@@ -1,6 +1,16 @@
 provider "cloudfoundry" {
-    api_url = var.api_url
-    user = var.user
-    password =var.password 
+  api_url  = var.api_url
+  user     = var.user
+  password = var.password
 }
 
+terraform {
+  required_version = ">= 0.13.4"
+
+  required_providers {
+    cloudfoundry = {
+      source  = "cloudfoundry-community/cloudfoundry"
+      version = "0.12.6"
+    }
+  }
+}
