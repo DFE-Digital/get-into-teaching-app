fee9c8d79dde7859ca4b86d6dcea4db141bfa9ff
Peter Yates <peter.yates@graphia.co.uk>
2020-10-22 14:28:59 +0100

    Add some more specs for the feedback controller

--- spec/javascript/controllers/feedback_controller_spec.js
+++ spec/javascript/controllers/feedback_controller_spec.js
@@ -2,17 +2,15 @@ import { Application } from 'stimulus' ;
 import FeedbackController from 'feedback_controller.js';
 
 class fakeLocalStorage {
-  constructor() {
-    this.store = {};
+  constructor(store = {}) {
+    this.store = store;
   }
 
   getItem(key) {
-    console.log(this.store)
     return this.store[key]
   }
 
   setItem(key, value) {
-    console.log(this.store, value)
     return this.store[key] = value
   }
 }
@@ -62,10 +60,23 @@ describe('FeedbackController', () => {
     })
   })
 
+  describe("when the user has visited fewer than 3 pages", () => {
+    beforeEach(() => {
+      window.localStorage.setItem('feedbackPageCount', '1')
+
+      const application = Application.start();
+      application.register('feedback', FeedbackController);
+    })
+
+    it('the feedback bar is never shown', () => {
+      const feedbackBar = document.getElementById('feedback-bar');
+      expect(feedbackBar.style.display).toEqual('')
+    })
+  })
+
   describe("when the user has visited 3 or more pages", () => {
     beforeEach(() => {
       Object.defineProperty(window, 'localStorage', { value: new fakeLocalStorage() });
-
       window.localStorage.setItem('feedbackPageCount', '5')
 
       const application = Application.start();
@@ -75,6 +86,54 @@ describe('FeedbackController', () => {
     it('shows the feedback bar', () => {
       const feedbackBar = document.getElementById('feedback-bar');
       expect(feedbackBar.style.display).toContain('flex');
+    })
+  })
+
+  describe("viewing a page increments the counter", () => {
+    beforeEach(() => {
+      Object.defineProperty(window, 'localStorage', { value: new fakeLocalStorage() });
+      window.localStorage.setItem('feedbackPageCount', '2')
+
+      const application = Application.start();
+      application.register('feedback', FeedbackController);
+    })
+
+    it("the counter has increased by one", () => {
+      const newCount = window.localStorage.getItem('feedbackPageCount')
+      // the real localStorage would cast this to a String
+      expect(newCount).toEqual(3)
+    })
+  })
+
+  describe("dismissing the feedback box", () => {
+    beforeEach(() => {
+      Object.defineProperty(window, 'localStorage', { value: new fakeLocalStorage({'feedbackPageCount': '2'}) });
+
+      const application = Application.start();
+      application.register('feedback', FeedbackController);
+    })
+
+    it("the dismissed flag should be true", () => {
+      const hideLink = document.getElementById('hide-feedback-bar');
+      hideLink.click();
+
+      const flag = window.localStorage.getItem('feedbackDismissed')
+      expect(flag).toEqual("true")
+    })
+  })
+
+  describe("once dismissed the feedback bar shouldn't be shown", () => {
+    beforeEach(() => {
+      Object.defineProperty(window, 'localStorage', { value: new fakeLocalStorage({ feedbackDismissed: "true", feedbackPageCount: "8" }) });
+
+      const application = Application.start();
+      application.register('feedback', FeedbackController);
+    })
+
+    it("the feedback bar shouldn't be shown", () => {
+      const feedbackBar = document.getElementById('feedback-bar');
+
+      expect(feedbackBar.style.display).toEqual('')
     })
   })
 })
