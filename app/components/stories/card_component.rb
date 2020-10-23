c7540769dbee51830f0b97fa0cd984c3c33417c1
Peter Yates <peter.yates@graphia.co.uk>
2020-10-23 10:50:50 +0100

    Add video functionality to the card component
    The video will pop up and play as before. Now there are tests to
    accompany the story card too.

--- app/components/stories/card_component.rb
+++ app/components/stories/card_component.rb
@@ -11,5 +11,23 @@ module Stories
       @image   = card["image"]
       @video   = card["video"]
     end
+
+    def media_link
+      @video ? video_link : image_link
+    end
+
+  private
+
+    def image_link
+      link_to(link, class: "story-card__thumb") do
+        image_tag(image)
+      end
+    end
+
+    def video_link
+      link_to(video, class: "story-card__thumb", data: { action: "click->video#play", target: "video.link" }) do
+        safe_join([tag.div(helpers.fas_icon("play"), class: "story-card__thumb--play-icon"), image_tag(image)])
+      end
+    end
   end
 end
---
+++ spec/components/stories/card_component_spec.rb
@@ -0,0 +1,57 @@
+require "rails_helper"
+
+describe Stories::CardComponent, type: "component" do
+  let(:story) do
+    {
+      name: "Edna Krabappel",
+      snippet: "Your education is important. Roman numerals, et cetera",
+      link: "/stories/edna-k",
+      image: "/images/edna-k.jpg",
+    }.with_indifferent_access
+  end
+
+  subject! { render_inline(Stories::CardComponent.new(card: story)) }
+
+  specify "renders a story card" do
+    expect(page).to have_css(".story-card")
+  end
+
+  specify "includes a link wrapping the story image" do
+    expect(page).to have_link(href: story[:link]) do |anchor|
+      expect(anchor).to have_css(%(img[src='#{story[:image]}']))
+    end
+  end
+
+  specify "includes the snippet" do
+    expect(page).to have_content(story[:snippet])
+  end
+
+  specify "includes the name in a link" do
+    expect(page).to have_link(%(Read #{story[:name]}'s story), href: story[:link], class: "git-link")
+  end
+
+  specify "no play icon is visible" do
+    expect(page).not_to have_css(".fas.fa-play")
+  end
+
+  context "video stories" do
+    let(:video) { "https://www.youtube.com/watch?v=dQw4w9WgXcQ" }
+    let(:video_story) { story.merge(video: video).with_indifferent_access }
+
+    subject! { render_inline(Stories::CardComponent.new(card: video_story)) }
+
+    specify "the image links to the video instead of the story" do
+      expect(page).to have_link(href: video_story[:video]) do |anchor|
+        expect(anchor).to have_css(%(img[src='#{story[:image]}']))
+      end
+    end
+
+    specify "the card has a play icon" do
+      expect(page).to have_css(".fas.fa-play")
+    end
+
+    specify "the link has the required data attributes for the pop up video" do
+      expect(page).to have_css(%(a[data-action='click->video#play'][data-target='video.link']))
+    end
+  end
+end
