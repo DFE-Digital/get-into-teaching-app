789bab41558b614e427a56c5a130ac34614e9bde
Jeb Wilkins <jeb@jdwilkins.co.uk>
2020-11-04 09:32:28 +0000

    Revert "Added a save button to the Cookie Preferences page"

--- spec/javascript/controllers/cookie_preferences_controller_spec.js
+++ spec/javascript/controllers/cookie_preferences_controller_spec.js
@@ -5,26 +5,18 @@ const Cookies = require('js-cookie') ;
 import CookiePreferences from 'cookie_preferences' ;
 
 describe('CookiePreferencesController', () => {
-  function clearPage() {
-    document.body.innerHTML = '' ;
-  }
+  document.body.innerHTML =
+  `<form data-controller="cookie-preferences">
+    <fieldset data-target="cookie-preferences.category" data-category="first">
+      <input type="radio" value="0" id="first-no" name="cookies-first" data-action="cookie-preferences#toggle" />
+      <input type="radio" value="1" id="first-yes" name="cookies-first" data-action="cookie-preferences#toggle" />
+    </fieldset>
 
-  function initPage() {
-    document.body.innerHTML =
-    `<form data-controller="cookie-preferences">
-      <fieldset data-target="cookie-preferences.category" data-category="first">
-        <input type="radio" value="0" id="first-no" name="cookies-first" data-action="cookie-preferences#toggle" />
-        <input type="radio" value="1" id="first-yes" name="cookies-first" data-action="cookie-preferences#toggle" />
-      </fieldset>
-
-      <fieldset data-target="cookie-preferences.category" data-category="second">
-        <input type="radio" value="0" id="second-no" name="cookies-second" data-action="cookie-preferences#toggle" />
-        <input type="radio" value="1" id="second-yes" name="cookies-second" data-action="cookie-preferences#toggle" />
-      </fieldset>
-
-      <button type="submit" data-action="cookie-preferences#save">Save</save>
-    </form>` ;
-  }
+    <fieldset data-target="cookie-preferences.category" data-category="second">
+      <input type="radio" value="0" id="second-no" name="cookies-second" data-action="cookie-preferences#toggle" />
+      <input type="radio" value="1" id="second-yes" name="cookies-second" data-action="cookie-preferences#toggle" />
+    </fieldset>
+  </form>` ;
 
   function getCookie() {
     return Cookies.get(CookiePreferences.cookieName) ;
@@ -46,18 +38,13 @@ describe('CookiePreferencesController', () => {
     setJsonCookie({ first: true, second: false })
   }
 
-  function getState() {
-    const form = document.querySelector('form[data-controller="cookie-preferences"]')
-    return form.getAttribute('data-cookie-preferences-save-state') ;
+  function initApp(setCookie) {
+    const application = Application.start() ;
+    application.register('cookie-preferences', CookiePreferencesController);
   }
 
-  const application = Application.start() ;
-  application.register('cookie-preferences', CookiePreferencesController);
-
-  beforeEach(clearPage) ;
-
   describe("on page load", () => {
-    beforeEach(() => { initCookie(); initPage() })
+    beforeEach(() => { initCookie(); initApp() })
 
     it('radios should be assigned', () => {
       expect(document.getElementById('first-yes').checked).toBe(true) ;
@@ -68,43 +55,23 @@ describe('CookiePreferencesController', () => {
   }) ;
 
   describe("on page load without cookie", () => {
-    beforeEach(() => { initPage() })
+    beforeEach(() => { initApp() })
 
     it('should save cookie', () => {
       const data = getJsonCookie() ;
-      expect(data['functional']).toBe(undefined) ;
+      expect(data['functional']).toBe(true) ;
     })
   })
 
   describe("when changing radios", () => {
-    beforeEach(() => { initCookie(); initPage() })
+    beforeEach(() => { initCookie(); initApp() })
 
-    it("cookie should only be updated when they click save", () => {
-      expect(getJsonCookie()).toEqual({ first: true, second: false })
-      document.getElementById('first-no').click()
-      expect(getJsonCookie()).toEqual({ first: true, second: false })
-      document.getElementById('second-yes').click()
-      expect(getJsonCookie()).toEqual({ first: true, second: false })
-
-      document.querySelector('button').click()
+    it("cookie should be updated", () => {
+      expect(getJsonCookie()).toEqual({ first: true, functional: true, second: false })
+      document.getElementById('first-no').click() ;
+      expect(getJsonCookie()).toEqual({ first: false, functional: true, second: false })
+      document.getElementById('second-yes').click() ;
       expect(getJsonCookie()).toEqual({ first: false, functional: true, second: true })
     })
   }) ;
-
-  describe("save state", () => {
-    beforeEach(() => { initCookie(); initPage() })
-
-    it("should change state when clicking save", () => {
-      expect(getState()).toEqual(null) ;
-
-      document.getElementById('first-no').click()
-      expect(getState()).toEqual('unsaved')
-
-      document.querySelector('button').click()
-      expect(getState()).toEqual('saving')
-
-      document.getElementById('second-yes').click()
-      expect(getState()).toEqual('unsaved')
-    })
-  })
 }) ;
