a4502fcdc2d68c03d1077eda22ea1105efae1568
Peter Yates <peter.yates@graphia.co.uk>
2020-11-09 09:29:04 +0000

    Add target="_blank" to external content links
    While we're here, fix the link controller specs that don't look like
    they ever quite worked.

--- app/webpacker/controllers/link_controller.js
+++ app/webpacker/controllers/link_controller.js
@@ -1,20 +1,30 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-
-    static targets = [ "content"];
+    static targets = ["content"];
 
     connect() {
         this.preventTurboLinksOnJumpLinks();
+        this.openExternalContentLinksInNewWindow();
     }
 
     preventTurboLinksOnJumpLinks() {
         const links = this.contentTarget.querySelectorAll('a');
+
         links.forEach((l) => {
             if (l.getAttribute('href')?.startsWith('#')) {
                 l.dataset.turbolinks = "false"
             }
         });
     }
-        
+
+    openExternalContentLinksInNewWindow() {
+        const links = this.contentTarget.querySelectorAll('.content a');
+
+        links.forEach((l) => {
+            if (l.getAttribute('href')?.startsWith('http')) {
+              l.setAttribute('target', '_blank');
+            }
+        });
+    }
 }
