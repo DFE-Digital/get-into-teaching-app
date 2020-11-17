c67b1f2764788172cde463e9f3d0158f0c2cdf8b
jebw <jeb@jdwilkins.co.uk>
2020-11-17 16:14:38 +0000

    Removed old wizard classes

--- spec/support/wizard_support.rb
+++ spec/support/wizard_support.rb
@@ -68,22 +68,3 @@ shared_examples "an issue verification code wizard step" do
     end
   end
 end
-
-class TestWizard < Wizard::Base
-  class Name < Wizard::Step
-    attribute :name
-    validates :name, presence: true
-  end
-
-  class Age < Wizard::Step
-    attribute :age, :integer
-    validates :age, presence: true
-  end
-
-  class Postcode < Wizard::Step
-    attribute :postcode
-    validates :postcode, presence: true
-  end
-
-  self.steps = [Name, Age, Postcode].freeze
-end
