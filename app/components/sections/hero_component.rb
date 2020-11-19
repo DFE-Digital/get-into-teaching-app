668fcc41b2a4ece214fc67085f68df605ae92a74
Peter Yates <peter.yates@graphia.co.uk>
2020-11-19 16:28:50 +0000

    Add 'deep' styling for taller hero sections

--- app/components/sections/hero_component.rb
+++ app/components/sections/hero_component.rb
@@ -4,25 +4,32 @@ module Sections
 
     attr_accessor :deep
 
-    def initialize(front_matter)
+    def initialize(front_matter, deep: false)
       front_matter.tap do |fm|
         @title             = fm["title"]
         @subtitle          = fm["subtitle"]
         @image             = fm["image"]
         @mobile_image      = fm["mobile_image"] || false
         @show_mailing_list = fm["mailinglist"]
+        @deep              = fm["deepheader"]
       end
 
-      # only used in two places, move to template
-      @deep = true
     end
 
-    # def header
-    #   tag.header(style: "background-image: url('#{helpers.hero_image_path(image)}')") { yield }
-    # end
+    def classes
+      %w[hero].tap do |c|
+        c << "hero--deep" if deep?
+      end
+    end
 
     def render?
       image.present?
+    end
+
+private
+
+    def deep?
+      @deep
     end
   end
 end
