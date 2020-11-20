66c4e6d1ba26a4dad488154a07104bbb47775c11
Peter Yates <peter.yates@graphia.co.uk>
2020-11-20 13:19:37 +0000

    Allow for responsive hero images

--- app/components/sections/hero_component.rb
+++ app/components/sections/hero_component.rb
@@ -1,15 +1,13 @@
 module Sections
   class HeroComponent < ViewComponent::Base
-    attr_accessor :title, :image, :mobile_image, :subtitle, :show_mailing_list
-
-    attr_accessor :deep
+    attr_accessor :title, :image, :mobile_image, :subtitle, :show_mailing_list, :deep
 
     def initialize(front_matter, deep: false)
       front_matter.with_indifferent_access.tap do |fm|
         @title             = fm["title"]
         @subtitle          = fm["subtitle"]
         @image             = fm["image"]
-        @mobile_image      = fm["mobile_image"] || false
+        @mobile_image      = fm["mobileimage"]
         @show_mailing_list = fm["mailinglist"]
         @deep              = fm["deepheader"]
       end
@@ -26,10 +24,25 @@ module Sections
       image.present?
     end
 
+    def responsive_image
+      image_sizes = [%(#{image_path} 800w)]
+      image_sizes << %(#{mobile_image_path} 600w) if mobile_image.present?
+
+      image_tag(image_path, srcset: image_sizes.join(", "), alt: "Student in a classroom")
+    end
+
   private
 
     def deep?
       @deep
+    end
+
+    def image_path
+      helpers.hero_image_path(image)
+    end
+
+    def mobile_image_path
+      helpers.hero_image_path(mobile_image)
     end
   end
 end
