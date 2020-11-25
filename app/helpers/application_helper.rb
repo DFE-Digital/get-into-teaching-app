99b3f16b4719fdabcb9a59db9bbe1e6ce018a2f2
Joseph Kempster <jkempster34@gmail.com>
2020-11-25 12:21:57 +0000

    Add highlight to current page title in navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -94,4 +94,12 @@ module ApplicationHelper
 
     referer.gsub(root_url, root_path)
   end
+
+
+  def nav_link(link_text, link_path)
+    class_name = 'active' if current_page?(link_path)
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
 end
