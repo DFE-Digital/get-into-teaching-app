a5c63b01892026d0ed61aaa089fb8dcc955b49ec
jebw <jeb@jdwilkins.co.uk>
2020-11-25 16:42:06 +0000

    Added CardWithHeaderComponent

---
+++ spec/components/stories/card_with_header_component_spec.rb
@@ -0,0 +1,57 @@
+require "rails_helper"
+
+describe Stories::CardWithHeaderComponent, type: "component" do
+  let :edna do
+    {
+      title: "Edna's career in teaching",
+      image: "/images/edna-k.jpg",
+    }
+  end
+
+  let :base do
+    {
+      name: "Edna Krabappel",
+      snippet: "Your education is important. Roman numerals, et cetera",
+      link: "/stories/edna-k",
+      image: "/images/edna-k.jpg",
+    }.with_indifferent_access
+  end
+
+  let(:story) { base }
+  let(:page_data) { nil }
+
+  subject do
+    render_inline(described_class.new(card: story, page_data: page_data))
+    page
+  end
+
+  it { is_expected.to have_css ".card" }
+  it { is_expected.to have_css ".card.card--no-border" }
+  it { is_expected.not_to have_css ".card header" }
+  it { is_expected.to have_content story[:snippet] }
+
+  context "With supplied header" do
+    let(:story) { base.merge header: "Edna's story" }
+
+    it { is_expected.to have_css ".card header" }
+  end
+
+  context "With header from stories frontmatter" do
+    let(:page_data) { Pages::Data.new }
+    let(:truncated) { edna[:title].slice 0, described_class::MAX_HEADER_LENGTH }
+
+    before do
+      allow(Pages::Frontmatter).to \
+        receive(:find).with(story[:link]).and_return edna
+    end
+
+    it do
+      is_expected.to have_css ".card header", text: truncated
+    end
+  end
+
+  context "With no header and unknown page" do
+    let(:page_data) { Pages::Data.new }
+    it { is_expected.not_to have_css ".card header" }
+  end
+end
