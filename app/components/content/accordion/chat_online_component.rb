b95a33dad7a9481cd5813a66611766f94dc64649
Peter Yates <peter.yates@graphia.co.uk>
2020-11-25 16:31:52 +0000

    Add a 'Chat online' component
    This will be appended to the bottom of the accordion segment when the
    corresponding value is found in the frontmatter.

--- app/components/content/accordion/chat_online_component.rb
+++ app/components/content/accordion/chat_online_component.rb
@@ -6,7 +6,8 @@ module Content
           asset_pack_path("media/images/calls-to-action/chat-icon.png"),
           width: 50,
           height: 50,
-          alt: "Chat with a advisor online"
+          alt: "Chat with a advisor online",
+          class: "call-to-action__icon",
         )
       end
     end
