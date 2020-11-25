66b972979d55e12dedcb4b915f7edaa77d4133f4
Peter Yates <peter.yates@graphia.co.uk>
2020-11-25 14:26:51 +0000

    Add an icon, text and button to chat online CTA

--- app/components/content/accordion/chat_online_component.rb
+++ app/components/content/accordion/chat_online_component.rb
@@ -1,5 +1,14 @@
 module Content
   module Accordion
-    class ChatOnlineComponent < ViewComponent::Base; end
+    class ChatOnlineComponent < ViewComponent::Base
+      def icon
+        image_tag(
+          asset_pack_path("media/images/calls-to-action/chat-icon.png"),
+          width: 50,
+          height: 50,
+          alt: "Chat with a advisor online"
+        )
+      end
+    end
   end
 end
