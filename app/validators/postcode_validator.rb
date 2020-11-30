91e681852b3a8055f594793ccdce73d340cb3669
Joseph Kempster <jkempster34@gmail.com>
2020-11-30 16:00:51 +0000

    Update validator to allow outward only postcodes in event search

--- app/validators/postcode_validator.rb
+++ app/validators/postcode_validator.rb
@@ -1,5 +1,16 @@
 class PostcodeValidator < ActiveModel::EachValidator
-  PATTERN = %r{\A([A-Z][A-HJ-Y]?\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})\Z}.freeze
+  OUTWARD_POSTCODE_PATTERN = /[A-Z][A-HJ-Y]?\d[A-Z\d]?/.freeze
+  FULL_POSTCODE_PATTERN = /#{OUTWARD_POSTCODE_PATTERN} ?\d[A-Z]{2}|GIR ?0A{2}/.freeze
+
+  OUTWARD_OR_FULL_POSTCODE_REGEX = %r{\A(#{OUTWARD_POSTCODE_PATTERN})\Z|\A(#{FULL_POSTCODE_PATTERN})\Z}.freeze
+  FULL_POSTCODE_REGEX = %r{\A(#{FULL_POSTCODE_PATTERN})\Z}.freeze
+
+  attr_reader :outward_only_postcode
+
+  def initialize(options)
+    super
+    @outward_only_postcode = options[:outward_only_postcode]
+  end
 
   def validate_each(record, attribute, value)
     if value.present? && invalid_postcode?(value)
@@ -9,7 +20,11 @@ class PostcodeValidator < ActiveModel::EachValidator
 
 private
 
+  def validation_regex
+    outward_only_postcode ? OUTWARD_OR_FULL_POSTCODE_REGEX : FULL_POSTCODE_REGEX
+  end
+
   def invalid_postcode?(postcode)
-    !PATTERN.match?(postcode)
+    !validation_regex.match?(postcode)
   end
 end
--- spec/validators/postcode_validator_spec.rb
+++ spec/validators/postcode_validator_spec.rb
@@ -3,24 +3,37 @@ require "rails_helper"
 describe PostcodeValidator do
   class TestModel
     include ActiveModel::Model
-    attr_accessor :postcode
-    validates :postcode, postcode: true
+    attr_accessor :postcode, :outward_only_postcode
+    validates :postcode, postcode: { outward_only_postcode: :outward_only_postcode }
   end
 
   before { instance.valid? }
   subject { instance.errors.to_h }
 
-  ["F00BAR", "M1", "123ABC", "TE57 ING"].each do |postcode|
-    context "checking '#{postcode}'" do
-      let(:instance) { TestModel.new(postcode: postcode) }
-      it { is_expected.to include postcode: "is invalid" }
+  context "with invalid full postcodes" do
+    ["F00BAR", "123ABC", "TE57 ING"].each do |postcode|
+      context "checking '#{postcode}'" do
+        let(:instance) { TestModel.new(postcode: postcode) }
+        it { is_expected.to include postcode: "is invalid" }
+      end
     end
   end
 
-  ["M1 2WD", "TE57 1NG", ""].each do |postcode|
-    context "checking '#{postcode}'" do
-      let(:instance) { TestModel.new(postcode: postcode) }
-      it { is_expected.not_to include :postcode }
+  context "with valid full postcodes" do
+    ["M1 2WD", "TE57 1NG", ""].each do |postcode|
+      context "checking '#{postcode}'" do
+        let(:instance) { TestModel.new(postcode: postcode) }
+        it { is_expected.not_to include :postcode }
+      end
+    end
+  end
+
+  context "with valid outward only postcodes" do
+    %w[ST6 M1 TE57].each do |postcode|
+      context "checking '#{postcode}'" do
+        let(:instance) { TestModel.new(postcode: postcode, outward_only_postcode: true) }
+        it { is_expected.not_to include :postcode }
+      end
     end
   end
 end
