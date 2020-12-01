e80447d1a80269f74382f7d43d8df6ff8867bbe2
Peter Yates <peter.yates@graphia.co.uk>
2020-12-01 10:34:58 +0000

    Remove the template handler and refactor sitemaps
    Setting the sitemap using the template handler had some unpredictable
    side effects that meant the instance variable's scope crept further than
    it should've.
    
    Now Pages::Frontmatter is in place we can use it to simplify the process
    and format an output compatible with the navbar directly from
    `navigation_resources`.

--- app/helpers/sitemap_helper.rb
+++ app/helpers/sitemap_helper.rb
@@ -1,8 +1,9 @@
 module SitemapHelper
-  def navigation_resources(sitemap)
-    navigation_content = sitemap[:markdown_content].select do |content|
-      content.dig(:front_matter, :navigation)
-    end
-    navigation_content.sort_by { |resource| resource.dig(:front_matter, :navigation) }
+  def navigation_resources
+    Pages::Frontmatter
+      .list
+      .select { |_relative_path, fm| fm.key?(:navigation) }
+      .sort_by { |_relative_path, fm| fm.fetch(:navigation) }
+      .map { |relative_path, fm| { path: relative_path, front_matter: fm } }
   end
 end
--- config/initializers/template_handlers.rb
+++ config/initializers/template_handlers.rb
@@ -1,6 +1,4 @@
 require "template_handlers/markdown"
-require "template_handlers/erb"
 
-ActionView::Template.register_template_handler :erb, TemplateHandlers::ERB
 ActionView::Template.register_template_handler :md, TemplateHandlers::Markdown
 ActionView::Template.register_template_handler :markdown, TemplateHandlers::Markdown
