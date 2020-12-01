819a5ec43c19121656a99b73e1a54850de79aa75
Joseph Kempster <jkempster34@gmail.com>
2020-12-01 10:35:49 +0000

    Add navbar component tests

--- app/components/navbar/navbar_component.rb
+++ app/components/navbar/navbar_component.rb
@@ -1,5 +1,7 @@
 module Navbar
   class NavbarComponent < ViewComponent::Base
+    attr_reader :sitemap
+
     def initialize(sitemap)
       @sitemap = sitemap
     end
@@ -12,13 +14,15 @@ module Navbar
 
     def nav_link(link_text, link_path)
       class_name = "active" if first_uri_segment_matches_link?(link_path)
+
       content_tag(:li, class: class_name) do
         link_to link_text, link_path
       end
     end
 
     def first_uri_segment_matches_link?(link_path)
-      current_uri = request.env["PATH_INFO"]
+      current_uri = request.path
+
       /^\/[^\/]*/.match(current_uri)[0] == link_path
     end
   end
