407558d05f560a0636c9e846962d35e2683084b6
Joseph Kempster <jkempster34@gmail.com>
2020-12-03 10:59:50 +0000

    Fix links not showing on mobile

--- spec/components/navbar/navbar_component_spec.rb
+++ spec/components/navbar/navbar_component_spec.rb
@@ -9,10 +9,16 @@ describe Navbar::NavbarComponent, type: "component" do
     allow(Pages::Frontmatter).to receive(:list).and_return all_frontmatter
   end
 
-  subject! { render_inline(Navbar::NavbarComponent.new(all_frontmatter)) }
+  subject! { render_inline(Navbar::NavbarComponent.new) }
 
   describe "Desktop navbar component" do
     context "when rendered" do
+      it "includes links from content" do
+        expect(page).to have_css(".navbar__desktop ul") do |ul|
+          expect(ul).to have_link("Mock title", href: "/mock-path")
+        end
+      end
+
       it "shows background colour on link that matches the current page" do
         expect(page).to have_css("li.active") do |active_li|
           expect(active_li).to have_link("Home", href: "/")
@@ -21,6 +27,16 @@ describe Navbar::NavbarComponent, type: "component" do
 
       it "shows no background colour on links that do not match the current page" do
         page.all(".navbar__desktop li.active", count: 1)
+      end
+    end
+  end
+
+  describe "Mobile navbar component" do
+    context "when rendered" do
+      it "includes links from content" do
+        expect(page).to have_css(".navbar__mobile ul") do |ul|
+          expect(ul).to have_link("Mock title", href: "/mock-path")
+        end
       end
     end
   end
