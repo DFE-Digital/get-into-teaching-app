b9c2cac6713d6fe01e14dfe80a7fbf21c7e256b2
Peter Yates <peter.yates@graphia.co.uk>
2020-12-04 11:53:48 +0000

    Replace PNG icons with SVG

--- app/components/calls_to_action/chat_online_component.rb
+++ app/components/calls_to_action/chat_online_component.rb
@@ -2,7 +2,7 @@ module CallsToAction
   class ChatOnlineComponent < ViewComponent::Base
     def icon
       image_tag(
-        asset_pack_path("media/images/calls-to-action/chat-icon.png"),
+        asset_pack_path("media/images/calls-to-action/icon-question.svg"),
         width: 50,
         height: 50,
         alt: "Chat with a advisor online",
--- app/components/calls_to_action/simple_component.rb
+++ app/components/calls_to_action/simple_component.rb
@@ -13,7 +13,7 @@ module CallsToAction
 
     def icon_element(icon)
       image_tag(
-        asset_pack_path("media/images/calls-to-action/#{icon}.png"),
+        asset_pack_path("media/images/calls-to-action/#{icon}.svg"),
         width: 50,
         height: 50,
         alt: "#{icon} icon",
