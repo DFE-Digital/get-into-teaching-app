eb0ac3268b91bbf36c898a400e55c372de55ed3f
Peter Yates <peter.yates@graphia.co.uk>
2020-12-04 16:45:09 +0000

    Fix spelling of 'adviser'

--- app/components/calls_to_action/chat_online_component.rb
+++ app/components/calls_to_action/chat_online_component.rb
@@ -5,7 +5,7 @@ module CallsToAction
         asset_pack_path("media/images/calls-to-action/icon-question.svg"),
         width: 50,
         height: 50,
-        alt: "Chat with a advisor online",
+        alt: "Chat with a adviser online",
         class: "call-to-action__icon",
       )
     end
