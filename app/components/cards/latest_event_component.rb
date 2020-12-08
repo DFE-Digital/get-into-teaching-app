b324629d2d932bb8267c5edb2b5f7a0f28f68b38
jebw <jeb@jdwilkins.co.uk>
2020-12-08 14:26:36 +0000

    Handle when no events or no category

--- app/components/cards/latest_event_component.rb
+++ app/components/cards/latest_event_component.rb
@@ -1,13 +1,22 @@
 module Cards
   class LatestEventComponent < CardComponent
-    attr_reader :page_data, :category
+    ALL_EVENTS_HEADER = "Find a Teacher Training Event".freeze
+    ALL_EVENTS_SNIPPET = <<~SNIPPET.freeze
+      Find an event to speak to real teachers and get your questions answered
+      by our expert advisers. You can also find out what it is like to train
+      and be in the classroom.
+    SNIPPET
+
+    attr_reader :page_data, :category, :event
     with_collection_parameter :card
 
     def initialize(card:, page_data: nil)
       super card: card
 
-      @category = card["category"]
+      @category = card["category"].to_s
       @page_data = page_data
+
+      fetch_event
     end
 
     def border
@@ -15,19 +24,23 @@ module Cards
     end
 
     def header
-      event.name
+      event ? "Event: #{event.name}" : ALL_EVENTS_HEADER
     end
 
     def snippet
-      event.summary
+      event ? event.summary : ALL_EVENTS_SNIPPET
     end
 
     def link_text
-      "View event"
+      event ? "View event" : "View events"
     end
 
     def link
-      event_path event.readable_id
+      if event
+        event_path event.readable_id
+      else
+        events_path
+      end
     end
 
     def image
@@ -37,8 +50,8 @@ module Cards
 
   private
 
-    def event
-      @event ||= @page_data.latest_event_for_category(category)
+    def fetch_event
+      @event = @page_data.latest_event_for_category(category)
     end
   end
 end
--- spec/components/cards/latest_event_component_spec.rb
+++ spec/components/cards/latest_event_component_spec.rb
@@ -3,27 +3,65 @@ require "rails_helper"
 RSpec.describe Cards::LatestEventComponent, type: :component do
   subject { render_inline(instance) && page }
 
+  let(:page_data) { Pages::Data.new }
   let(:url_helpers) { Rails.application.routes.url_helpers }
   let(:event) { build(:event_api, name: "Test event") }
-  let(:page_data) { double Pages::Data, latest_event_for_category: event }
   let(:instance) { described_class.new card: card, page_data: page_data }
   let(:card) { { category: "train to teach event" }.with_indifferent_access }
 
-  before do
-    expect(page_data).to \
-      receive(:latest_event_for_category).with("train to teach event")
+  context "with category" do
+    before do
+      expect(page_data).to receive(:latest_event_for_category)
+        .with("train to teach event")
+        .and_return event
+    end
+
+    context "with events" do
+      it { is_expected.to have_css ".card" }
+      it { is_expected.to have_css ".card.card--no-border" }
+      it { is_expected.to have_css ".card header", text: event.name }
+      it { is_expected.to have_css "img" }
+      it { is_expected.to have_content event.summary }
+
+      it "includes the footer link" do
+        is_expected.to have_link \
+          "View event",
+          href: url_helpers.event_path(event.readable_id),
+          class: "git-link"
+      end
+    end
+
+    context "with no events" do
+      let(:event) { nil }
+      let(:header) { described_class::ALL_EVENTS_HEADER }
+      let(:snippet) { described_class::ALL_EVENTS_SNIPPET }
+
+      it { is_expected.to have_css ".card" }
+      it { is_expected.to have_css ".card.card--no-border" }
+      it { is_expected.to have_css ".card header", text: header }
+      it { is_expected.to have_css "img" }
+      it { is_expected.to have_content snippet }
+      it { is_expected.to have_link "View events", href: url_helpers.events_path }
+    end
   end
 
-  it { is_expected.to have_css ".card" }
-  it { is_expected.to have_css ".card.card--no-border" }
-  it { is_expected.to have_css ".card header", text: event.name }
-  it { is_expected.to have_css "img" }
-  it { is_expected.to have_content event.summary }
+  context "with unknown category" do
+    include_context "stub types api"
 
-  it "includes the footer link" do
-    is_expected.to have_link \
-      "View event",
-      href: url_helpers.event_path(event.readable_id),
-      class: "git-link"
+    let(:card) { { category: "unknown" }.with_indifferent_access }
+
+    it "will raise an error" do
+      expect { subject }.to raise_exception Events::Category::UnknownEventCategory
+    end
+  end
+
+  context "with no category" do
+    include_context "stub types api"
+
+    let(:card) { {} }
+
+    it "will raise an error" do
+      expect { subject }.to raise_exception Events::Category::UnknownEventCategory
+    end
   end
 end
