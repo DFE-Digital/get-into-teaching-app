ad46033599816db6da3ef4c5b55ede3a6575c55c
Peter Yates <peter.yates@graphia.co.uk>
2020-12-10 11:16:59 +0000

    Allow for simple calls to action without a title

--- app/components/calls_to_action/simple_component.rb
+++ app/components/calls_to_action/simple_component.rb
@@ -2,11 +2,13 @@ module CallsToAction
   class SimpleComponent < ViewComponent::Base
     attr_accessor :icon, :title, :text, :link
 
-    def initialize(icon:, title:, text: nil, link_text:, link_target:)
+    def initialize(icon:, title: nil, text: nil, link_text:, link_target:)
       @icon  = icon_element(icon)
       @title = title
       @text  = text
       @link  = link_to(tag.span(link_text), link_target)
+
+      fail(ArgumentError, "a title or text must be present") if title.nil? && text.nil?
     end
 
   private
