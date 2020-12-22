b44fd0232e6e09e33f93f5077a836bf631798a01
Joseph Kempster <jkempster34@gmail.com>
2020-12-22 15:24:16 +0000

    Replace asset_pack_path with image_pack_tag

--- app/components/calls_to_action/chat_online_component.rb
+++ app/components/calls_to_action/chat_online_component.rb
@@ -1,13 +1,11 @@
 module CallsToAction
   class ChatOnlineComponent < ViewComponent::Base
     def icon
-      image_tag(
-        asset_pack_path("media/images/icon-question.svg"),
-        width: 50,
-        height: 50,
-        alt: "Chat with a adviser online",
-        class: "call-to-action__icon",
-      )
+      image_pack_tag("media/images/icon-question.svg",
+                     width: 50,
+                     height: 50,
+                     alt: "Chat with a adviser online",
+                     class: "call-to-action__icon")
     end
   end
 end
