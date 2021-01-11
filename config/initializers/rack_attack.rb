076227aa63723dfc41ac1a7eff5f69591f355298
jebw <jeb@jdwilkins.co.uk>
2021-01-11 16:53:54 +0000

    Added Fail2Ban to trap hostile bots

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,5 +1,15 @@
 module Rack
   class Attack
+    FAIL2BAN_LIST = %w[
+      /etc/passwd
+      wp-admin
+      wp-login
+      wp-includes
+      .php
+    ].freeze
+
+    FAIL2BAN_REGEX = Regexp.union(FAIL2BAN_LIST).freeze
+
     # Throttle /csp_reports requests by IP (5rpm)
     throttle("csp_reports req/ip", limit: 5, period: 1.minute) do |req|
       req.ip if req.path == "/csp_reports"
@@ -43,6 +53,28 @@ module Rack
       end
 
       req.ip if (req.patch? || req.put?) && path_performs_event_sign_up
+    end
+
+    if ENV["FAIL2BAN"].to_s.match? %r{\A\d+\z}
+      FAIL2BAN_TIME = ENV["FAIL2BAN"].to_s.to_i.minutes.freeze
+
+      blocklist("block hostile bots") do |req|
+        Fail2Ban.filter("hostile-bots-#{req.ip}", maxretry: 0, findtime: 1.second, bantime: FAIL2BAN_TIME) do
+          (
+            FAIL2BAN_REGEX.match?(CGI.unescape(req.query_string)) ||
+            FAIL2BAN_REGEX.match?(req.path)
+          ).tap do |should_ban|
+            if should_ban
+              obscured_ip = req.ip.to_s.gsub(%r{\.\d+\.(\d+)\.}, ".***.***.")
+
+              Raven.capture_message <<~BAN_MESSAGE
+                Banning IP: #{obscured_ip} for #{FAIL2BAN_TIME.to_i / 60} minutes
+                accessing #{req.path} with '#{req.query_string}'
+              BAN_MESSAGE
+            end
+          end
+        end
+      end
     end
   end
 
