31cb0370d09174d585637dfcd4062329add4ba20
jebw <jeb@jdwilkins.co.uk>
2021-01-13 11:56:47 +0000

    Added searching keywords for markdown pages

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,7 +1,17 @@
 class Search
-  attr_reader :results
+  attr_reader :search_term
 
-  def initialize(term)
-    @results = term.present? ? [] : nil
+  def initialize(search_term)
+    @search_term = search_term.presence
+  end
+
+  def results
+    return nil unless search_term
+
+    @results ||= Pages::Frontmatter.list.select do |_path, frontmatter|
+      Array.wrap(frontmatter[:keywords]).any? do |keyword|
+        keyword.downcase.starts_with? search_term.downcase
+      end
+    end
   end
 end
--- spec/fixtures/files/markdown_content/third.md
+++ spec/fixtures/files/markdown_content/third.md
@@ -2,6 +2,8 @@
 title: Third
 priority: 30
 section: stories
+keywords:
+  - teaching
 ---
 
 Third page
