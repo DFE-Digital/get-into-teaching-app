0ca67cef9814c8003f620fb46b0f2057e995a127
Peter Yates <peter.yates@graphia.co.uk>
2021-01-26 14:47:35 +0000

    Allow the hero subtitle to be a hyperlink
    This was one of the ideas to increase engagement with the mailing list
    signup. When a `subtitle_link` key is present in the front matter the
    subtitle text is a link rather than regular text. It remains unstyled
    except that on hover it's underlined (via a border to match the title
    above).
    
    When no subtitle is present the text will be displayed without any
    changes.

--- app/components/sections/hero_component.rb
+++ app/components/sections/hero_component.rb
@@ -1,11 +1,12 @@
 module Sections
   class HeroComponent < ViewComponent::Base
-    attr_accessor :title, :image, :mobile_image, :subtitle, :show_mailing_list
+    attr_accessor :title, :image, :mobile_image, :show_mailing_list
 
     def initialize(front_matter)
       front_matter.with_indifferent_access.tap do |fm|
         @title             = fm["title"]
         @subtitle          = fm["subtitle"]
+        @subtitle_link     = fm["subtitle_link"]
         @image             = fm["image"]
         @mobile_image      = fm["mobileimage"]
         @show_mailing_list = fm["mailinglist"]
@@ -25,6 +26,18 @@ module Sections
       image_sizes << %(#{mobile_image_path} 600w) if mobile_image.present?
 
       image_tag(image_path, class: "hero__img", srcset: image_sizes.join(", "), alt: "Student in a classroom")
+    end
+
+    def subtitle
+      if @subtitle_link.present?
+        link_to(@subtitle, @subtitle_link)
+      else
+        @subtitle
+      end
+    end
+
+    def show_subtitle?
+      @subtitle.present?
     end
 
   private
