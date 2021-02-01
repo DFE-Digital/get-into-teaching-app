2997f7b952bb2e0d6d9975fd501d614ed7cdb28f
Peter Yates <peter.yates@graphia.co.uk>
2021-02-01 14:16:25 +0000

    Retire .overhang and make all markdown overhang
    It appeared that all of our articles that were .markdown also had
    .overhang. This can be simplified by moving the .overhang styles into
    .markdown directly and later amending the story layout which wasn't
    overhanging.

--- spec/components/stories/story_component_spec.rb
+++ spec/components/stories/story_component_spec.rb
@@ -44,7 +44,7 @@ describe Stories::StoryComponent, type: "component" do
   end
 
   describe "layout elements" do
-    it { is_expected.to have_css(".container .markdown.overhang.no-hero") }
+    it { is_expected.to have_css(".container .markdown.no-hero") }
     it { is_expected.to have_css(".container .feature") }
   end
 
