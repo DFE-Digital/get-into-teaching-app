0c6c8333993d96f612849d430666dd9d0e87c428
Peter Yates <peter.yates@graphia.co.uk>
2021-02-02 12:32:59 +0000

    Make the chat component text configurable
    The title, text and button text can be set via params, the button action
    cannot (unlike the SimpleComponent) because it's closely integrated with
    a JavaScript controller

--- app/components/calls_to_action/chat_online_component.rb
+++ app/components/calls_to_action/chat_online_component.rb
@@ -1,11 +1,33 @@
 module CallsToAction
   class ChatOnlineComponent < ViewComponent::Base
+    attr_accessor :title, :text, :button_text
+
+    def initialize(title: default_title, text: default_text, button_text: default_button_text)
+      @title       = title
+      @text        = text
+      @button_text = button_text
+    end
+
     def icon
       image_pack_tag("media/images/icon-question.svg",
                      width: 50,
                      height: 50,
                      alt: "Chat with a adviser online",
                      class: "call-to-action__icon")
+    end
+
+  private
+
+    def default_title
+      "Chat to us"
+    end
+
+    def default_text
+      "If you're unsure whether your qualifications are equivalent, you can chat to us."
+    end
+
+    def default_button_text
+      "Chat online"
     end
   end
 end
--- spec/components/calls_to_action/chat_online_component_spec.rb
+++ spec/components/calls_to_action/chat_online_component_spec.rb
@@ -22,4 +22,31 @@ RSpec.describe CallsToAction::ChatOnlineComponent, type: :component do
       text: "Chat online",
     )
   end
+
+  context "when a custom title is passed in" do
+    let(:title_text) { "Converse" }
+    let(:component) { described_class.new(title: title_text) }
+
+    specify "some useful text is included" do
+      expect(page).to have_css(".call-to-action .call-to-action__heading", text: title_text)
+    end
+  end
+
+  context "when custom text is passed in" do
+    let(:content) { "Some helpful text should go here" }
+    let(:component) { described_class.new(text: content) }
+
+    specify "some useful text is included" do
+      expect(page).to have_css(".call-to-action .call-to-action__text", text: content)
+    end
+  end
+
+  context "when custom button text is passed in" do
+    let(:button_text) { "Gossip instantly" }
+    let(:component) { described_class.new(button_text: button_text) }
+
+    specify "some useful text is included" do
+      expect(page).to have_css(".call-to-action .call-to-action__action", text: button_text)
+    end
+  end
 end
