0256ddf4695db58304b34177b6c9eaba98978b3e
Peter Yates <peter.yates@graphia.co.uk>
2021-02-08 16:44:14 +0000

    Add the homepage call to action component
    This CTA differs from the others in that the icon background is at an
    angle and there is an image on the background at the right.

---
+++ spec/components/calls_to_action/homepage_component_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe CallsToAction::HomepageComponent, type: :component do
+  let(:icon) { "icon-question" }
+  let(:image) { "some-image.jpg" }
+  let(:title) { "Joey" }
+  let(:link_text) { "Click here" }
+  let(:link_target) { "/some-dir/some-page" }
+
+  describe "rendering the component" do
+    let(:kwargs) { { icon: icon, title: title, link_text: link_text, link_target: link_target, image: image } }
+
+    let(:component) { described_class.new(kwargs) }
+    before { render_inline(component) }
+
+    specify "renders the call to action" do
+      expect(page).to have_css(".call-to-action")
+    end
+
+    specify "the icon is present" do
+      image_element = page.find(".call-to-action__icon")
+      expect(image_element[:src]).to match(Regexp.new(icon))
+    end
+
+    specify "the title is present" do
+      expect(page).to have_css(".call-to-action__heading", text: title)
+    end
+
+    specify "the link is present" do
+      expect(page).to have_link(link_text, href: link_target)
+    end
+
+    specify "the image is present" do
+      expect(page.find(".call-to-action__image")["style"]).to eql(%(background-image: url('#{image}')))
+    end
+  end
+end
