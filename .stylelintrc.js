e23c2909270b281ab48550a91cba7cca655cf78e
Peter Yates <peter.yates@graphia.co.uk>
2021-02-09 16:35:10 +0000

    Make the purple box button an inline block
    If it's a block it remains too wide and takes up all the horizontal
    space

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,5 +5,6 @@ module.exports = {
     'selector-no-qualifying-type': [true, { ignore: ['class'] }],
     'selector-max-id': 1,
     'declaration-block-single-line-max-declarations': 1,
+    'declaration-no-important': false,
   },
 };
