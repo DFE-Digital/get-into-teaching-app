f3fcf7cf9d6dbb0bf77de247b969ae60cb2c6611
Peter Yates <peter.yates@graphia.co.uk>
2021-02-15 10:12:29 +0000

    Move the state selectors back inside form
    The prefix ampersands were lost during the linting process (a7e7b01409)
    and have been readded. As they're flagged by the linter a new config
    option on the 'selector-no-qualifying-type' rule has been added to make
    it ignore attributes.
    
    https://stylelint.io/user-guide/rules/selector-no-qualifying-type

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: 'stylelint-config-gds/scss',
   rules: {
     'max-nesting-depth': null,
-    'selector-no-qualifying-type': [true, { ignore: ['class'] }],
+    'selector-no-qualifying-type': [true, { ignore: ['class', 'attribute'] }],
     'selector-max-id': 1,
     'declaration-block-single-line-max-declarations': 1,
   },
