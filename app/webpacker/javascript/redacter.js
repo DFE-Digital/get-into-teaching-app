fd5607c8c1b2349f90053ed13b39d8e9d1e1ad5c
jebw <jeb@jdwilkins.co.uk>
2021-02-16 17:53:28 +0000

    Added redacter for search queries

---
+++ app/webpacker/javascript/redacter.js
@@ -0,0 +1,31 @@
+export default class Redacter {
+  static postcodeMatcher = /[A-Z][A-HJ-Y]?\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2}/m
+  static dateMatcher = /\b\d\d?(st|nd|rd|th)?[ .\/-]([a-zA-Z]+|[012]?\d?)[ .\/-]\d\d\d{2}?\b/m
+  static emailMatcher = /[^\s]+@[^\s]+/m
+
+  originalText = null
+
+  constructor(string) {
+    this.originalText = string
+  }
+
+  get redacted() {
+    let redacted = this.redactEmailAddresses(this.originalText)
+    redacted = this.redactPostcodes(redacted)
+    redacted = this.redactDates(redacted)
+
+    return redacted
+  }
+
+  redactEmailAddresses(text) {
+    return text.replace(Redacter.emailMatcher, '[EMAIL]')
+  }
+
+  redactPostcodes(text) {
+    return text.replace(Redacter.postcodeMatcher, '[POSTCODE]')
+  }
+
+  redactDates(text) {
+    return text.replace(Redacter.dateMatcher, '[DATE]')
+  }
+}
