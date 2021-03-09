93cd8f96c8f06c555dea32dc5a05895e0f4cb7b8
Ross Oliver <ross.oliver36@gmail.com>
2021-03-09 13:51:03 +0000

    Update GroupPresenter to handle pagination
    We want to be able to paginate the events sorted by type if they are part of
    search results (as opposed to the upcoming events).
    
    Add `paginated_events_by_type` method that paginates each set of events
    according to the arguments (`pages_by_type` contains the active page for each
    type and `per_page` is how many events per page we want to display).
    
    Add `paginated_events_of_type` convenience method so that we can update
    `EventCategoriesController` to use the presenter for pagination as well.

--- app/presenters/events/group_presenter.rb
+++ app/presenters/events/group_presenter.rb
@@ -1,5 +1,7 @@
 module Events
   class GroupPresenter
+    EVENTS_PER_TYPE = 9
+
     def initialize(events_by_type, display_empty_types = false, ascending = true)
       @display_empty_types = display_empty_types
       @events_by_type = events_by_type
@@ -11,12 +13,37 @@ module Events
         end
     end
 
+    def paginated_events_by_type(pages, per_page = EVENTS_PER_TYPE)
+      sorted_events_by_type.map do |type_id, events|
+        paginated_events = Kaminari
+          .paginate_array(events, total_count: events.count)
+          .page(pages[page_param_name(type_id)])
+          .per(per_page)
+
+        [type_id, paginated_events]
+      end
+    end
+
     def sorted_events_by_type
       populate_events_by_type.sort_by { |k, _| event_type_ids.index(k) }
     end
 
     def sorted_events_of_type(type_id)
-      sorted_events_by_type.first { |v| v.first == type_id }&.last
+      sorted_events_by_type.find { |v| v.first == type_id }&.last
+    end
+
+    def paginated_events_of_type(type_id, page, per_page = EVENTS_PER_TYPE)
+      pages_by_type = { page_param_name(type_id) => page }
+      paginated_events_by_type(pages_by_type, per_page).first { |v| v.first == type_id }&.last
+    end
+
+    def page_param_names
+      event_type_ids.index_with { |type_id| page_param_name(type_id) }
+    end
+
+    def page_param_name(type_id)
+      category_name = GetIntoTeachingApiClient::Constants::EVENT_TYPES.key(type_id)
+      "#{category_name.parameterize(separator: '_')}_page"
     end
 
   private
--- spec/presenters/events/group_presenter_spec.rb
+++ spec/presenters/events/group_presenter_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 describe Events::GroupPresenter do
-  let(:train_to_teach_events) { build_list(:event_api, 3, :train_to_teach_event) }
-  let(:online_events) { build_list(:event_api, 3, :online_event) }
-  let(:school_and_university_events) { build_list(:event_api, 3, :school_or_university_event) }
+  let(:train_to_teach_events) { build_list(:event_api, 5, :train_to_teach_event) }
+  let(:online_events) { build_list(:event_api, 2, :online_event) }
+  let(:school_and_university_events) { build_list(:event_api, 15, :school_or_university_event) }
   let(:all_events) { [train_to_teach_events, online_events, school_and_university_events].flatten }
   let(:events_by_type) { group_events_by_type(all_events) }
   let(:display_empty_types) { false }
@@ -66,8 +66,60 @@ describe Events::GroupPresenter do
     end
   end
 
+  describe "#paginated_events_by_type" do
+    it "paginates the events sorted by type" do
+      pages_by_type = {
+        "train_to_teach_event_page" => nil,
+        "online_event_page" => 1,
+        "school_or_university_event_page" => 2,
+      }
+      paginated_events_by_type = subject.paginated_events_by_type(pages_by_type, 3)
+
+      expect(paginated_events_by_type).to eq([
+        [GetIntoTeachingApiClient::Constants::EVENT_TYPES["Train to Teach event"], train_to_teach_events[0...3]],
+        [GetIntoTeachingApiClient::Constants::EVENT_TYPES["Online event"], online_events[0...2]],
+        [GetIntoTeachingApiClient::Constants::EVENT_TYPES["School or University event"], school_and_university_events[3...6]],
+      ])
+    end
+
+    it "defaults to 9 events per page" do
+      pages_by_type = GetIntoTeachingApiClient::Constants::EVENT_TYPES.values.product([nil]).to_h
+      paginated_events_by_type = subject.paginated_events_by_type(pages_by_type)
+
+      expect(paginated_events_by_type).to include(
+        [GetIntoTeachingApiClient::Constants::EVENT_TYPES["School or University event"], school_and_university_events[0...9]],
+      )
+    end
+  end
+
+  describe "#paginated_events_of_type" do
+    let(:type) { GetIntoTeachingApiClient::Constants::EVENT_TYPES["Train to Teach event"] }
+    let(:page) { 2 }
+    let(:per_page) { 2 }
+
+    it "returns paginated events of the given type" do
+      expect(subject.paginated_events_of_type(type, page, per_page)).to eq(train_to_teach_events[2...4])
+    end
+  end
+
+  describe "#page_param_names" do
+    it "returns a hash of page param names" do
+      expect(subject.page_param_names).to eq({
+        222_750_001 => "train_to_teach_event_page",
+        222_750_008 => "online_event_page",
+        222_750_009 => "school_or_university_event_page",
+      })
+    end
+  end
+
+  describe "#page_param_name" do
+    it { expect(subject.page_param_name(222_750_001)).to eq("train_to_teach_event_page") }
+    it { expect(subject.page_param_name(222_750_008)).to eq("online_event_page") }
+    it { expect(subject.page_param_name(222_750_009)).to eq("school_or_university_event_page") }
+  end
+
   describe "#sorted_events_of_type" do
-    let(:type) { GetIntoTeachingApiClient::Constants::EVENT_TYPES["Train to Teach Event"] }
+    let(:type) { GetIntoTeachingApiClient::Constants::EVENT_TYPES["Train to Teach event"] }
 
     it "returns the events of the given type" do
       expect(subject.sorted_events_of_type(type)).to eq(train_to_teach_events)
