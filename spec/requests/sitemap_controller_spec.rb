01ac62cbdad3c2f4175ef7ed254af17edd5b5c10
Peter Yates <peter.yates@graphia.co.uk>
2021-03-10 16:16:18 +0000

    Rename nodes to markdown_pages in spec for clarity

--- spec/requests/sitemap_controller_spec.rb
+++ spec/requests/sitemap_controller_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe SitemapController do
   subject { Nokogiri::XML(response.body) }
 
-  let(:nodes) do
+  let(:content_pages) do
     {
       "/ways-to-train" => {
         "name" => "Ways to train",
@@ -22,7 +22,7 @@ RSpec.describe SitemapController do
     }
   end
 
-  before { allow(Pages::Frontmatter).to receive(:list) { nodes } }
+  before { allow(Pages::Frontmatter).to receive(:list) { content_pages } }
 
   before { get("/sitemap.xml") }
 
@@ -32,15 +32,15 @@ RSpec.describe SitemapController do
       expect(subject.at_xpath("/xmlns:urlset").namespace.href).to eql(sitemap_namespace)
     end
 
-    describe "nodes" do
-      let(:expected) { SitemapController::OTHER_PATHS.count + nodes.count }
-      let(:paths) { nodes.keys.concat(SitemapController::OTHER_PATHS) }
+    describe "content_pages" do
+      let(:expected) { SitemapController::OTHER_PATHS.count + content_pages.count }
+      let(:paths) { content_pages.keys.concat(SitemapController::OTHER_PATHS) }
 
-      specify "should have the right number of nodes" do
+      specify "should have the right number of content_pages" do
         expect(expected).to eql(subject.xpath("/xmlns:urlset/xmlns:url").size)
       end
 
-      specify "the nodes should have the correct paths" do
+      specify "the content_pages should have the correct paths" do
         expect(
           subject
             .xpath("xmlns:urlset/xmlns:url/xmlns:loc")
@@ -51,7 +51,7 @@ RSpec.describe SitemapController do
 
     describe "priority" do
       specify "priority should be assinged from frontmatter" do
-        nodes.each do |path, data|
+        content_pages.each do |path, data|
           importance = subject.at_xpath(
             %(/xmlns:urlset/xmlns:url[xmlns:loc = 'http://www.example.com#{path}']/xmlns:priority),
           ).text
@@ -63,7 +63,7 @@ RSpec.describe SitemapController do
 
     describe "lastmod" do
       specify "last modified date should be assinged from frontmatter" do
-        nodes.each do |path, data|
+        content_pages.each do |path, data|
           last_modified = subject.at_xpath(
             %(/xmlns:urlset/xmlns:url[xmlns:loc = 'http://www.example.com#{path}']/xmlns:lastmod),
           ).text
