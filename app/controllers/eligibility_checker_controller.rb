3b0a7fcb74e92b73b614cff95d334bb2bb6df89a
Peter Yates <peter.yates@graphia.co.uk>
2021-03-15 13:57:03 +0000

    Add a basic form with subject checkboxes

--- app/controllers/eligibility_checker_controller.rb
+++ app/controllers/eligibility_checker_controller.rb
@@ -1,3 +1,19 @@
 class EligibilityCheckerController < ApplicationController
-  def show; end
+  class Eligibility
+    include ActiveModel::Model
+
+    attr_reader :gcse_english, :gcse_maths, :gcse_science, :degree
+  end
+
+  def show
+    @eligibility = OpenStruct.new(**eligibility_params)
+
+    Rails.logger.debug(@eligibility)
+  end
+
+private
+
+  def eligibility_params
+    params.permit(:gcse_english, :gcse_maths, :gcse_science, :degree).to_hash
+  end
 end
