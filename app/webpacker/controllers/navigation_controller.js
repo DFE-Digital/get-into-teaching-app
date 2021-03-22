cae2f95d243494ce8d7ce0657ee5ef16cf6a317a
jebw <jeb@jdwilkins.co.uk>
2021-03-22 11:30:55 +0000

    Split up show/hide actions for navigation controller

--- app/webpacker/controllers/navigation_controller.js
+++ app/webpacker/controllers/navigation_controller.js
@@ -2,17 +2,33 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
   static targets = ['hamburger', 'label', 'links'];
+  static closedClass = 'navbar__mobile--closed';
 
   connect() {
     this.navToggle();
   }
 
   navToggle() {
-    const hide =
-      this.linksTarget.style.display === 'block' ||
-      this.linksTarget.style.display === '';
-    this.linksTarget.style.display = hide ? 'none' : 'block';
-    this.hamburgerTarget.className = hide ? 'icon-hamburger' : 'icon-close';
-    this.labelTarget.innerHTML = hide ? 'Menu' : 'Close';
+    this.visible ? this.hide() : this.show()
+  }
+
+  show() {
+    this.element.classList.remove(this.constructor.closedClass) ;
+
+    this.linksTarget.style.display = 'block';
+    this.hamburgerTarget.className = 'icon-close';
+    this.labelTarget.innerHTML = 'Close';
+  }
+
+  hide() {
+    this.element.classList.add(this.constructor.closedClass) ;
+
+    this.linksTarget.style.display = 'none';
+    this.hamburgerTarget.className = 'icon-hamburger';
+    this.labelTarget.innerHTML = 'Menu';
+  }
+
+  get visible() {
+    return !this.element.classList.contains(this.constructor.closedClass)
   }
 }
