32bf16ee6a7cffc6b20eba0d16a453446eeb98b0
Peter Yates <peter.yates@graphia.co.uk>
2021-03-26 11:54:14 +0000

    Set @image_description for story cards
    Without an @image_description being set the alt text isn't present, this
    was flagged by our SEO audit.

--- app/components/stories/card_component.rb
+++ app/components/stories/card_component.rb
@@ -5,10 +5,17 @@ module Stories
     def initialize(card:)
       super(card: card)
       @name = card["name"]
+      @image_description = "A photograph of #{image_description_name}"
     end
 
     def link_text
       "Read #{name}'s story"
+    end
+
+  private
+
+    def image_description_name
+      name || "a teacher"
     end
   end
 end
