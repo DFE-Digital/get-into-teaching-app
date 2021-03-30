b3bad07cfbfbc6658cce6366011241056abdd086
Peter Yates <peter.yates@graphia.co.uk>
2021-03-30 13:30:17 +0100

    Add comment explaining why robots has a controller

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,6 +1,10 @@
 class RobotsController < ApplicationController
   layout :none
 
+  # we're serving this from here rather than /public so we can apply any
+  # redirects from beta-getintoteaching.education.gov.uk to
+  # getintoteaching.education.gov.uk - hopefully preventing any search
+  # engines from indexing the site under the beta domain
   def show
     render plain: <<~ROBOTS
       User-agent: *
