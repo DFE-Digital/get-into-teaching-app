ea6e90f83cebeb1e6742647d70e2f5d36da5bfee
Peter Yates <peter.yates@graphia.co.uk>
2021-04-06 09:22:52 +0100

    Remove duplicate describe block

--- spec/requests/beta_redirect_spec.rb
+++ spec/requests/beta_redirect_spec.rb
@@ -3,14 +3,11 @@ require "rails_helper"
 describe "Redirecting beta-getintoteaching.education.gov.uk to getintoteaching.education.gov.uk", type: "request" do
   before { allow(Rails.configuration.x).to receive(:enable_beta_redirects).and_return(true) }
 
-  describe "Redirecting beta-getintoteaching.education.gov.uk to getintoteaching.education.gov.uk", type: :request do
-    before { allow(Rails.configuration.x).to receive(:enable_beta_redirects).and_return(true) }
-    before { host!("beta-getintoteaching.education.gov.uk") }
+  before { host!("beta-getintoteaching.education.gov.uk") }
 
-    it "redirects content pages" do
-      get "/a-very-nice-page?something=value"
+  it "redirects content pages" do
+    get "/a-very-nice-page?something=value"
 
-      expect(response).to redirect_to("http://getintoteaching.education.gov.uk/a-very-nice-page?something=value")
-    end
+    expect(response).to redirect_to("http://getintoteaching.education.gov.uk/a-very-nice-page?something=value")
   end
 end
