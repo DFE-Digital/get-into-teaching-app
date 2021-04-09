f5a04b7b8bcc384c068f4e012c40ff6630768629
Ross Oliver <ross.oliver36@gmail.com>
2021-04-09 10:02:48 +0100

    Make image dimensions explicit
    One of the page speed criteria checks for content shift, which is caused by
    images loading and resulting in other content moving due to not having the
    image size explicitly set.
    
    Set image sizes explicitly to avoid content shift.

--- app/components/content/generic_block_component.rb
+++ app/components/content/generic_block_component.rb
@@ -2,16 +2,17 @@ module Content
   class GenericBlockComponent < ViewComponent::Base
     attr_reader :title, :classes
 
-    def initialize(title:, icon_image:, icon_alt:, classes: [])
+    def initialize(title:, icon_image:, icon_alt:, icon_size: nil, classes: [])
       @title      = title
       @icon_image = icon_image
       @icon_alt   = icon_alt
+      @icon_size  = icon_size
       @classes    = classes
     end
 
     def icon
       tag.div(class: "blocks__icon") do
-        image_pack_tag(@icon_image, alt: @icon_alt)
+        image_pack_tag(@icon_image, alt: @icon_alt, size: @icon_size)
       end
     end
   end
