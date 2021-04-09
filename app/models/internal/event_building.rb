46f2caed25a83c63aa633c4fa71f3e4e3f06f9fa
Joseph Kempster <jkempster34@gmail.com>
2021-04-09 16:04:29 +0100

    Add event building

---
+++ app/models/internal/event_building.rb
@@ -0,0 +1,30 @@
+module Internal
+  class EventBuilding
+    include ActiveModel::Model
+    include ActiveModel::Attributes
+
+    attribute :id, :string
+    attribute :venue, :string
+    attribute :address_line1, :string
+    attribute :address_line2, :string
+    attribute :address_line3, :string
+    attribute :address_city, :string
+    attribute :address_postcode, :string
+
+    validates :venue, presence: true, allow_blank: false
+    validates :address_postcode, presence: true, postcode: true, allow_blank: false
+
+    def self.initialize_with_api_building(building)
+      hash = building.to_hash.transform_keys { |k| k.to_s.underscore }.filter { |k| attribute_names.include?(k) }
+      new(hash)
+    end
+
+    def map_to_api_building
+      hash = attributes
+               .filter { |k| attribute_names.include?(k) }
+               .transform_keys { |k| k.to_s.camelize(:lower) }
+               .filter { |_, v| v.presence }
+      GetIntoTeachingApiClient::TeachingEventBuilding.new(hash)
+    end
+  end
+end
