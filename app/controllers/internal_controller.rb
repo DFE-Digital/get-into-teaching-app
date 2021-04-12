4130d5c49e745aeec2ecd55a1043c7e7b75f61a0
Joseph Kempster <jkempster34@gmail.com>
2021-04-12 10:22:44 +0100

    Update authenticate method
    Co-authored-by: Peter Yates <peter.yates@graphia.co.uk>

--- app/controllers/internal_controller.rb
+++ app/controllers/internal_controller.rb
@@ -15,16 +15,14 @@ private
   end
 
   def authenticate
-    authenticated = true
     authenticate_or_request_with_http_basic do |username, password|
       if username == ENV["PUBLISHER_USERNAME"] && password == ENV["PUBLISHER_PASSWORD"]
         set_account_role(:publisher)
       elsif username == ENV["AUTHOR_USERNAME"] && password == ENV["AUTHOR_PASSWORD"]
         set_account_role(:author)
-      else
-        authenticated = false
       end
-      authenticated
+
+      session[:role].present?
     end
   end
 end
