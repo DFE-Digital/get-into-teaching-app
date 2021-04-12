7e01bc004b03ea5d99b15c72fa0ae6140c61452d
Ross Oliver <ross.oliver36@gmail.com>
2021-04-12 11:37:12 +0100

    Make lazy images more robust
    Add polyfill for `forEach` in IE11, which is breaking other parts of the
    website as well at the moment.
    
    Move lazy image JS into its own pack, so that if an error in the application.js
    exists it will never stop the images from loading.

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,9 +1,9 @@
 import '@stimulus/polyfills';
 import '../styles/application.scss';
+import 'polyfills/ie11-foreach.js';
 import 'polyfills/ie8.js';
 import 'polyfills/objectFitPolyfill.basic.min.js';
 import 'javascript/object_fit.js';
-import 'javascript/lazy_images.js';
 import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
 
