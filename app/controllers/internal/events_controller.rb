894a6184b308d1dbdd83afefdcd23a04661b9c4e
Joseph Kempster <jkempster34@gmail.com>
2021-04-13 13:43:45 +0100

    Add controller actions and utilities for create/update

--- app/controllers/internal/events_controller.rb
+++ app/controllers/internal/events_controller.rb
@@ -1,6 +1,7 @@
 module Internal
   class EventsController < ::InternalController
     layout "internal"
+    before_action :load_buildings, only: %i[new edit create]
     before_action :load_pending_events, only: %i[index]
 
     def index
@@ -14,7 +15,74 @@ module Internal
       @page_title = @event.name
     end
 
+    def new
+      @event = Event.new(venue_type: Event::VENUE_TYPES[:existing])
+      @event.building = EventBuilding.new
+    end
+
+    def approve
+      api_event = GetIntoTeachingApiClient::TeachingEventsApi.new.get_teaching_event(params[:format])
+      api_event.status_id = GetIntoTeachingApiClient::Constants::EVENT_STATUS["Open"]
+      submit_to_api(api_event)
+      redirect_to internal_events_path(success: true)
+    end
+
+    def create
+      @event = Event.new(event_params)
+      @event.building = format_building(event_params)
+      if submit_pending
+        redirect_to internal_events_path(success: :pending)
+      else
+        render action: :new
+      end
+    end
+
+    def edit
+      api_event = GetIntoTeachingApiClient::TeachingEventsApi.new.get_teaching_event(params[:id])
+      @event = Event.initialize_with_api_event(api_event)
+      render :new
+    end
+
   private
+
+    def submit_pending
+      return if invalid?
+
+      @event.status_id = GetIntoTeachingApiClient::Constants::EVENT_STATUS["Pending"]
+      api_event = @event.map_to_api_event
+      submit_to_api(api_event)
+    end
+
+    def invalid?
+      invalid_building = @event.building.nil? ? false : @event.building.invalid?
+      (@event.invalid? || invalid_building)
+    end
+
+    def submit_to_api(body)
+      GetIntoTeachingApiClient::TeachingEventsApi.new.upsert_teaching_event(body)
+      true
+    rescue GetIntoTeachingApiClient::ApiError => e
+      @event.map_api_errors_to_attributes(e) if e.code == 400
+      false
+    end
+
+    def format_building(event_params)
+      if event_params[:venue_type] == Event::VENUE_TYPES[:existing] && event_params[:building][:id].present?
+        api_building = @buildings.find { |b| b.id == event_params[:building][:id] }
+        return EventBuilding.initialize_with_api_building(api_building)
+      end
+      if event_params[:venue_type] == Event::VENUE_TYPES[:add]
+        building = event_params[:building].to_hash
+        building[:id] = nil # Id may be present on hidden field from previous selection
+        return EventBuilding.new(building)
+      end
+      nil
+    end
+
+    def load_buildings
+      @buildings = GetIntoTeachingApiClient::TeachingEventBuildingsApi
+                     .new.get_teaching_event_buildings
+    end
 
     def load_pending_events
       search_results = GetIntoTeachingApiClient::TeachingEventsApi
@@ -36,6 +104,33 @@ module Internal
 
     def pending_event_status_id
       GetIntoTeachingApiClient::Constants::EVENT_STATUS["Pending"]
+    end
+
+    def event_params
+      params.require(:internal_event).permit(
+        :id,
+        :name,
+        :readable_id,
+        :summary,
+        :description,
+        :is_online,
+        :start_at,
+        :end_at,
+        :provider_contact_email,
+        :provider_organiser,
+        :provider_target_audience,
+        :provider_website_url,
+        :venue_type,
+        building: %i[
+          id
+          venue
+          address_line1
+          address_line2
+          address_line3
+          address_city
+          address_postcode
+        ],
+      )
     end
   end
 end
