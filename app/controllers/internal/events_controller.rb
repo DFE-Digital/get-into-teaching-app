613acc89547dadd7934896ffcf17ae5ea931ad57
Joseph Kempster <jkempster34@gmail.com>
2021-04-13 09:59:31 +0100

    Add controller actions

--- app/controllers/internal/events_controller.rb
+++ app/controllers/internal/events_controller.rb
@@ -1,8 +1,40 @@
 module Internal
   class EventsController < ::InternalController
+    layout "internal"
+
     def index
-      # stubbed for now
-      head :ok
+      @no_results = load_pending_events.blank?
+    end
+
+    def show
+      @event = GetIntoTeachingApiClient::TeachingEventsApi.new.get_teaching_event(params[:id])
+      raise_not_found unless @event.status_id == pending_event_status_id
+
+      @page_title = @event.name
+    end
+
+  private
+
+    def load_pending_events
+      opts = {
+        type_id: GetIntoTeachingApiClient::Constants::EVENT_TYPES["School or University event"],
+        status_ids: [pending_event_status_id],
+        quantity_per_type: 1_000,
+      }
+
+      search_results = GetIntoTeachingApiClient::TeachingEventsApi
+                         .new
+                         .search_teaching_events_grouped_by_type(opts)
+
+      @group_presenter = Events::GroupPresenter.new(search_results)
+      @events = @group_presenter.paginated_events_of_type(
+        GetIntoTeachingApiClient::Constants::EVENT_TYPES["School or University event"],
+        params[:page],
+      )
+    end
+
+    def pending_event_status_id
+      GetIntoTeachingApiClient::Constants::EVENT_STATUS["Pending"]
     end
   end
 end
