c628e1dcbedc9407ae8176aeb16e94373915bc32
Peter Yates <peter.yates@graphia.co.uk>
2021-04-15 09:09:52 +0100

    Add some specs covering abbreviation replacement

--- app/webpacker/controllers/abbreviation_controller.js
+++ app/webpacker/controllers/abbreviation_controller.js
@@ -9,12 +9,12 @@ export default class extends Controller {
     const abbrs = this.element.querySelectorAll('abbr');
 
     const replace = (event) => {
-      // get the abbr element and pull it's name from title
+      // get the abbr element and pull its name from title
       const abbr = event.currentTarget;
       const name = abbr.getAttribute('title');
 
       // create a span with appropriate class that we can use
-      // to replce the <abbr>. This would be easier with jQuery 😢
+      // to replce the <abbr>. This would be easier with jQuery
       const replacementSpan = document.createElement('span');
       const replacementText = document.createTextNode(name);
       replacementSpan.appendChild(replacementText);
