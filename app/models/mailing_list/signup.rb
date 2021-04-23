812fb070214ec336dc559818c4d0c35bc564d192
Peter Yates <peter.yates@graphia.co.uk>
2021-04-23 14:33:32 +0100

    Make mailing list dropdowns show real values

--- app/models/mailing_list/signup.rb
+++ app/models/mailing_list/signup.rb
@@ -13,5 +13,33 @@ module MailingList
                   :email,
                   :channel_id,
                   :degree_status_id
+
+    def degree_status_options
+      @degree_status_options ||= query_degree_status
+    end
+
+    def teaching_subjects
+      @teaching_subjects ||= [OpenStruct.new(id: nil, value: "Please select")] + query_teaching_subjects
+    end
+
+    def consideration_journey_stages
+      @consideration_journey_stages ||= query_consideration_journey_stages
+    end
+
+  private
+
+    def query_degree_status
+      GetIntoTeachingApiClient::PickListItemsApi.new.get_qualification_degree_status
+    end
+
+    def query_teaching_subjects
+      GetIntoTeachingApiClient::LookupItemsApi.new.get_teaching_subjects.reject do |type|
+        GetIntoTeachingApiClient::Constants::IGNORED_PREFERRED_TEACHING_SUBJECTS.values.include?(type.id)
+      end
+    end
+
+    def query_consideration_journey_stages
+      GetIntoTeachingApiClient::PickListItemsApi.new.get_candidate_journey_stages
+    end
   end
 end
