2d0fc4d3c6a5fba9bcc3ef09a0feeefd59ed0e5e
Leonidas Apostolidis <leoapost@gmail.com>
2021-05-04 14:45:29 +0100

    Move inline js to /javascripts

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -6,6 +6,7 @@ import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
 
 import 'controllers';
+import 'javascript/js_enabled';
 
 require.context('../fonts', true);
 require.context('../images', true);
