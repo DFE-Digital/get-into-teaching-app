b0cf55a416291fc2161aaf6347ff8f40175eed66
Ross Oliver <ross.oliver36@gmail.com>
2021-06-09 10:18:25 +0100

    Update search with ranking/levenshtein comparisons
    In order to improve the search we want to rank the results (more keyword
    matches resulting in a higher-ranked page), make use the Levenshtein comparison
    to reduce spelling errors impacting the search results and remove stopwords to
    reduce noise.
    
    The search algorithm now consists of:
    
    - Tokenize the search term, keywords and page title (removing stopwords, downcase)
    - For each page
      - Count keyword matches and multiply by 2 to add weight
      - Count page title word matches
      - Sum to give page rank
    - Remove unranked pages (no matches)
    - Sort pages by rank, descending
    
    A 'match' is defined as:
    
    - The word starts with one of the search tokens
    - The word is > 5 characters and Levenshtein distance is <= 1 to a search token
    
    The original search tests have been left as unchanged as possible to show that
    the new search works with all the original test cases. Tests for the new search
    logic will be in a follow up commit.

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -10,32 +10,32 @@ RSpec.describe Search do
       let(:search) { "returners" }
 
       it { is_expected.to have_attributes length: 1 }
-      it { is_expected.to include "/first" }
+      it { is_expected.to include(a_hash_including(path: "/first")) }
     end
 
     context "with keyword matching multiple results" do
       let(:search) { "teaching" }
 
       it { is_expected.to have_attributes length: 2 }
-      it { is_expected.to include "/second" }
-      it { is_expected.to include "/third" }
+      it { is_expected.to include(a_hash_including(path: "/second")) }
+      it { is_expected.to include(a_hash_including(path: "/third")) }
     end
 
     context "with partial keyword match" do
       let(:search) { "teach" }
 
       it { is_expected.to have_attributes length: 3 }
-      it { is_expected.to include "/first" }
-      it { is_expected.to include "/second" }
-      it { is_expected.to include "/third" }
+      it { is_expected.to include(a_hash_including(path: "/first")) }
+      it { is_expected.to include(a_hash_including(path: "/second")) }
+      it { is_expected.to include(a_hash_including(path: "/third")) }
     end
 
     context "with mismatched case" do
       let(:search) { "tEaCh" }
       it { is_expected.to have_attributes length: 3 }
-      it { is_expected.to include "/first" }
-      it { is_expected.to include "/second" }
-      it { is_expected.to include "/third" }
+      it { is_expected.to include(a_hash_including(path: "/first")) }
+      it { is_expected.to include(a_hash_including(path: "/second")) }
+      it { is_expected.to include(a_hash_including(path: "/third")) }
     end
 
     context "with keyword matching no results" do
@@ -59,22 +59,22 @@ RSpec.describe Search do
     context "with numeric keyword" do
       let(:search) { "2000" }
 
-      it { is_expected.to include("/second") }
+      it { is_expected.to include(a_hash_including(path: "/second")) }
     end
 
     context "with search term matching partially page title" do
       let(:search) { "sec" }
 
       it { is_expected.to have_attributes length: 1 }
-      it { is_expected.to include "/second" }
+      it { is_expected.to include(a_hash_including(path: "/second")) }
     end
 
     context "with search term matching word later in title" do
       let(:search) { "wor" }
 
       it { is_expected.to have_attributes length: 2 }
-      it { is_expected.to include "/page1" }
-      it { is_expected.to include "/subfolder/page2" }
+      it { is_expected.to include(a_hash_including(path: "/page1")) }
+      it { is_expected.to include(a_hash_including(path: "/subfolder/page2")) }
     end
 
     context "with search term starting in the middle of a word" do
@@ -96,7 +96,7 @@ RSpec.describe Search do
             let(:search) { kw }
 
             it "finds the events page when searching for #{kw}" do
-              expect(subject).to include "/events"
+              expect(subject).to include(a_hash_including(path: "/events"))
             end
           end
         end
