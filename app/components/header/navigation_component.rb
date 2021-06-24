60d6c98931497defd81ce93641a7c4a7c7c42108
Peter Yates <peter.yates@graphia.co.uk>
2021-06-24 10:45:37 +0100

    Reintroduce the chevrons and improve mobile styles

--- app/components/header/navigation_component.rb
+++ app/components/header/navigation_component.rb
@@ -26,7 +26,7 @@ module Header
     def menu(link_text, link_path, children)
       heading_args = { class: "menu__heading", data: { action: "click->navigation#toggleMenu" } }
 
-      link_content = safe_join([link_text, tag.div(class: "menu-chevron")])
+      link_content = safe_join([tag.span(link_text, class: "menu__text"), tag.div(class: "menu__chevron")])
 
       tag.li(class: ["menu", class_name(link_path)]) do
         safe_join(
--- app/webpacker/controllers/navigation_controller.js
+++ app/webpacker/controllers/navigation_controller.js
@@ -10,8 +10,8 @@ export default class extends Controller {
     event.preventDefault();
     event.stopPropagation();
 
-    const toggle = event.target.parentElement;
-    const secondary = event.target.parentElement.querySelector('ol');
+    const toggle = event.target.closest('li');
+    const secondary = event.target.closest('li').querySelector('ol.secondary')
 
     if (secondary.classList.contains(this.menuHiddenClass)) {
       this.expandMenu(secondary, toggle);
