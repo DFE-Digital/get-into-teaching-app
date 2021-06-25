2d0961516b09554a493c991f6680423fb420af68
Peter Yates <peter.yates@graphia.co.uk>
2021-06-25 14:03:27 +0100

    Add specs for the logo component

---
+++ spec/components/header/logo_component_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe Header::LogoComponent, type: "component" do
+  let(:component) { described_class.new }
+  subject! { render_inline(component) }
+
+  specify "renders the logo wrapper with the get into teaching and DfE logos" do
+    expect(page).to have_css(".logo-wrapper") do |wrapper|
+      expect(wrapper).to have_css(".logo")
+      expect(wrapper).to have_css(".dfe-logo")
+    end
+  end
+end
