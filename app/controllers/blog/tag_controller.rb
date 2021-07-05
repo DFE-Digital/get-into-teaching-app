c29a38e7db5025315d33712e550abd2588cc1290
Peter Yates <peter.yates@graphia.co.uk>
2021-07-05 11:28:55 +0100

    Add basic listing functionality for blog tags

--- app/controllers/blog/tag_controller.rb
+++ app/controllers/blog/tag_controller.rb
@@ -1,24 +1,23 @@
-class BlogController < ApplicationController
+class Blog::TagController < ApplicationController
   include StaticPages
   around_action :cache_static_page, only: %i[show]
 
   layout "layouts/blog/index"
 
-  def index
-    @front_matter = { title: "Get Into Teaching Blog" }
+  def show
+    breadcrumb "Blog", blog_index_path
+
+    @front_matter = {
+      title: "Blog posts about #{params[:id]}",
+    }
+
+    @tag = params[:id]
 
     @posts = Pages::Frontmatter
       .select_by_path("/blog")
       .select { |_path, fm| fm[:date] <= Time.zone.today.iso8601 }
+      .select { |_path, fm| @tag.in?(fm[:tags]) }
       .sort_by { |_path, fm| fm[:date] }
       .reverse
-  end
-
-  def show
-    breadcrumb "Blog", blog_index_path
-
-    @page = Pages::Page.find(request.path)
-
-    render template: @page.template, layout: "layouts/blog/post"
   end
 end
