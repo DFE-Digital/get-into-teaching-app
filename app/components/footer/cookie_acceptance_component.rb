56862bf656acc55d9b059209f0ef0e454d3082e5
Peter Yates <peter.yates@graphia.co.uk>
2021-08-03 11:13:36 +0100

    Hide the cookie popup for Silktide tests
    Our score is artificially low because the overlay rendered with our
    cookie popup affects the bot's ability to focus and click on certain
    elements.

--- app/components/footer/cookie_acceptance_component.rb
+++ app/components/footer/cookie_acceptance_component.rb
@@ -1,3 +1,16 @@
 module Footer
-  class CookieAcceptanceComponent < ViewComponent::Base; end
+  class CookieAcceptanceComponent < ViewComponent::Base
+    def render?
+      return true if request.user_agent.blank?
+
+      # we're being penalised by Silktide's accessibility checker because
+      # our cookie banner is blocking the screen. Silktide's user agents
+      # all have the suffix Silktide, so for them simply don't show the
+      # cookie acceptance popup.
+      #
+      # https://support.silktide.com/guides/ip-address-user-agent-silktide-use/
+
+      !request.user_agent.end_with?("Silktide")
+    end
+  end
 end
