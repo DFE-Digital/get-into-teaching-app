4fa56f86c5dd1566a6a7914e7a08d4af3b4af682
Peter Yates <peter.yates@graphia.co.uk>
2021-08-06 14:05:43 +0100

    Move the overseas AO group description inside box
    This works in a generic fashion by adding a new namespace called
    'providers' within 'provider_groups', allowing us to insert a sibling
    'description' that can point to any arbitrary partial.
    
    The partial's contents will be rendered between the heading and the list
    of cards.

--- spec/components/grouped_cards/listing_component_spec.rb
+++ spec/components/grouped_cards/listing_component_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 describe GroupedCards::ListingComponent, type: "component" do
   subject { render_inline(instance) && page }
+  let(:custom_description) { "a very nice card indeed" }
 
   let(:instance) { described_class.new data }
   let(:organisation) do
@@ -14,8 +15,11 @@ describe GroupedCards::ListingComponent, type: "component" do
   end
   let :data do
     {
-      "Region 1" => [organisation, organisation],
-      "Region 2" => [organisation],
+      "Region 1" => { "providers" => [organisation, organisation] },
+      "Region 2" => {
+        "providers" => [organisation],
+        "description" => { plain: custom_description },
+      },
     }
   end
 
@@ -27,4 +31,6 @@ describe GroupedCards::ListingComponent, type: "component" do
   it { is_expected.to have_css "h3", text: "Region 2" }
 
   it { is_expected.to have_css ".group__card", count: 3 }
+
+  it { is_expected.to have_css(".group__description", text: custom_description) }
 end
