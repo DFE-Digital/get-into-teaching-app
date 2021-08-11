079a451b759462a36f2042ebddfcde66daba969d
Peter Yates <peter.yates@graphia.co.uk>
2021-08-11 16:42:35 +0100

    Make the blog title display correctly
    Previously it wasn't displaying at all because front matter expects a
    string key rather than a symbol. Secondly, 'Get Into Teaching' would be
    repeated ("Get Into Teaching: Get Into Teaching Blog"), so just call it
    blog.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -5,7 +5,7 @@ class BlogController < ApplicationController
   layout "layouts/blog/index"
 
   def index
-    @front_matter = { title: "Get Into Teaching Blog" }
+    @front_matter = { "title" => "Blog" }
 
     @posts = Pages::Blog.posts
   end
