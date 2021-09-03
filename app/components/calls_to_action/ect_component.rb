8143b96afc08e7237028daaa39047a357e05a3c1
Peter Yates <peter.yates@graphia.co.uk>
2021-09-03 12:55:41 +0100

    Add basic ECT component
    It doesn't take any args but the content is repeated in several places
    so makes sense to componentise

---
+++ app/components/calls_to_action/ect_component.rb
@@ -0,0 +1,11 @@
+module CallsToAction
+  class EctComponent < ViewComponent::Base
+    def icon
+      image_pack_tag("media/images/icon-school-black.svg",
+                     width: 50,
+                     height: 50,
+                     alt: "Mortar board icon",
+                     class: "call-to-action__icon")
+    end
+  end
+end
