7fd649aa74b490e38e78aacbf083782bcd9d0c89
Peter Yates <peter.yates@graphia.co.uk>
2021-09-27 14:13:56 +0100

    Replace mailing list completion page
    The new content is less wordy and will provide a CTA that takes users to
    the welcome guide.
    
    As a stopgap, before the welcome guide is complete, I've created a
    fallback where the old block (purple boxes) content is present in
    production. This will give users a path to follow before the welcome
    guide is ready to be released.

--- spec/features/mailing_list_wizard_spec.rb
+++ spec/features/mailing_list_wizard_spec.rb
@@ -45,7 +45,7 @@ RSpec.feature "Mailing list wizard", type: :feature do
     click_on "Complete sign up"
 
     expect(page).to have_title("You've signed up | Get Into Teaching")
-    expect(page).to have_text "You've signed up"
+    expect(page).to have_text "you're signed up"
   end
 
   scenario "Full journey as an on-campus candidate" do
@@ -84,7 +84,7 @@ RSpec.feature "Mailing list wizard", type: :feature do
     check "Yes"
     click_on "Complete sign up"
 
-    expect(page).to have_text "You've signed up"
+    expect(page).to have_text "you're signed up"
   end
 
   scenario "Full journey as an existing candidate" do
@@ -138,7 +138,7 @@ RSpec.feature "Mailing list wizard", type: :feature do
     check "Yes"
     click_on "Complete sign up"
 
-    expect(page).to have_text "You've signed up"
+    expect(page).to have_text "you're signed up"
   end
 
   scenario "Full journey as an existing candidate that resends the verification code" do
@@ -259,7 +259,7 @@ RSpec.feature "Mailing list wizard", type: :feature do
     check "Yes"
     click_on "Complete sign up"
 
-    expect(page).to have_text "You've signed up"
+    expect(page).to have_text "you're signed up"
   end
 
   scenario "Invalid magic link tokens" do
