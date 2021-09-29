1aca4c384081a443a041a93e3c8c82060e3c7869
Peter Yates <peter.yates@graphia.co.uk>
2021-09-29 11:18:45 +0100

    Move mailing list completed content to component
    Now it has some basic logic in this makes it easier to test

---
+++ app/components/registration/mailing_list_signup_confirmation_component.rb
@@ -0,0 +1,15 @@
+class Registration::MailingListSignupConfirmationComponent < ViewComponent::Base
+  attr_reader :mailing_list_session
+
+  def initialize(mailing_list_session)
+    @first_name = mailing_list_session["first_name"]
+  end
+
+  def heading_text
+    if @first_name.present?
+      %(#{@first_name}, you're signed up)
+    else
+      %(You've signed up)
+    end
+  end
+end
