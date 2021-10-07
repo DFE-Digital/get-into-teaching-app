f4d523f68a2622dde480fc3206f0089dab110f2a
Joseph Kempster <jkempster34@gmail.com>
2021-10-07 17:02:59 +0100

    Increase wait time for the Zendesk widget to animate in
    On first page load, the widget isn't focusable after 500ms, increase this to 1000ms.

--- app/webpacker/controllers/talk-to-us_controller.js
+++ app/webpacker/controllers/talk-to-us_controller.js
@@ -54,7 +54,7 @@ export default class extends Controller {
       setTimeout(() => {
         this.buttonTarget.querySelector('span').innerHTML = originalText;
         document.getElementById('webWidget').focus();
-      }, 500); // Small delay to account for the chat box animating in.
+      }, 1000); // Small delay to account for the chat box animating in.
     });
   }
 
