a81d7306fba589fc04a4d4d320b3b4932ad2190c
Peter Yates <peter.yates@graphia.co.uk>
2021-10-19 17:12:29 +0100

    Move generic content to a constant

--- app/helpers/welcome_content_helper.rb
+++ app/helpers/welcome_content_helper.rb
@@ -27,6 +27,13 @@ module WelcomeContentHelper
     text: "Meet Tom - find out how he got into teaching Spanish and what he's looking forward to next.",
   }.freeze
 
+  GENERIC = {
+    name: "Helen",
+    subject: nil,
+    image: "dimitra-desert-crop.jpg",
+    text: "Meet Helen - find out how she went from trainee to Assistant Headteacher and what she's looking forward to next.",
+  }.freeze
+
   def subject_specific_args
     {
       "a42655a1-2afa-e811-a981-000d3a276620" => MATHS,
@@ -42,13 +49,6 @@ module WelcomeContentHelper
       "9c2655a1-2afa-e811-a981-000d3a276620" => MFL,       # german
       "b82655a1-2afa-e811-a981-000d3a276620" => MFL,       # spanish
       "a22655a1-2afa-e811-a981-000d3a276620" => MFL,       # languages (other)
-    }.fetch(welcome_guide_subject_id) do
-      {
-        name: "Helen",
-        subject: nil,
-        image: "dimitra-desert-crop.jpg",
-        text: "Meet Helen - find out how she went from trainee to Assistant Headteacher and what she's looking forward to next.",
-      }
-    end
+    }.fetch(welcome_guide_subject_id) { GENERIC }
   end
 end
