0c269394a040683cff161fa262d5aece068ac0c1
Ross Oliver <ross.oliver36@gmail.com>
2021-10-28 15:04:04 +0100

    Fix tab behaviour with cookie dialog
    The cookie dialog being within the page but hidden causes some issues with
    tabbing behaviour on the page. When tabbing down the page it enters the
    hidden cookie dialog elements before proceeding to the footer. In addition,
    after accepting cookies the tabbing sequence remains within the dialog and
    the rest of the page can't be tabbed through.
    
    To fix this, the `tabindex` of the cookie dialog elements are initially set
    to `-1`, which causes the browser to skip over them during regular tabbing.
    
    When the cookie dialog opens we set the `tabindex` of elements to ensure the
    first tab goes to the accept button. We listen for `keydown` events to ensure
    the tab sequence goes through the dialog elements only whilst it is open.
    
    When the dialog is closed we remove the `keydown` listeners to revert tabbing
    back to the main page and focus/blur on the first anchor link to ensure the
    tab sequence starts at the top of the page instead of the footer (which is
    where the cookie acceptance HTML is located).
    
    Lastly, we set the `tabindex` of the cookie dialog elements back to `-1` so
    they are skipped over on any subsequent tabbing.

--- app/webpacker/controllers/cookie-acceptance_controller.js
+++ app/webpacker/controllers/cookie-acceptance_controller.js
@@ -19,60 +19,90 @@ export default class extends Controller {
 
   accept(event) {
     event.preventDefault();
-    this.overlayTarget.classList.remove('visible');
 
     const cookiePrefs = new CookiePreferences();
     cookiePrefs.allowAll();
+
+    this.overlayTarget.classList.remove('visible');
+    this.stopInterceptingTabKeydown();
+    this.resetFocus();
   }
 
   showDialog() {
     this.overlayTarget.classList.add('visible');
+    this.startInterceptingTabKeydown();
+  }
 
+  resetFocus() {
+    // focus then blur on the first link to prevent
+    // the next element being tabbed to after accepting
+    // being in the footer (which is where the cookie acceptance
+    // HTML is placed)
+    document.querySelector('a').focus();
+    document.querySelector('a').blur();
+  }
+
+  startInterceptingTabKeydown() {
+    // by default these have a -1 tab index which
+    // prevents the keydown event triggering on tab
+    this.agreeTarget.tabIndex = 1;
+    this.infoTarget.tabIndex = 2;
+    this.disagreeTarget.tabIndex = 3;
+
+    this.keydownHandler = this.handleKeydown.bind(this);
+    this.agreeTarget.addEventListener('keydown', this.keydownHandler);
+    this.infoTarget.addEventListener('keydown', this.keydownHandler);
+    this.disagreeTarget.addEventListener('keydown', this.keydownHandler);
+  }
+
+  stopInterceptingTabKeydown() {
+    this.agreeTarget.removeEventListener('keydown', this.keydownHandler);
+    this.infoTarget.removeEventListener('keydown', this.keydownHandler);
+    this.disagreeTarget.removeEventListener('keydown', this.keydownHandler);
+
+    // revert to -1 to ensure tabbing ignores these elements
+    this.agreeTarget.tabIndex = -1;
+    this.infoTarget.tabIndex = -1;
+    this.disagreeTarget.tabIndex = -1;
+  }
+
+  // agree button is focussed on the first tab
+  // cookies (link) on the second
+  // disagree button on the third
+  // back to agree on the fourth, and so on
+  handleKeydown(e) {
     const tabKey = 9;
-    const agreeButtonID = 'biscuits-agree';
-    const disagreeButtonID = 'cookies-disagree';
-    const infoLinkID = 'cookies-info';
 
-    // agree button is focussed on the first tab
-    // cookies (link) on the second
-    // disagree button on the third
-    // back to agree on the fourth, and so on
+    if (e.keyCode === tabKey) {
+      e.preventDefault();
+      e.shiftKey ? this.focusOnPrevious(e) : this.focusOnNext(e);
+    }
+  }
 
-    this.agreeTarget.addEventListener('keydown', function (e) {
-      if (e.keyCode === tabKey) {
-        e.preventDefault();
+  focusOnNext(e) {
+    switch (e.target) {
+      case this.agreeTarget:
+        this.infoTarget.focus();
+        break;
+      case this.infoTarget:
+        this.disagreeTarget.focus();
+        break;
+      default:
+        this.agreeTarget.focus();
+    }
+  }
 
-        if (e.shiftKey) {
-          document.getElementById(disagreeButtonID).focus();
-        } else {
-          document.getElementById(infoLinkID).focus();
-        }
-      }
-    });
-
-    this.infoTarget.addEventListener('keydown', function (e) {
-      if (e.keyCode === tabKey) {
-        e.preventDefault();
-
-        if (e.shiftKey) {
-          document.getElementById(agreeButtonID).focus();
-        } else {
-          document.getElementById(disagreeButtonID).focus();
-        }
-      }
-    });
-
-    this.disagreeTarget.addEventListener('keydown', function (e) {
-      if (e.keyCode === tabKey) {
-        e.preventDefault();
-
-        if (e.shiftKey) {
-          document.getElementById(infoLinkID).focus();
-        } else {
-          document.getElementById(agreeButtonID).focus();
-        }
-      }
-    });
+  focusOnPrevious(e) {
+    switch (e.target) {
+      case this.agreeTarget:
+        this.disagreeTarget.focus();
+        break;
+      case this.disagreeTarget:
+        this.infoTarget.focus();
+        break;
+      default:
+        this.agreeTarget.focus();
+    }
   }
 
   isPrivacyPage() {
