4a2a01ead1deb452c4fc8e3ae4ff0cb6e1878fc5
Steven Fawcett <steve.fawcett@capgemini.com>
2021-11-01 17:07:22 +0000

    Terraform updated
    Terraform updated to use the compound key files

--- terraform/paas/data.tf
+++ terraform/paas/data.tf
@@ -8,36 +8,13 @@ data "azurerm_key_vault_secret" "application" {
   name         = var.azure_vault_secret
 }
 
+data "azurerm_key_vault_secret" "infrastructure" {
+  key_vault_id = data.azurerm_key_vault.vault.id
+  name         = "INFRA-KEYS"
+}
+
 locals {
-  application_secrets = yamldecode(data.azurerm_key_vault_secret.application.value)
+  application_secrets    = yamldecode(data.azurerm_key_vault_secret.application.value)
+  infrastructure_secrets = yamldecode(data.azurerm_key_vault_secret.infrastructure.value)
 }
 
-data "azurerm_key_vault_secret" "paas_username" {
-  key_vault_id = data.azurerm_key_vault.vault.id
-  name         = "PAAS-USERNAME"
-}
-
-data "azurerm_key_vault_secret" "paas_password" {
-  key_vault_id = data.azurerm_key_vault.vault.id
-  name         = "PAAS-PASSWORD"
-}
-
-data "azurerm_key_vault_secret" "statuscake_username" {
-  key_vault_id = data.azurerm_key_vault.vault.id
-  name         = "SC-USERNAME"
-}
-
-data "azurerm_key_vault_secret" "statuscake_password" {
-  key_vault_id = data.azurerm_key_vault.vault.id
-  name         = "SC-PASSWORD"
-}
-
-data "azurerm_key_vault_secret" "http_username" {
-  key_vault_id = data.azurerm_key_vault.vault.id
-  name         = "HTTP-USERNAME"
-}
-
-data "azurerm_key_vault_secret" "http_password" {
-  key_vault_id = data.azurerm_key_vault.vault.id
-  name         = "HTTP-PASSWORD"
-}
