bf41ec4099a8a8fefee9d87592415ad1fbab4dd9
Peter Yates <peter.yates@graphia.co.uk>
2021-11-16 13:09:33 +0000

    Adjust the link checker so it works page-by-page
    This moves the loop outside of the scenario so tests are run at a
    more-granular level - previously all errors in the run would be reported
    in a single failure.

--- spec/features/external_links_spec.rb
+++ spec/features/external_links_spec.rb
@@ -2,49 +2,51 @@ require "rails_helper"
 
 RSpec.feature "link checks", :onschedule do
   describe "all pages (external)" do
-    specify "all linked-to pages exist" do
-      results = LinkChecker::Results.new
+    PageLister.content_urls.each do |current_page|
+      context "page: #{current_page}" do
+        before { WebMock.enable_net_connect! }
 
-      PageLister.content_urls.each do |current_page|
-        visit(current_page)
-        checker = LinkChecker::External.new(current_page, page.body)
+        after { WebMock.disable_net_connect! }
 
-        WebMock.enable_net_connect!
-        checker.check_links(results)
-        WebMock.disable_net_connect!
+        specify "returns a 200..299 status" do
+          visit(current_page)
+          results = LinkChecker::Results.new
+          LinkChecker::External.new(current_page, page.body).check_links(results)
+
+          failures = results.reject do |_page, response|
+            response[:status].in?(200..299)
+          end
+
+          expect(failures).to eql({})
+        end
       end
-
-      failures = results.reject do |_page, response|
-        (200..299).include? response[:status]
-      end
-
-      # comparing to hash instead of checking .empty? so the failures dump
-      # the difference in the hashes
-      expect(failures).to eql({})
     end
   end
 
   describe "all pages (internal)" do
     let(:production_url) { "https://getintoteaching.education.gov.uk" }
 
-    specify "all linked-to pages exist" do
-      results = LinkChecker::Results.new
+    PageLister.content_urls.each do |current_page|
+      context "page: #{current_page}" do
+        before { WebMock.enable_net_connect! }
 
-      PageLister.content_urls.map { |path| production_url + path }.each do |current_page|
-        WebMock.enable_net_connect!
-        visit(current_page)
-        checker = LinkChecker::Internal.new(current_page, page.body, adjust_packs_path: %w[packs-test packs])
-        checker.check_links(production_url, results)
-        WebMock.disable_net_connect!
+        after { WebMock.disable_net_connect! }
+
+        specify "returns a 200..299 status" do
+          visit(current_page)
+          results = LinkChecker::Results.new
+
+          LinkChecker::Internal
+            .new(current_page, page.body, adjust_packs_path: %w[packs-test packs])
+            .check_links(production_url, results)
+
+          failures = results.reject do |_page, response|
+            response[:status].in?(200..299)
+          end
+
+          expect(failures).to eql({})
+        end
       end
-
-      failures = results.reject do |_page, response|
-        (200..299).include? response[:status]
-      end
-
-      # comparing to hash instead of checking .empty? so the failures dump
-      # the difference in the hashes
-      expect(failures).to eql({})
     end
   end
 end
