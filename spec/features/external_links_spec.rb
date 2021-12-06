82da427f8ed4dc11149b7c5f56bb23b16f7ee6fa
Ross Oliver <ross.oliver36@gmail.com>
2021-12-06 16:30:31 +0000

    Retry failed external link checks
    We are seeing flakiness in the external link checker where it reports a
    broken link and when we check it manually straight after, or perform a
    re-run, the link check passes. To try and limit the impact of 'blips'
    with external websites we can retry the external links multiple times.
    The order is also shuffled which may help weed out flakiness.

--- spec/features/external_links_spec.rb
+++ spec/features/external_links_spec.rb
@@ -3,24 +3,14 @@ require "rails_helper"
 RSpec.feature "link checks", :onschedule do
   describe "all pages (external)" do
     specify "all linked-to pages exist" do
-      results = LinkChecker::Results.new
-
-      PageLister.content_urls.each do |current_page|
-        visit(current_page)
-        checker = LinkChecker::External.new(current_page, page.body)
-
-        WebMock.enable_net_connect!
-        checker.check_links(results)
-        WebMock.disable_net_connect!
-      end
-
-      failures = results.reject do |_page, response|
-        (200..299).include? response[:status]
-      end
+      # Check external links multiple times
+      failures_per_run = 3.times.collect { check_external_links }
+      # Persistent failures are those that failed on every run
+      persistent_failures = failures_per_run.map(&:keys).reduce(&:intersection)
 
       # comparing to hash instead of checking .empty? so the failures dump
       # the difference in the hashes
-      expect(failures).to eql({})
+      expect(failures_per_run.first.slice(*persistent_failures)).to eql({})
     end
   end
 
@@ -45,6 +35,23 @@ RSpec.feature "link checks", :onschedule do
       # comparing to hash instead of checking .empty? so the failures dump
       # the difference in the hashes
       expect(failures).to eql({})
+    end
+  end
+
+  def check_external_links
+    results = LinkChecker::Results.new
+
+    PageLister.content_urls.shuffle.each do |current_page|
+      visit(current_page)
+      checker = LinkChecker::External.new(current_page, page.body)
+
+      WebMock.enable_net_connect!
+      checker.check_links(results)
+      WebMock.disable_net_connect!
+    end
+
+    results.reject do |_page, response|
+      (200..299).include? response[:status]
     end
   end
 end
