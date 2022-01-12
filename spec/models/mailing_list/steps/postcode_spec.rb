713e8209bd833417584735b01f3178b9c02f7596
Ross Oliver <ross.oliver36@gmail.com>
2022-01-12 09:52:48 +0000

    Add send_event_emails attribute to postcode step
    We want to show a yes/no toggle on the postcode step, so we only show the text
    input when the user selects 'yes'.
    
    Add necessary attribute to populate the form with.
    
    Change postcode presence to be conditional based on if they select 'yes'.
    
    Retain CRM postcode if they have one and select 'No' to ensure we
    don't accidently clear it in the CRM.v

--- spec/models/mailing_list/steps/postcode_spec.rb
+++ spec/models/mailing_list/steps/postcode_spec.rb
@@ -7,7 +7,6 @@ describe MailingList::Steps::Postcode do
   it_behaves_like "a with wizard step"
 
   it { is_expected.to respond_to :address_postcode }
-  it { expect(subject).to be_optional }
 
   describe "validations for address_postcode" do
     it { is_expected.to allow_value("TE57 1NG").for :address_postcode }
@@ -16,6 +15,33 @@ describe MailingList::Steps::Postcode do
     it { is_expected.to allow_value("").for :address_postcode }
     it { is_expected.not_to allow_value("random").for(:address_postcode).with_message(msg) }
     it { is_expected.not_to allow_value("TE57 ING").for(:address_postcode).with_message(msg) }
+
+    context "when send_event_emails is true" do
+      before { subject.send_event_emails = true }
+
+      it { is_expected.not_to allow_value(nil).for(:address_postcode) }
+    end
+  end
+
+  describe "validations for send_event_emails" do
+    it { is_expected.not_to allow_value(nil).for(:send_event_emails) }
+    it { is_expected.to validate_inclusion_of(:send_event_emails).in_array([true, false]) }
+  end
+
+  describe "#export" do
+    subject { instance.export["address_postcode"] }
+
+    let(:backingstore) { { "send_event_emails" => true, "address_postcode" => "app-postcode" } }
+    let(:crm_backingstore) { {} }
+
+    it { is_expected.to eq("app-postcode") }
+
+    context "when the postcode exists in the CRM and they select 'No'" do
+      let(:backingstore) { { "send_event_emails" => false } }
+      let(:crm_backingstore) { { "address_postcode" => "crm-postcode" } }
+
+      it { is_expected.to eq("crm-postcode") }
+    end
   end
 
   describe "data cleaning for the postcode" do
