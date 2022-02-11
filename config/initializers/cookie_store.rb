3be322cf8971dee3ba33f883fb7de52c6750d78c
Ross Oliver <ross.oliver36@gmail.com>
2022-02-11 14:16:58 +0000

    Use Redis for session storage
    We are seeing cookie overeflow errors in production; this happens when more
    than 4kb is stored in a cookie (the current session storage medium).
    
    To avoid this we can tell Rails to use Redis as the session store in
    production. I've set the TTL to 1 day; I figure if a user hasn't completed a
    form by then its not unreasonable that they should start again.

--- config/initializers/cookie_store.rb
+++ config/initializers/cookie_store.rb
@@ -1,4 +1,18 @@
-Rails.application.config.session_store :cookie_store,
-                                       key: "_dfe_session",
-                                       same_site: :lax,
-                                       secure: Rails.env.production?
+if Rails.env.development? || Rails.env.test?
+  Rails.application.config.session_store :cookie_store,
+                                         key: "_dfe_session",
+                                         same_site: :lax,
+                                         secure: false
+else
+  Rails.application.config.session_store :redis_session_store, {
+    key: "_dfe_session",
+    same_site: :lax,
+    secure: true,
+    redis: {
+      expire_after: 1.day,
+      ttl: 1.day,
+      key_prefix: "get-into-teaching-app:session:",
+      url: ENV["REDIS_URL"],
+    },
+  }
+end
