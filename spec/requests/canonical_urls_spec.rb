83c555f699a09e1f2cdca6a9a27478886e79069e
Ross Oliver <ross.oliver36@gmail.com>
2022-02-21 09:48:00 +0000

    Add canonical URL tags to all pages
    We serve the app assets from another domain in production
    (app-assets-getintoteaching.education.gov.uk) and Google is indexing some of
    our pages on this domain instead of the production domain (as we serve the web
    pages as well as the assets).
    
    To prevent this and avoid it happening again in the future we can indicate to
    Google that we want getintoteaching.education.gov.uk to be used as the
    canonical domain).

---
+++ spec/requests/canonical_urls_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe "Canonical URLs", type: :request do
+  subject { response.body }
+
+  context "when requesting the root path" do
+    before { get root_path }
+
+    it { is_expected.to include(canonical_tag) }
+  end
+
+  context "when requesting a nested path" do
+    before { get blog_path("choosing-the-right-teacher-training-course-provider") }
+
+    it { is_expected.to include(canonical_tag("/blog/choosing-the-right-teacher-training-course-provider")) }
+  end
+
+  context "when requesting a path with query parameters" do
+    before { get "/ways-to-train", params: { some: "param" } }
+
+    it { is_expected.to include(canonical_tag("/ways-to-train")) }
+  end
+
+  def canonical_tag(path = nil)
+    %(<link href="https://getintoteaching.education.gov.uk#{path}" rel="canonical">)
+  end
+end
