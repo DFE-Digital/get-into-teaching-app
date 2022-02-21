eff9dbd28fecd06fa64db976ab4923b239834c42
Ross Oliver <ross.oliver36@gmail.com>
2022-02-21 10:37:26 +0000

    Don't serve web pages from asset host
    We only want to serve static assets from our asset host; to avoid Google ever
    indexing this domain we can prevent it having any other routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,141 +1,151 @@
 Rails.application.routes.draw do
-  root to: "pages#show", page: "home"
+  asset_host_constraint = lambda do |req|
+    asset_host = Rails.application.config.action_controller.asset_host
 
-  get "/robots.txt", to: "robots#show"
-  get "/events/not-available", to: "events#not_available"
-  get "/mailinglist/not-available", to: "mailing_list/steps#not_available"
-  get "/callbacks/not-available", to: "callbacks/steps#not_available"
+    return true if asset_host.blank?
 
-  get "/404", to: "errors#not_found", via: :all
-  get "/422", to: "errors#unprocessable_entity", via: :all
-  get "/500", to: "errors#internal_server_error", via: :all
-  get "/403", to: "errors#forbidden"
-  get "/healthcheck.json", to: "healthchecks#show", as: :healthcheck
-  get "/sitemap.xml", to: "sitemap#show", via: :all
-
-  YAML.load_file(Rails.root.join("config/redirects.yml")).fetch("redirects").tap do |redirect_rules|
-    redirect_rules.each { |from, to| get from, to: redirect(path: to) }
+    req.host != asset_host
   end
 
-  if Rails.env.rolling? || Rails.env.preprod? || Rails.env.production? || Rails.env.pagespeed?
-    get "/assets/*missing", to: "errors#not_found", via: :all
-  end
+  scope constraints: asset_host_constraint do
+    root to: "pages#show", page: "home"
 
-  post "/pagespeed/run", constraints: -> { Rails.env.pagespeed? }, to: proc { |_env|
-    require "page_speed_score"
+    get "/robots.txt", to: "robots#show"
+    get "/events/not-available", to: "events#not_available"
+    get "/mailinglist/not-available", to: "mailing_list/steps#not_available"
+    get "/callbacks/not-available", to: "callbacks/steps#not_available"
 
-    sitemap_url = "https://getintoteaching.education.gov.uk/sitemap.xml"
-    page_speed_score = PageSpeedScore.new(sitemap_url)
-    page_speed_score.fetch
+    get "/404", to: "errors#not_found", via: :all
+    get "/422", to: "errors#unprocessable_entity", via: :all
+    get "/500", to: "errors#internal_server_error", via: :all
+    get "/403", to: "errors#forbidden"
+    get "/healthcheck.json", to: "healthchecks#show", as: :healthcheck
+    get "/sitemap.xml", to: "sitemap#show", via: :all
 
-    [204, {}, []]
-  }
-
-  post "/assets/check", constraints: -> { Rails.env.pagespeed? }, to: proc { |_env|
-    require "asset_checker"
-
-    root_url = "https://getintoteaching.education.gov.uk"
-    checker = AssetChecker.new(root_url)
-    result = checker.run
-
-    [result.empty? ? 204 : 404, {}, [result.join($INPUT_RECORD_SEPARATOR)]]
-  }
-
-  namespace :internal do
-    resources :events, only: %i[index show new create edit]
-    put "/approve", to: "events#approve"
-    put "/withdraw", to: "events#withdraw"
-    get "/open_events", to: "events#open_events"
-  end
-
-  get "/funding-your-training", to: "pages#funding_your_training", as: :funding_your_training
-  get "/privacy-policy", to: "pages#privacy_policy", as: :privacy_policy
-  get "/cookies", to: "pages#cookies", as: :cookies
-  get "/tta-service", to: "pages#tta_service", as: :tta_service
-  get "/tta", to: "pages#tta_service", as: nil
-
-  get "/welcome", to: "pages#welcome", as: :welcome_guide
-  get "/welcome/my-journey-into-teaching", to: "pages#welcome_my_journey_into_teaching", as: :welcome_my_journey_into_teaching
-
-  # param paths for welcome guide customisation
-  get "/welcome/email/subject/:subject", to: "pages#welcome"
-  get "/welcome/email/degree-status/:degree_status", to: "pages#welcome"
-  get "/welcome/email/subject/:subject/degree-status/:degree_status", to: "pages#welcome"
-
-  resource :search, only: %i[show]
-  # resource :eligibility_checker,
-  #          only: %i[show],
-  #          controller: "eligibility_checker",
-  #          path: "eligibility-checker"
-
-  resource "cookie_preference", only: %i[show]
-  get "/cookie-policy", to: redirect("/cookies")
-
-  resource :csp_reports, only: %i[create]
-  resource :client_metrics, only: %i[create]
-
-  resources :blog, controller: "blog", only: %i[index show] do
-    collection do
-      resources :tag, only: %i[show], as: :blog_tag, controller: "blog/tag"
+    YAML.load_file(Rails.root.join("config/redirects.yml")).fetch("redirects").tap do |redirect_rules|
+      redirect_rules.each { |from, to| get from, to: redirect(path: to) }
     end
-  end
 
-  resources "events", path: "/events", only: %i[index show search] do
-    collection do
-      get "search"
+    if Rails.env.rolling? || Rails.env.preprod? || Rails.env.production? || Rails.env.pagespeed?
+      get "/assets/*missing", to: "errors#not_found", via: :all
     end
-    resources "steps",
-              path: "/apply",
-              controller: "event_steps",
-              only: %i[index show update] do
+
+    post "/pagespeed/run", constraints: -> { Rails.env.pagespeed? }, to: proc { |_env|
+      require "page_speed_score"
+
+      sitemap_url = "https://getintoteaching.education.gov.uk/sitemap.xml"
+      page_speed_score = PageSpeedScore.new(sitemap_url)
+      page_speed_score.fetch
+
+      [204, {}, []]
+    }
+
+    post "/assets/check", constraints: -> { Rails.env.pagespeed? }, to: proc { |_env|
+      require "asset_checker"
+
+      root_url = "https://getintoteaching.education.gov.uk"
+      checker = AssetChecker.new(root_url)
+      result = checker.run
+
+      [result.empty? ? 204 : 404, {}, [result.join($INPUT_RECORD_SEPARATOR)]]
+    }
+
+    namespace :internal do
+      resources :events, only: %i[index show new create edit]
+      put "/approve", to: "events#approve"
+      put "/withdraw", to: "events#withdraw"
+      get "/open_events", to: "events#open_events"
+    end
+
+    get "/funding-your-training", to: "pages#funding_your_training", as: :funding_your_training
+    get "/privacy-policy", to: "pages#privacy_policy", as: :privacy_policy
+    get "/cookies", to: "pages#cookies", as: :cookies
+    get "/tta-service", to: "pages#tta_service", as: :tta_service
+    get "/tta", to: "pages#tta_service", as: nil
+
+    get "/welcome", to: "pages#welcome", as: :welcome_guide
+    get "/welcome/my-journey-into-teaching", to: "pages#welcome_my_journey_into_teaching", as: :welcome_my_journey_into_teaching
+
+    # param paths for welcome guide customisation
+    get "/welcome/email/subject/:subject", to: "pages#welcome"
+    get "/welcome/email/degree-status/:degree_status", to: "pages#welcome"
+    get "/welcome/email/subject/:subject/degree-status/:degree_status", to: "pages#welcome"
+
+    resource :search, only: %i[show]
+    # resource :eligibility_checker,
+    #          only: %i[show],
+    #          controller: "eligibility_checker",
+    #          path: "eligibility-checker"
+
+    resource "cookie_preference", only: %i[show]
+    get "/cookie-policy", to: redirect("/cookies")
+
+    resource :csp_reports, only: %i[create]
+    resource :client_metrics, only: %i[create]
+
+    resources :blog, controller: "blog", only: %i[index show] do
       collection do
-        get :completed
-        get :resend_verification
+        resources :tag, only: %i[show], as: :blog_tag, controller: "blog/tag"
       end
     end
-  end
 
-  resources "teaching_events", path: "/teaching-events", controller: "teaching_events" do
-    collection do
-      get :about_ttt_events, path: "about-ttt-events", as: "about_ttt"
-    end
-  end
-
-  namespace :callbacks do
-    resources :steps, path: "/book", only: %i[index show update] do
+    resources "events", path: "/events", only: %i[index show search] do
       collection do
-        get :completed
-        get :resend_verification
+        get "search"
+      end
+      resources "steps",
+                path: "/apply",
+                controller: "event_steps",
+                only: %i[index show update] do
+        collection do
+          get :completed
+          get :resend_verification
+        end
       end
     end
-  end
 
-  # Needs to have higher priority to redirect the category
-  get "event-categories/online-events", to: redirect("/event-categories/online-q-as")
-  get "event-categories/online-events/archive", to: redirect("/event-categories/online-q-as/archive")
-
-  resources :event_categories, only: %i[show], path: "event-categories" do
-    member do
-      get "archive", to: "event_categories#show_archive"
-    end
-  end
-  # The event category pages used to exist here - once we're sure no traffic is
-  # coming to these paths we can safely remove these redirects.
-  get "events/category/:id/", to: redirect("/event-categories/%{id}")
-  get "events/category/:id/archive", to: redirect("/event-categories/%{id}/archive")
-  get "event_categories/:id", to: redirect("/event-categories/%{id}")
-  get "event_categories/:id/archive", to: redirect("/event-categories/%{id}/archive")
-
-  namespace :mailing_list, path: "/mailinglist" do
-    resources :steps,
-              path: "/signup",
-              only: %i[index show update] do
+    resources "teaching_events", path: "/teaching-events", controller: "teaching_events" do
       collection do
-        get :completed
-        get :resend_verification
+        get :about_ttt_events, path: "about-ttt-events", as: "about_ttt"
       end
     end
-  end
 
-  get "*page", to: "pages#show", as: :page, constraints: ->(request) { !request.path.start_with?("/rails/") }
+    namespace :callbacks do
+      resources :steps, path: "/book", only: %i[index show update] do
+        collection do
+          get :completed
+          get :resend_verification
+        end
+      end
+    end
+
+    # Needs to have higher priority to redirect the category
+    get "event-categories/online-events", to: redirect("/event-categories/online-q-as")
+    get "event-categories/online-events/archive", to: redirect("/event-categories/online-q-as/archive")
+
+    resources :event_categories, only: %i[show], path: "event-categories" do
+      member do
+        get "archive", to: "event_categories#show_archive"
+      end
+    end
+    # The event category pages used to exist here - once we're sure no traffic is
+    # coming to these paths we can safely remove these redirects.
+    get "events/category/:id/", to: redirect("/event-categories/%{id}")
+    get "events/category/:id/archive", to: redirect("/event-categories/%{id}/archive")
+    get "event_categories/:id", to: redirect("/event-categories/%{id}")
+    get "event_categories/:id/archive", to: redirect("/event-categories/%{id}/archive")
+
+    namespace :mailing_list, path: "/mailinglist" do
+      resources :steps,
+                path: "/signup",
+                only: %i[index show update] do
+        collection do
+          get :completed
+          get :resend_verification
+        end
+      end
+    end
+
+    get "*page", to: "pages#show", as: :page, constraints: ->(request) { !request.path.start_with?("/rails/") }
+  end
 end
