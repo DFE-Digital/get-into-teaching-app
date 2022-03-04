60e22e80893eb23e3397c7a12fe7f40ca96e294d
EllieNodder <94541493+EllieNodder@users.noreply.github.com>
2022-03-04 14:23:25 +0000

    Add custom content specs for physics funding

--- spec/components/funding_widget_component_spec.rb
+++ spec/components/funding_widget_component_spec.rb
@@ -59,27 +59,28 @@ RSpec.describe FundingWidgetComponent, type: :component do
   end
 
   describe "custom content" do
-    let(:funding_widget) { FundingWidget.new(subject: "mathematics") }
+    context "when subject is mathematics" do
+      let(:funding_widget) { FundingWidget.new(subject: "mathematics") }
 
-    it "contains subject-specific funding content" do
-      expect(page).to have_text("Scholarships of £26,000 and bursaries of £24,000 are available for trainee maths teachers.")
+      it "contains subject-specific funding content" do
+        expect(page).to have_text("Scholarships of £26,000 and bursaries of £24,000 are available for trainee maths teachers.")
+      end
+
+      it "contains subject-specific next steps content" do
+        expect(page).to have_text("If you have a passion for maths")
+      end
     end
 
-    it "contains subject-specific next steps content" do
-      expect(page).to have_text("If you have a passion for maths")
+    context "when subject is physics" do
+      let(:funding_widget) { FundingWidget.new(subject: "physics") }
+
+      it "contains subject-specific funding content" do
+        expect(page).to have_text("Scholarships of £26,000 and bursaries of £24,000 are available for trainee physics teachers.")
+      end
+
+      it "contains subject-specific next steps content" do
+        expect(page).to have_text("Discover how to become a physics teacher")
+      end
     end
   end
-end
-
-describe "custom content" do
-  let(:funding_widget) { FundingWidget.new(subject: "physics") }
-
-  it "contains subject-specific funding content" do
-    expect(page).to have_text("Scholarships of £26,000 and bursaries of £24,000 are available for trainee physics teachers.")
-  end
-
-  it "contains subject-specific next steps content" do
-    expect(page).to have_text("Discover how to become a physics teacher")
-  end
-end
 end
