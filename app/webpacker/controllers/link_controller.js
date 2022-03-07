b92cd773c997e7a55a79cd8020ba9b3bc41ca4f6
Peter Yates <peter.yates@graphia.co.uk>
2022-03-07 14:23:27 +0000

    Filter the array instead of just using a forEach

--- app/webpacker/controllers/link_controller.js
+++ app/webpacker/controllers/link_controller.js
@@ -13,22 +13,14 @@ export default class extends Controller {
   }
 
   openExternalContentLinksInNewWindow() {
-    const links = this.contentTarget.querySelectorAll('a');
+    const links = Array.from(this.contentTarget.querySelectorAll('a'));
 
-    links.forEach((l) => {
-      if (this.isExternal(l)) {
+    links
+      .filter((l) => this.isExternal(l))
+      .forEach((l) => {
         l.setAttribute('target', '_blank');
         l.setAttribute('rel', 'noopener');
-
-        // add hidden text to inform screen reader users that
-        // link will open in a new window
-        const linkOpensInNewWindow = document.createElement('span');
-        const text = document.createTextNode('(Link opens in new window)');
-        linkOpensInNewWindow.classList.add('govuk-visually-hidden');
-        linkOpensInNewWindow.appendChild(text);
-        l.appendChild(linkOpensInNewWindow);
-      }
-    });
+      });
   }
 
   preventTurboLinksOnJumpLinks() {
@@ -44,7 +36,10 @@ export default class extends Controller {
   isExternal(link) {
     const href = link.getAttribute('href');
 
-    return href?.startsWith('http') && !href?.includes(this.assetsUrlValue);
+    return (
+      href?.startsWith('http') &&
+      !href?.includes(this.assetsUrlValue || window.location.host)
+    );
   }
 
   prepareChevronOnButtonLinks() {
