bc1ddbdd88748a3bbc408749a462d06c3df1309f
Peter Yates <peter.yates@graphia.co.uk>
2022-04-11 10:06:23 +0100

    Pass args to session store config as keywords

--- config/initializers/cookie_store.rb
+++ config/initializers/cookie_store.rb
@@ -4,15 +4,14 @@ if Rails.env.development? || Rails.env.test?
                                          same_site: :lax,
                                          secure: false
 else
-  Rails.application.config.session_store :redis_session_store, {
-    key: "_dfe_session",
-    same_site: :lax,
-    secure: true,
-    redis: {
-      expire_after: 1.day,
-      ttl: 1.day,
-      key_prefix: "get-into-teaching-app:session:",
-      url: ENV["REDIS_URL"],
-    },
-  }
+  Rails.application.config.session_store :redis_session_store,
+                                         key: "_dfe_session",
+                                         same_site: :lax,
+                                         secure: true,
+                                         redis: {
+                                           expire_after: 1.day,
+                                           ttl: 1.day,
+                                           key_prefix: "get-into-teaching-app:session:",
+                                           url: ENV["REDIS_URL"],
+                                         }
 end
