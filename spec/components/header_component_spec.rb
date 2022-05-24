41c576487d9f2619478933826be389d4c0b1637f
Ross Oliver <ross.oliver36@gmail.com>
2022-05-24 13:11:07 +0100

    Restructure header and hero sections
    We want the hero section to be part of the main content (as it often contains
    the main heading of the page). The rest of the heading component (navigation,
    logo, breadcrumbs, etc) should be above/out of the main content.

--- spec/components/header_component_spec.rb
+++ spec/components/header_component_spec.rb
@@ -4,7 +4,7 @@ describe HeaderComponent, type: "component" do
   subject! { render_inline(described_class.new) }
 
   let(:front_matter) do
-    { title: "What a nice page", image: "media/images/content/hero-images/0012.jpg" }
+    { title: "What a nice page" }
   end
 
   specify "renders a skiplink container" do
@@ -23,42 +23,11 @@ describe HeaderComponent, type: "component" do
     expect(page).not_to have_css(".breadcrumb")
   end
 
-  specify "doesn't render hero by default" do
-    expect(page).not_to have_css(".hero")
-  end
-
   context "when breadcrumbs: true" do
     subject! { render_inline(described_class.new(breadcrumbs: true)) }
 
     specify "renders the breadcrumbs" do
       expect(page).to have_css(".breadcrumbs")
-    end
-  end
-
-  context "when the hero slot is called with valid front matter" do
-    subject! do
-      render_inline(described_class.new) do |component|
-        component.hero(front_matter)
-      end
-    end
-
-    specify "renders the hero" do
-      expect(page).to have_css(".hero")
-    end
-  end
-
-  context "when content is added above the hero" do
-    let(:above_hero_content) { "some above hero content" }
-
-    subject! do
-      render_inline(described_class.new) do |component|
-        component.above_hero { above_hero_content }
-        component.hero(front_matter)
-      end
-    end
-
-    specify "the content is rendered in the output" do
-      expect(page).to have_content(above_hero_content)
     end
   end
 end
