56052fb5a40bb13ffcbe7ad0f7923da2229e0bf0
Peter Yates <peter.yates@graphia.co.uk>
2022-06-10 11:11:33 +0100

    Add some specs covering path/wildcard redirects

--- spec/requests/redirects_spec.rb
+++ spec/requests/redirects_spec.rb
@@ -4,6 +4,7 @@ describe "Redirects", content: true, type: :request do
   before(:all) { @result = {} }
 
   let(:query_string) { "abc=def&ghi=jkl" }
+  let(:valid_results) { [200, 301, 302] }
 
   redirects = YAML.load_file(Rails.root.join("config/redirects.yml")).fetch("redirects")
   redirects.each do |from, to|
@@ -24,7 +25,35 @@ describe "Redirects", content: true, type: :request do
 
         @result[target] ||= get(target)
 
-        expect(@result[target]).to be_in([200, 301, 302])
+        expect(@result[target]).to be_in(valid_results)
+      end
+    end
+  end
+
+  describe "path redirects" do
+    {
+      "/life-as-a-teacher/some-old-page" => "/blog",
+      "/life-as-a-teacher/some-old-page/some-old-subpage" => "/blog",
+      "/why-teach" => "/blog",
+      "/why-teach/why-teach-subpage" => "/blog",
+      "/content/stories" => "/blog",
+      "/content/stories/subpage" => "/blog",
+    }.each do |from, to|
+      describe "'#{from}' redirects to '#{to}'" do
+        subject { get(build_url(from, query_string)) }
+
+        specify "redirects successfully" do
+          allow(Rails.logger).to receive(:info)
+
+          expect(subject).to be 301
+          expect(response).to redirect_to(build_url(to, query_string))
+
+          target = Nokogiri.parse(response.body).at_css("a")["href"].gsub(root_url, "/")
+
+          @result[target] ||= get(target)
+
+          expect(@result[target]).to be_in(valid_results)
+        end
       end
     end
   end
