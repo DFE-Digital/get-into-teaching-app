d70163407028294ef5ee0cbfd94639c9c156601c
Ross Oliver <ross.oliver36@gmail.com>
2022-10-17 11:27:32 +0100

    Update homepage component for badge/caption
    We want to add a purple caption and badge (containing text) to the homepage
    component. Update component with attributes and tweak CSS of badge to support
    fixed-width for multi-line text content.

--- spec/components/calls_to_action/homepage_component_spec.rb
+++ spec/components/calls_to_action/homepage_component_spec.rb
@@ -6,10 +6,23 @@ RSpec.describe CallsToAction::HomepageComponent, type: :component do
   let(:title) { "Joey" }
   let(:link_text) { "Click here" }
   let(:link_target) { "/some-dir/some-page" }
+  let(:caption) { "Caption text" }
+  let(:badge_text) { "Badge text" }
   let(:text) { nil }
 
   describe "rendering the component" do
-    let(:kwargs) { { icon: icon, title: title, link_text: link_text, link_target: link_target, image: image, text: text } }
+    let(:kwargs) do
+      {
+        icon: icon,
+        title: title,
+        link_text: link_text,
+        link_target: link_target,
+        image: image,
+        text: text,
+        caption: caption,
+        badge_text: badge_text,
+      }
+    end
 
     let(:component) { described_class.new(**kwargs) }
 
@@ -29,6 +42,10 @@ RSpec.describe CallsToAction::HomepageComponent, type: :component do
       expect(page).to have_css(".call-to-action__heading", text: title)
     end
 
+    specify "the caption is present" do
+      expect(page).to have_css(".caption-m", text: caption)
+    end
+
     specify "the link is present" do
       expect(page).to have_link(link_text, href: link_target)
     end
@@ -37,12 +54,28 @@ RSpec.describe CallsToAction::HomepageComponent, type: :component do
       expect(page.find(".call-to-action__image")["style"]).to include("packs-test/v1/media/images/dfelogo")
     end
 
+    specify "the badge_text is present" do
+      expect(page).to have_css(".call-to-action__image .badge", text: badge_text)
+    end
+
     context "when text is present" do
       let(:text) { "some text" }
 
       it "renders the text" do
         expect(page).to have_css(".call-to-action__content p", text: text)
       end
+    end
+
+    context "when caption is not present" do
+      let(:caption) { nil }
+
+      it { expect(page).not_to have_css(".caption-m") }
+    end
+
+    context "when badge_text is not present" do
+      let(:badge_text) { nil }
+
+      it { expect(page).not_to have_css(".badge") }
     end
   end
 end
