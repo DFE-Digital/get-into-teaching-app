65ad48e06a2ed306a5f731519adf8ec13bd2722a
Ross Oliver <ross.oliver36@gmail.com>
2022-11-16 12:05:48 +0000

    Make text block with image components generic
    We have started to reference these components outside of the campaign/landing
    pages; they were also tied to the `campaign` layout; this pulls them out of
    that module and extracts the styles into a separate sheet.

--- spec/components/text_block_with_one_image_component_spec.rb
+++ spec/components/text_block_with_one_image_component_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Campaign::TextBlockWithOneImageComponent, type: "component" do
+RSpec.describe TextBlockWithOneImageComponent, type: "component" do
   subject! { Capybara.string(component) }
 
   let(:heading) { "A heading" }
