ff6cebe7ac553df4446d870789d4b8da36148f07
Ross Oliver <ross.oliver36@gmail.com>
2023-01-09 10:52:08 +0000

    Add QuoteListComponent
    We want to display a list of quotes on some of our content pages; this is
    similar to the `PhotoQuoteListComponent` but without the photos and with
    the heading half overlapping the containing box. I initially tried to
    combine the two quote list components but it became awkward due to the
    overlapping heading on the non-photo variant, so I've kept them separate.

---
+++ app/components/content/quote_list_component.rb
@@ -0,0 +1,26 @@
+module Content
+  class QuoteListComponent < ViewComponent::Base
+    attr_reader :quotes
+
+    def initialize(quotes)
+      super
+
+      @quotes = quotes
+    end
+
+  private
+
+    def before_render
+      validate!
+    end
+
+    def validate!
+      quotes.each_with_index do |quote, idx|
+        %i[heading text accreditation].each do |required_key|
+          error_message = "#{required_key} must be present for quote #{idx + 1}"
+          fail(ArgumentError, error_message) if quote[required_key].blank?
+        end
+      end
+    end
+  end
+end
