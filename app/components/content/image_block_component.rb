b701869befd97b865a563c9638d2c03c55f413d7
Ross Oliver <ross.oliver36@gmail.com>
2023-04-03 16:06:19 +0100

    Ensure images have alt text

--- app/components/content/image_block_component.rb
+++ app/components/content/image_block_component.rb
@@ -1,12 +1,16 @@
 module Content
   class ImageBlockComponent < ViewComponent::Base
-    attr_reader :title, :image
+    attr_reader :title, :image_path
 
-    def initialize(title:, image:)
+    def initialize(title:, image_path:)
       super
 
       @title = title
-      @image = image
+      @image_path = image_path
+    end
+
+    def image
+      helpers.image_pack_tag(image_path, alt: helpers.image_alt(image_path))
     end
   end
 end
