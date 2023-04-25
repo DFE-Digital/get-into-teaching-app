cdedf23d306347aad98ea0700c8983738a76cace
Ross Oliver <ross.oliver36@gmail.com>
2023-04-25 12:36:13 +0100

    Migrate TTA validators
    Migrate across the TTA validation modules for pick list/lookup items.
    
    Also updates the `rails_helper` to persist test status. This lets you run only
    the failed tests after a test run has completed with `rspec --only-failures` -
    makes debugging easier when migrating hundreds of tests!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,8 @@ end
 DfE::Analytics::Testing.fake!
 
 RSpec.configure do |config|
+  config.example_status_persistence_file_path = "tmp/examples.txt"
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
