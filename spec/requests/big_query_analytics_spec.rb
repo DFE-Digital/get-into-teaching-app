ecf3c66710df71e406d5ec4ce7d7abdc20a7a372
Ross Oliver <ross.oliver36@gmail.com>
2023-05-10 08:15:53 +0100

    Namespace dfe-analytics web_request events
    We want to namespace the dfe-analytics web_request events so that we
    can easily distinguish between the adviser sign up on the GiT website
    and the Get an advsier service.

--- spec/requests/big_query_analytics_spec.rb
+++ spec/requests/big_query_analytics_spec.rb
@@ -5,6 +5,17 @@ RSpec.describe "BigQuery Analytics", type: :request do
     expect { get root_path }.to have_sent_analytics_event_types(:web_request)
   end
 
+  it "namespaces DFE analytics web request events" do
+    get root_path
+
+    event_namespaces = enqueued_jobs
+      .find { |j| j["job_class"] == "DfE::Analytics::SendEvents" }["arguments"]
+      .flatten
+      .map { |a| a["namespace"] }
+
+    expect(event_namespaces).to all(eq("get_into_teaching"))
+  end
+
   it "sends DFE Analytics entity events" do
     params = { teacher_training_adviser_feedback: attributes_for(:feedback) }
     post teacher_training_adviser_feedbacks_path, params: params
