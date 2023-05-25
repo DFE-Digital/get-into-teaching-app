4c1a4786919b09cbadede426fdfa4a2e492357b2
Pavel Lisovin <pavel.lisovin@education.gov.uk>
2023-05-25 13:14:41 +0100

    Re-introduce feedback.js for feedback via button click at the end of the event funnel

---
+++ app/webpacker/controllers/feedback_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ['text', 'link'];
+
+  connect() {
+    this.element.classList.add('visible');
+  }
+
+  answer() {
+    // Answer is captured by event configured in GTM.
+    this.hideLinks();
+    this.showThankYouText();
+  }
+
+  showThankYouText() {
+    this.textTarget.textContent = 'Thank you for your feedback';
+  }
+
+  hideLinks() {
+    this.linkTargets.forEach((target) => target.classList.add('hidden'));
+  }
+}
