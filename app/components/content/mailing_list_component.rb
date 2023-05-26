076e3688b97c34314042fef3abbf3ad9721df030
Ross Oliver <ross.oliver36@gmail.com>
2023-05-26 11:59:40 +0100

    Update MailingListComponent for new landing page
    As part of a new landing page design we need to be able to drop the
    mailing list component on a grey background and without the purple color
    accents. We also need finer control lover the margin and heading size.

--- app/components/content/mailing_list_component.rb
+++ app/components/content/mailing_list_component.rb
@@ -1,12 +1,29 @@
 module Content
   class MailingListComponent < ViewComponent::Base
-    attr_reader :title, :intro
+    attr_reader :title, :intro, :color, :margin, :heading
 
-    def initialize(title:, intro:)
+    def initialize(title:, intro:, color: "purple", margin: true, heading: :m)
       super
 
       @title = title
       @intro = intro
+      @color = color
+      @margin = margin
+      @heading = heading
+    end
+
+    def classes
+      %w[action-container].tap do |c|
+        c << "action-container--#{color}"
+        c << "action-container--no-margin" unless margin
+      end
+    end
+
+    def heading_classes
+      %w[].tap do |c|
+        c << "heading-#{heading}"
+        c << "heading--box-#{color}" unless color == "transparent"
+      end
     end
 
     def privacy_policy
