bec77a59a62a8802fdac6aa0e8dc4711747b6c1b
Pavel Lisovin <pavel.lisovin@education.gov.uk>
2023-05-30 15:03:56 +0100

    Fix feature tests to include first name in the completed step

--- spec/features/mailing_list_wizard_spec.rb
+++ spec/features/mailing_list_wizard_spec.rb
@@ -41,7 +41,7 @@ RSpec.feature "Mailing list wizard", type: :feature do
     click_on "Complete sign up"
 
     expect(page).to have_title("You've signed up | Get Into Teaching")
-    expect(page).to have_text "#{first_name}, you're signed up"
+    expect(page).to have_text "Test, you're signed up"
     expect(page).to have_link("Book a callback")
   end
 
@@ -81,7 +81,7 @@ RSpec.feature "Mailing list wizard", type: :feature do
     fill_in "Your UK postcode (optional)", with: "TE57 1NG"
     click_on "Complete sign up"
 
-    expect(page).to have_text "You're signed up"
+    expect(page).to have_text "Test, you're signed up"
 
     # We pass this to the BAM tracking pixel in GTM.
     expect(page).to have_selector("[data-sub-channel-id='#{sub_channel_id}']")
@@ -120,7 +120,7 @@ RSpec.feature "Mailing list wizard", type: :feature do
     fill_in "Your UK postcode (optional)", with: "TE57 1NG"
     click_on "Complete sign up"
 
-    expect(page).to have_text "#{first_name}, you're signed up"
+    expect(page).to have_text "Test, you're signed up"
     expect(page).to have_link("helpful guide")
 
     # We pass this to the BAM tracking pixel in GTM.
@@ -159,7 +159,7 @@ RSpec.feature "Mailing list wizard", type: :feature do
     fill_in "Your UK postcode (optional)", with: "TE57 1NG"
     click_on "Complete sign up"
 
-    expect(page).to have_text "#{first_name}, you're signed up"
+    expect(page).to have_text "Test, you're signed up"
   end
 
   scenario "Full journey as an existing candidate" do
@@ -210,7 +210,7 @@ RSpec.feature "Mailing list wizard", type: :feature do
     )
     click_on "Complete sign up"
 
-    expect(page).to have_text "You're signed up"
+    expect(page).to have_text "Joey, you're signed up"
   end
 
   scenario "Full journey as a candidate that is already qualified to teach" do
@@ -349,7 +349,7 @@ RSpec.feature "Mailing list wizard", type: :feature do
     fill_in "Your UK postcode (optional)", with: ""
     click_on "Complete sign up"
 
-    expect(page).to have_text "You're signed up"
+    expect(page).to have_text "you're signed up"
   end
 
   scenario "Invalid magic link tokens" do
@@ -425,7 +425,7 @@ RSpec.feature "Mailing list wizard", type: :feature do
     scenario "Viewing the completion page" do
       visit mailing_list_step_path(:completed)
 
-      expect(page).to have_text("#{first_name}, you're signed up")
+      expect(page).to have_text("you're signed up")
       expect(page).not_to have_link("Book a callback")
     end
   end
