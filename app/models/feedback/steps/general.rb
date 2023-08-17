3e9a73fc190644610ba90c72255d9a100b6eb40a
Pavel Lisovin <pavel.lisovin@education.gov.uk>
2023-08-17 17:11:23 +0100

    Add more pages to the steps

--- app/models/feedback/steps/general.rb
+++ app/models/feedback/steps/general.rb
@@ -1,7 +1,22 @@
 module Feedback
   module Steps
     class General < ::GITWizard::Step
-      attribute :general
+
+      OPTIONS = [
+        "Yes",
+        "No",
+      ].freeze
+
+      attribute :achieved
+      validates :achieved, inclusion: { in: OPTIONS }
+
+      def skipped?
+        website?
+      end
+
+      def website?
+        @store["action"] == "Give feedback about the website"
+      end
     end
   end
 end
--- app/models/feedback/steps/purpose.rb
+++ app/models/feedback/steps/purpose.rb
@@ -3,17 +3,13 @@ module Feedback
     class Purpose < ::GITWizard::Step
       OPTIONS = [
         "Give feedback about the website",
-        "Give feedback about signing up for an adviser ",
+        "Give feedback about signing up for an adviser",
         "Give feedback about signing up for an emails",
         "Give feedback about signing up for an event ",
       ].freeze
 
       attribute :action
       validates :action, inclusion: { in: OPTIONS }
-
-      # def can_proceed?
-      #   true
-      # end
     end
   end
 end
