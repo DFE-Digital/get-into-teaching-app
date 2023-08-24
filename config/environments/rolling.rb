a7494cdbabbcc36c3265406cd4c79657de01521e
Pavel Lisovin <pavel.lisovin@education.gov.uk>
2023-08-24 15:41:25 +0100

    Change API to aks url

--- config/environments/rolling.rb
+++ config/environments/rolling.rb
@@ -4,7 +4,7 @@ require File.expand_path("production.rb", __dir__)
 Rails.application.configure do
   # Override any production defaults here
   config.x.git_api_endpoint = \
-    "https://get-into-teaching-api-dev.london.cloudapps.digital/api"
+    "https://getintoteachingapi-development.test.teacherservices.cloud/api"
 
   config.view_component.show_previews = true
 
