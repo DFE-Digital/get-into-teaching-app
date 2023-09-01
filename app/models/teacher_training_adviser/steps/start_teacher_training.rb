2fa5f8526b81540be4780d329a2625b757e4c5f4
Pavel Lisovin <pavel.lisovin@education.gov.uk>
2023-09-01 23:37:06 +0100

    Change start date so 2023 is removed from the list of teacher training years in adviser funnel

--- app/models/teacher_training_adviser/steps/start_teacher_training.rb
+++ app/models/teacher_training_adviser/steps/start_teacher_training.rb
@@ -90,7 +90,7 @@ module TeacherTrainingAdviser::Steps
     end
 
     def date_to_drop_current_year
-      Date.new(current_year, 9, 7)
+      Date.new(current_year, 9, 3)
     end
 
     def current_year
