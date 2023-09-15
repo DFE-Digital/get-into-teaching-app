b0df4e0e09ba4b46e493b5a08d99a77b871e5151
Pavel Lisovin <pavel.lisovin@education.gov.uk>
2023-09-15 10:28:40 +0100

    Add validation to events through the GiT API

--- app/models/events/steps/personal_details.rb
+++ app/models/events/steps/personal_details.rb
@@ -7,8 +7,28 @@ module Events
 
       validates :event_id, presence: true
 
+      def channel_ids
+        query_channels.map { |channel| channel.id.to_i }
+      end
+
       def is_walk_in?
         is_walk_in.present?
+      end
+
+      def save
+        self.channel_id = nil if channel_invalid?
+
+        super
+      end
+
+    private
+
+      def channel_invalid?
+        channel_id.present? && !channel_id.in?(channel_ids)
+      end
+
+      def query_channels
+        @query_channels ||= GetIntoTeachingApiClient::PickListItemsApi.new.get_teaching_event_registration_channels
       end
     end
   end
