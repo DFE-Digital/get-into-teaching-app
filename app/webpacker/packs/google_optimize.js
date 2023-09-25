bfb48d7354d52f83e0c826447738bb98a5121ebe
Pavel Lisovin <77098906+plisovin@users.noreply.github.com>
2023-09-25 11:59:07 +0100

    Remove googke optimize (#3548)

--- app/webpacker/packs/google_optimize.js
+++
@@ -1,9 +0,0 @@
-import GoogleOptimize from '../javascript/google_optimize'
-
-const config = document.querySelector("[data-google-optimize-id]").dataset;
-
-if (config.googleOptimizeId && config.googleOptimizePaths) {
-  const paths = JSON.parse(config.googleOptimizePaths);
-  const optimize = new GoogleOptimize(config.googleOptimizeId, paths)
-  optimize.init()
-}
