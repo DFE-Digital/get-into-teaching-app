f54fd7964f16f9fc05b691065f59c109d7f944d2
john hayes-reed <john.hayesreed@tecknuovo.com>
2023-10-23 15:34:47 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2021_04_15_104621) do
+ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "user_feedbacks", force: :cascade do |t|
-    t.string "topic", null: false
-    t.integer "rating", null: false
-    t.text "explanation"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
