a8ab5e46752315a7b5c1d96e03b2b0d3a49a4d13
john hayes-reed <john.hayesreed@tecknuovo.com>
2023-10-26 14:10:20 +0100

    Address PR comments for error messages and styling

--- app/wizard_pipelines/feedback/steps/rating.rb
+++ app/wizard_pipelines/feedback/steps/rating.rb
@@ -3,9 +3,9 @@ module Feedback
     class Rating < ::GITWizard::Step
       RATINGS = [
         OpenStruct.new(id: 0, value: "Very satisfied"),
-        OpenStruct.new(id: 1, value: "Somewhat satisfied"),
+        OpenStruct.new(id: 1, value: "Satisfied"),
         OpenStruct.new(id: 2, value: "Neither satisfied nor dissatisfied"),
-        OpenStruct.new(id: 3, value: "Somewhat dissatisfied"),
+        OpenStruct.new(id: 3, value: "Dissatisfied"),
         OpenStruct.new(id: 4, value: "Very dissatisfied"),
       ].freeze
 
