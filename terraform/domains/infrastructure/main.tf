065e0c27c51a2bc2610cf333c38a42951b110df0
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2023-11-07 12:09:57 +0000

    wip

--- terraform/domains/infrastructure/main.tf
+++ terraform/domains/infrastructure/main.tf
@@ -3,3 +3,8 @@ module "domains_infrastructure" {
   hosted_zone            = var.hosted_zone
   deploy_default_records = var.deploy_default_records
 }
+
+module "dns" {
+  source      = "./vendor/modules/domains//dns/zones"
+  hosted_zone = var.hosted_zone_without_front_door
+}
--- terraform/domains/infrastructure/variables.tf
+++ terraform/domains/infrastructure/variables.tf
@@ -2,6 +2,10 @@ variable "hosted_zone" {
   type = map(any)
 }
 
+variable "hosted_zone_without_front_door" {
+  type = map(any)
+}
+
 variable "deploy_default_records" {
   default = true
 }
