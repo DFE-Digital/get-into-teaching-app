94618d8dbc55c476b9040ade8a80518311276ae1
john hayes-reed <john.hayesreed@tecknuovo.com>
2023-11-17 08:48:07 +0000

    Auto populate preferred teaching subject id to primary if preferred stage is primary

--- app/models/teacher_training_adviser/steps/stage_interested_teaching.rb
+++ app/models/teacher_training_adviser/steps/stage_interested_teaching.rb
@@ -2,15 +2,20 @@ module TeacherTrainingAdviser::Steps
   class StageInterestedTeaching < GITWizard::Step
     extend ApiOptions
 
+    before_validation :primary_subject_preferred, if: :interested_in_primary?
+
     attribute :preferred_education_phase_id, :integer
+    attribute :preferred_teaching_subject_id, :string
 
     validates :preferred_education_phase_id, pick_list_items: { method: :get_candidate_preferred_education_phases }
+    validates :preferred_teaching_subject_id, inclusion: { in: [Crm::TeachingSubject::PRIMARY] }, if: :interested_in_primary?
 
     OPTIONS = { primary: 222_750_000, secondary: 222_750_001 }.freeze
 
     def reviewable_answers
       super.tap do |answers|
         answers["preferred_education_phase_id"] = OPTIONS.key(preferred_education_phase_id).to_s.capitalize
+        answers["preferred_teaching_subject_id"] = Crm::TeachingSubject.lookup_by_uuid(preferred_teaching_subject_id)
       end
     end
 
@@ -20,6 +25,12 @@ module TeacherTrainingAdviser::Steps
 
     def returning_teacher?
       other_step(:returning_teacher).returning_to_teaching
+    end
+
+    private
+
+    def primary_subject_preferred
+      assign_attributes(preferred_teaching_subject_id: Crm::TeachingSubject::PRIMARY)
     end
   end
 end
