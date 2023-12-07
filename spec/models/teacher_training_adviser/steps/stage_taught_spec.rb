fd6f555369485720ccddaff5c07f5ae26bd8e07c
Martyn Whitwell <martyn.whitwell@gmail.com>
2023-12-07 16:20:17 +0000

    model spec fixes

--- spec/models/teacher_training_adviser/steps/stage_taught_spec.rb
+++ spec/models/teacher_training_adviser/steps/stage_taught_spec.rb
@@ -7,30 +7,30 @@ RSpec.describe TeacherTrainingAdviser::Steps::StageTaught do
   it { is_expected.to be_skipped }
 
   describe "attributes" do
-    it { is_expected.to respond_to :stage_taught }
+    it { is_expected.to respond_to :stage_taught_id }
   end
 
   describe "#stage_taught" do
-    it { is_expected.not_to allow_values("", nil, 123).for :stage_taught }
-    it { is_expected.to allow_value("primary", "secondary").for :stage_taught }
+    it { is_expected.not_to allow_values("", nil, 123, "primary", "secondary").for :stage_taught_id }
+    it { is_expected.to allow_value(222_750_000, 222_750_001).for :stage_taught_id }
   end
 
-  describe "#previous_stage_primary?" do
+  describe "#stage_taught_primary??" do
     before do
       allow_any_instance_of(described_class).to \
-        receive(:stage_taught) { stage_taught }
+        receive(:stage_taught_id) { stage_taught_id }
     end
 
     context "when stage_taught is primary" do
-      let(:stage_taught) { "primary" }
+      let(:stage_taught_id) { 222_750_000 }
 
-      it { is_expected.to be_previous_stage_primary }
+      it { is_expected.to be_stage_taught_primary }
     end
 
     context "when stage_taught is secondary" do
-      let(:stage_taught) { "secondary" }
+      let(:stage_taught_id) { 222_750_001 }
 
-      it { is_expected.not_to be_previous_stage_primary }
+      it { is_expected.not_to be_stage_taught_primary }
     end
   end
 end
