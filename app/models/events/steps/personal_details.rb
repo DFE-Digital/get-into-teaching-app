2985a4d9c25212013554c213a52bb023da8f59af
Pavel Lisovin <77098906+plisovin@users.noreply.github.com>
2024-01-09 11:45:49 +0000

    Add channel id to events page and carry this param to the register form (#3529)
    * Add channel id to events page and carry this param to the register form
    * Add validation to events through the GiT API
    * Adds API call to load correct channel IDs
    * Adds channel_id attribute to PersonalDetails model
    * Add channel parameter to categorise_events method
    * Update Gemfile.lock with new dependencies (api-client)
    * Update specs
    * Add channelId to attendee JSON in Events::Wizard#add_attendee_to_event
    
    ---------
    
    Co-authored-by: Chidi Ekuma <bukki.arise@yahoo.co.uk>
    Co-authored-by: Ekuma Chidi <7931750+ekumachidi@users.noreply.github.com>

--- app/models/events/steps/personal_details.rb
+++ app/models/events/steps/personal_details.rb
@@ -3,11 +3,32 @@ module Events
     class PersonalDetails < ::GITWizard::Steps::Identity
       attribute :is_walk_in, :boolean
       attribute :event_id
+      attribute :channel_id, :integer
 
       validates :event_id, presence: true
 
+      def channel_ids
+        query_channels.map { |channel| channel.id.to_i }
+      end
+
       def is_walk_in?
         is_walk_in.present?
+      end
+
+      def save
+        self.channel_id = nil unless channel_valid?
+
+        super
+      end
+
+    private
+
+      def channel_valid?
+        channel_id.present? && channel_id.in?(channel_ids)
+      end
+
+      def query_channels
+        @query_channels ||= GetIntoTeachingApiClient::PickListItemsApi.new.get_candidate_event_subscription_channels
       end
     end
   end
