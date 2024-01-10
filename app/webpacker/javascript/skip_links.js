73dbcc8e9aa814c4a7af2b6b1b6d7313948ae7c7
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-01-10 09:16:14 +0000

    Update skip_links.js
    fix linting issues

--- app/webpacker/javascript/skip_links.js
+++ app/webpacker/javascript/skip_links.js
@@ -2,20 +2,24 @@
 // Android. See https://axesslab.com/skip-links/ for a discussion of the issue
 // and suggested fixes (including the one below from Mike Foskett).
 
-(_ => {
-  const skip_lnk = document.querySelector('.skiplink');
-  if (!skip_lnk) return false;
-  skip_lnk.addEventListener('click', e => {
+((_) => {
+  const skiplink = document.querySelector('.skiplink');
+  if (!skiplink) return false;
+  skiplink.addEventListener('click', (e) => {
     e.preventDefault();
-    const to_obj = document.getElementById(skip_lnk.href.split('#')[1]);
-    if (to_obj) {
-      to_obj.setAttribute('tabindex', '-1');
-      to_obj.addEventListener('blur', e => {
-        to_obj.removeAttribute('tabindex');
-      }, {
-        once: true
-      });
-      to_obj.focus();
+    const obj = document.getElementById(skiplink.href.split('#')[1]);
+    if (obj) {
+      obj.setAttribute('tabindex', '-1');
+      obj.addEventListener(
+        'blur',
+        (e) => {
+          obj.removeAttribute('tabindex');
+        },
+        {
+          once: true,
+        }
+      );
+      obj.focus();
     }
   });
 })();
