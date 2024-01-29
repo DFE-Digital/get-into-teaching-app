2357cc0d2d6232805cbe1d2ba67dd53f2bf6e814
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-01-29 08:39:52 +0000

    Remove alt text on hero images (#3816)
    * Remove alt text on hero images
    
    * Update image_spec.rb
    
    * Remove descriptions from decorative images
    
    * Add a role=presentation attrib for decorative images
    
    * add helper specs

--- spec/helpers/image_helper_spec.rb
+++ spec/helpers/image_helper_spec.rb
@@ -11,4 +11,20 @@ describe ImageHelper, type: "helper" do
   describe "#image_alt" do
     it { expect(image_alt(path)).to eq(alt) }
   end
+
+  describe "#image_alt_attribs" do
+    it { expect(image_alt_attribs(path)).to eq({ alt: alt }) }
+  end
+
+  context "with alt text" do
+    describe "#image_alt_attribs_for_text" do
+      it { expect(image_alt_attribs_for_text(alt)).to eq({ alt: alt }) }
+    end
+  end
+
+  context "with blank alt text" do
+    describe "#image_alt_attribs_for_text" do
+      it { expect(image_alt_attribs_for_text("")).to eq({ alt: "", role: "presentation" }) }
+    end
+  end
 end
