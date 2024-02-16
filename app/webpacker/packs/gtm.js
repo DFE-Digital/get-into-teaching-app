8e10918d593e806d4f55e43fb06c66a316889f70
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-02-16 17:45:48 +0000

    work in progress...

--- app/webpacker/packs/gtm.js
+++ app/webpacker/packs/gtm.js
@@ -1,8 +1,8 @@
-import Gtm from '../javascript/gtm'
+import Gtm from '../javascript/gtm';
 
-const gtmId = document.querySelector("[data-gtm-id]").dataset.gtmId
+const gtmId = document.querySelector('[data-gtm-id]').dataset.gtmId;
 
 if (gtmId) {
-  const gtm = new Gtm(gtmId)
-  gtm.init()
+  const gtm = new Gtm(gtmId);
+  gtm.init();
 }
