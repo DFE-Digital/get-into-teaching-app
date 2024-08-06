fcb1054a51b160069c3f3aad80f9f0625ec7a78f
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-08-06 10:38:21 +0100

    test fixes, remove old images

--- spec/lib/template_handlers/markdown_spec.rb
+++ spec/lib/template_handlers/markdown_spec.rb
@@ -240,7 +240,7 @@ describe TemplateHandlers::Markdown, type: :view do
     specify "the rendered output contains the specified images" do
       expect(rendered).to have_css("img", count: 2)
 
-      %w[0001 0009].each do |photo|
+      %w[0027 0032].each do |photo|
         expect(rendered).to have_css("img")
         expect(rendered).to match(%r{src="/packs-test/v1/static/images/content/hero-images/#{photo}-.*.jpg"})
         key = Image.new.alt("static/images/content/hero-images/#{photo}.jpg")
