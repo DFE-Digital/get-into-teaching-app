f6834dc34d85eb8cad0b41a5fdd64a1483ba7fc4
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-08-08 17:30:47 +0100

    remove old comments

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -19,18 +19,12 @@ private
 
   def show_html
     render layout: "chat"
-
-    # render \
-    #   template: "errors/not_found",
-    #   layout: "application",
-    #   status: :not_found
   end
 
   def show_json
     # De-activate the CSP header on the chats page
     SecureHeaders.opt_out_of_header(request, "csp")
     render json: Chat.new
-    # render status: :not_found, body: nil
   end
 
   def authenticate?
