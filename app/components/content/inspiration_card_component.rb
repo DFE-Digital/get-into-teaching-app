ca5712de4d5f236accb9e4d7871fd555172a86de
Spencer Dixon <spencerlloyddixon@gmail.com>
2024-08-16 14:10:42 +0100

    Added inspiration card component and how to become a teacher cta sections

---
+++ app/components/content/inspiration_card_component.rb
@@ -0,0 +1,23 @@
+class Content::InspirationCardComponent < ViewComponent::Base
+  attr_reader :title, :description, :path, :heading_tag, :image
+
+  include ContentHelper
+
+  def initialize(card:, heading_tag: "h2")
+    @title       = substitute_values(card.title)
+    @description = substitute_values(card.description)
+    @path        = card.path
+    @heading_tag = heading_tag
+    @image       = card.image if card.respond_to?(:image)
+
+    super
+  end
+
+  def image?
+    image.present?
+  end
+
+  def image_tag
+    helpers.image_pack_tag(image, **helpers.image_alt_attribs(image))
+  end
+end
