573632b688296fc57914ec412d45b57494bdd041
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2024-09-19 16:47:24 +0100

    removed content from .rb file to .erb file

--- app/components/content/subject_page_content_component.rb
+++ app/components/content/subject_page_content_component.rb
@@ -1,14 +1,13 @@
 # Description: This component is used to display the content for the subject page.
 module Content
   class SubjectPageContentComponent < ViewComponent::Base
-    attr_reader :subject_name, :salary, :salary_in_london
+    attr_reader :subject_name
 
     include ContentHelper
 
-    def initialize(subject_name:, salary_in_london: nil)
+    def initialize(subject_name:)
       super
       @subject_name = substitute_values(subject_name)
-      @salary_in_london = salary_in_london || "or higher in London"
     end
   end
 end
