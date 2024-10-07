71966b7c4b01da205debcd65c0d7de522e0c6a03
Spencer Dixon <spencerlloyddixon@gmail.com>
2024-10-07 16:54:14 +0100

    WIP can toggle quotes

--- app/components/content/quote_with_image_component.rb
+++ app/components/content/quote_with_image_component.rb
@@ -1,8 +1,8 @@
 module Content
   class QuoteWithImageComponent < ViewComponent::Base
-    attr_reader :title, :text, :image, :heading, :color
+    attr_reader :title, :text, :image, :heading, :color, :quotes
 
-    def initialize(title:, text:, image: nil, heading: :m, color: "pink")
+    def initialize(title:, text:, image: nil, heading: :m, color: "pink", quotes: true)
       super
 
       @title = title
@@ -10,18 +10,19 @@ module Content
       @image = image
       @heading = heading
       @color = color
+      @quotes = quotes
     end
 
     def image
       image_pack_tag(@image, **helpers.image_alt_attribs(@image)) if @image
     end
 
-    def heading_classes
-      %w[].tap do |c|
-        c << "heading-#{heading}"
-        c << "heading--box-#{color}" unless color == "transparent"
-      end
-    end
+    # def heading_classes
+    #   %w[].tap do |c|
+    #     c << "heading-#{heading}"
+    #     c << "heading--box-#{color}" unless color == "transparent"
+    #   end
+    # end
   end
 end
 
