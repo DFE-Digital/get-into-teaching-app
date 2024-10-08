0a7b7b67436d61752e03063ddd3cb677a7b330a1
Spencer Dixon <spencerlloyddixon@gmail.com>
2024-10-08 10:45:01 +0100

    Add ability to change background

--- app/components/content/quote_with_image_component.rb
+++ app/components/content/quote_with_image_component.rb
@@ -1,8 +1,8 @@
 module Content
   class QuoteWithImageComponent < ViewComponent::Base
-    attr_reader :title, :text, :image, :heading, :color, :quotes, :background_color,
+    attr_reader :title, :text, :image, :heading, :color, :quotes, :background_color
 
-    def initialize(title:, text:, image: nil, heading: :m, color: "pink", background_color: "grey", quotes: true)
+    def initialize(title:, text:, image: nil, heading: :m, color: "pink", background_color: "blue", quotes: true)
       super
 
       @title = title
