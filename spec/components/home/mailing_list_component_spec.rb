20e1a482d36a34c749d13fe18c11761a9a7c0a9b
mariosh-e <166138805+mariosh-e@users.noreply.github.com>
2024-10-09 09:44:45 +0100

    Apply suggestions from code review
    changed to 'opens in new tab'

--- spec/components/home/mailing_list_component_spec.rb
+++ spec/components/home/mailing_list_component_spec.rb
@@ -11,5 +11,5 @@ describe Home::MailingListComponent, type: :component do
   it { is_expected.to have_css(".home-mailing-list") }
   it { is_expected.to have_css("h2") }
   it { is_expected.to have_css("form") }
-  it { is_expected.to have_link("privacy notice (opens in a new tab)", href: "/privacy-policy?id=123") }
+  it { is_expected.to have_link("privacy notice (opens in new tab)", href: "/privacy-policy?id=123") }
 end
