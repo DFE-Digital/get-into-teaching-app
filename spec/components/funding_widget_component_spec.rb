c948e1e8ebda83fa7175e7fa9dfc17faef8e4899
emmaachesongray <168454683+emmaachesongray@users.noreply.github.com>
2024-10-10 14:18:15 +0000

    update figures

--- spec/components/funding_widget_component_spec.rb
+++ spec/components/funding_widget_component_spec.rb
@@ -56,7 +56,7 @@ RSpec.describe FundingWidgetComponent, type: :component do
     context "when subject is maths" do
       before { render_inline(component) }
 
-      it "contains subject-specific funding content", skip: "Temporarily removed" do
+      it "contains subject-specific funding content" do
         expect(page).to have_text("Scholarships of £30,000 and bursaries of £28,000 are available for trainee maths teachers if you're eligible.")
       end
     end
@@ -72,7 +72,7 @@ RSpec.describe FundingWidgetComponent, type: :component do
         expect(page).to have_text("Maths - starting salary: £31,650")
       end
 
-      it "substitutes the variable for a value in funding content", skip: "Temporarily removed" do
+      it "substitutes the variable for a value in funding content" do
         expect(page).to have_text("Bursaries of £28,000 are available.")
       end
     end
