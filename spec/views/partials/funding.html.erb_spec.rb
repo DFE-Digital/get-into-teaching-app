8e995b97f25e7c4e0189c644371171dcc28a2cf7
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2024-10-23 13:51:37 +0100

    Update funding.html.erb_spec.rb

--- spec/views/partials/funding.html.erb_spec.rb
+++ spec/views/partials/funding.html.erb_spec.rb
@@ -5,7 +5,7 @@ describe "_funding.html.erb" do
   let(:salary_value) { YAML.load_file("config/values/salaries.yml").dig("burseries", "postgraduate", "chemistry") }
 
   subject do
-    assign(:front_matter, { "subject" => subject_name, "veteran" => veteran_status })
+    assign(:front_matter, { "subject" => subject_name })
     assign(:bursaries_postgraduate_chemistry, "£25,000")
     assign(:scholarships_chemistry, "£10,000")
     render partial: "content/partials/teaching/funding"
