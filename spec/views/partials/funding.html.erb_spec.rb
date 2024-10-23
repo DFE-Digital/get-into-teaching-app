b3bbb5882e372bfadaccb0988a2b689b9555876b
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2024-10-23 10:38:24 +0100

    removed veteran logic from funding partial and test / created test for benefits partial

--- spec/views/partials/funding.html.erb_spec.rb
+++ spec/views/partials/funding.html.erb_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 describe "_funding.html.erb" do
   let(:subject_name) { "chemistry" }
-  let(:veteran_status) { "true" }
   let(:salary_value) { YAML.load_file("config/values/salaries.yml").dig("burseries", "postgraduate", "chemistry") }
 
   subject do
@@ -17,11 +16,4 @@ describe "_funding.html.erb" do
   it { is_expected.to have_link("extra funding and support", href: "/funding-and-support") }
   it { is_expected.to have_text(subject_name) }
   it { is_expected.to have_text(salary_value) }
-  it { is_expected.to include("a veteran") }
-
-  context "when veteran status is false" do
-    let(:veteran_status) { false }
-
-    it { is_expected { subject }.not_to include("a veteran") }
-  end
 end
