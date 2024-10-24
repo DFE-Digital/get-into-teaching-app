1eb8f6c8307b9bcdd0b6c60b5f25c77f87f5d8ac
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2024-10-24 11:24:30 +0100

    created test for advisor-promo partial to check subject article is rendered correctly

---
+++ spec/views/content/shared/advisor_promo.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "_advisor-promo.html.erb" do
+  subject do
+    assign(:front_matter, { "subject" => subject_name })
+    render partial: "content/shared/subject-pages/adviser-promo"
+    rendered
+  end
+
+  context "when a subject starts with a consenant it renders 'a'" do
+    let(:subject_name) { "chemistry" }
+
+    it { is_expected { subject }.to include("a chemistry teacher") }
+  end
+
+  context "when a subject starts with a vowel it renders 'an'" do
+    let(:subject_name) { "art" }
+
+    it { is_expected { subject }.to include("an art teacher") }
+  end
+end
