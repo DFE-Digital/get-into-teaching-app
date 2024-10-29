2e92710e3d55fa9be71737cda5774d7c5e73c749
Spencer Dixon <spencerlloyddixon@gmail.com>
2024-10-29 16:09:52 +0000

    Remove old footer spec

--- spec/components/footer_component_spec.rb
+++ spec/components/footer_component_spec.rb
@@ -16,12 +16,4 @@ describe FooterComponent, type: "component" do
   specify "renders the cookie acceptance popup" do
     expect(page).to have_css(".cookie-acceptance")
   end
-
-  context "when 'Talk to us' is disabled" do
-    subject! { render_inline(described_class.new(talk_to_us: false)) }
-
-    specify "does not render the 'Talk to us' section" do
-      expect(page).not_to have_css(".talk-to-us")
-    end
-  end
 end
