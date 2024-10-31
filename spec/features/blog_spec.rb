f9c4f810e658495985cbdb86456397370b301f8b
emmaachesongray <168454683+emmaachesongray@users.noreply.github.com>
2024-10-31 11:16:12 +0000

    remove advisers tag spec test

--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -37,7 +37,6 @@ describe "reading the blog", type: :feature do
   end
 
   include_context "paginating blog posts", "/blog", 10, true
-  include_context "paginating blog posts", "/blog/tag/advisers", 2, false
 
   scenario "viewing a post" do
     path = "my-career-change-to-teaching"
