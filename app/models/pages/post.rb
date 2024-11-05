44e8d8de9e4d6216c58aa8c0316983dbcd396646
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-11-05 16:05:48 +0000

    Update post.rb

--- app/models/pages/post.rb
+++ app/models/pages/post.rb
@@ -1,16 +1,11 @@
 module Pages
   class Post < Page
     def validate!
-      invalid_tags = frontmatter.tags - tags_whitelist
+      invalid_tags = frontmatter.tags
 
-      message = "These tags are not defined in tags.yml: #{invalid_tags.to_sentence}"
+      message = "Do not use tags in your post: #{invalid_tags.to_sentence}"
       raise ContentError.new(message, "#tags") if invalid_tags.any?
     end
 
-  private
-
-    def tags_whitelist
-      @tags_whitelist ||= YAML.load_file(Rails.root.join("config/tags.yml"))
-    end
   end
 end
