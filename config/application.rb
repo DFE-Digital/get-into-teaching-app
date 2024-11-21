9a0ee8b0794f08ba543d65fd137d93609eff311a
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-11-21 14:30:03 +0000

    Revert "Upgrade ruby v3 3 (#4357)"
    This reverts commit 6c7ce27124b3e1cee0828dd96eb3c62f5a68b39a.

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,6 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# Patch to Rack::PageCaching::Utils to substitute Fixnum --> Integer
-require_relative "../lib/patch_rack_page_caching"
-
 module GetIntoTeachingWebsite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
