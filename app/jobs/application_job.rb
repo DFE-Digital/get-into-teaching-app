643b19ea24c794f18ebf9ce28f3b9298fe9acba2
Spencer Dixon <spencerlloyddixon@gmail.com>
2024-11-25 15:42:49 +0000

    Fix retry behaviour for active job

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,5 +1,5 @@
 class ApplicationJob < ActiveJob::Base
   retry_on ActiveRecord::Deadlocked
-  retry_on StandardError, wait: :polynomially_longer, attempts: 3
+  retry_on StandardError, wait: :exponentially_longer, attempts: 3
   discard_on ActiveJob::DeserializationError
 end
