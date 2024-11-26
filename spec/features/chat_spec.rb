f6c38a208bfee3aa1fb663d3300f97b02b0bcca7
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-11-26 12:14:31 +0000

    Update chat_spec.rb

--- spec/features/chat_spec.rb
+++ spec/features/chat_spec.rb
@@ -3,6 +3,20 @@ require "rails_helper"
 RSpec.feature "Chat", type: :feature do
   include_context "when requesting a page with the Get Into Teaching events badge"
 
+  # before do
+  #   allow(ENV).to receive(:fetch).and_call_original
+  #   allow(ENV).to receive(:fetch).with("CHAT_AVAILABILITY_API", nil).and_return("http://api.example/")
+  #   stub_request(:get, "http://api.example/")
+  #     .to_return(status: 200, body: "{\"skillid\": 123456, \"available\": false, \"status_age\": 123 }")
+  # end
+  #
+  # scenario "test" do
+  #   puts "TEST1g"
+  #   visit root_path
+  #   puts "TEST2g"
+  # end
+
+
   before do
     allow(ENV).to receive(:fetch).and_call_original
     allow(ENV).to receive(:fetch).with("CHAT_AVAILABILITY_API", nil).and_return("http://api.example/")
@@ -10,21 +24,7 @@ RSpec.feature "Chat", type: :feature do
       .to_return(status: 200, body: "{\"skillid\": 123456, \"available\": false, \"status_age\": 123 }")
   end
 
-  scenario "test" do
-    puts "TEST1g"
-    visit root_path
-    puts "TEST2g"
-  end
-
-
-  # before do
-  #   allow(ENV).to receive(:fetch).and_call_original
-  #   allow(ENV).to receive(:fetch).with("CHAT_AVAILABILITY_API", nil).and_return("http://api.example/")
-  #   stub_request(:get, "http://api.example/")
-  #     .to_return(status: 200, body: "{\"skillid\": 123456, \"available\": false, \"status_age\": 123 }")
-  # end
-
-  # context "when javascript is enabled", :js do
+  context "when javascript is enabled", :js do
   #   # context "when agents are available" do
   #   #   before do
   #   #     stub_request(:get, "http://api.example/")
@@ -47,31 +47,31 @@ RSpec.feature "Chat", type: :feature do
   #   #   end
   #   # end
   #
-  #   context "when agents are not available" do
-  #     before do
-  #       stub_request(:get, "http://api.example/")
-  #         .to_return(status: 200, body: "{\"skillid\": 123456,	\"available\": false, \"status_age\": 123 }")
-  #     end
-  #
-  #     scenario "viewing the chat section of the talk to us component" do
-  #       puts "TEST A: #{ENV["CHAT_AVAILABILITY_API"]}"
-  #       # puts "TEST B: #{ENV.fetch("CHAT_AVAILABILITY_API")}"
-  #       puts "TEST C: #{ENV.fetch("CHAT_AVAILABILITY_API", nil)}"
-  #       puts "TEST D: #{Chat.new.availability_api_uri}"
-  #       puts "TEST E: #{Chat.new.availability}"
-  #
-  #       puts "TEST1"
-  #       visit root_path
-  #       puts "TEST2"
-  #       dismiss_cookies
-  #       puts "TEST3c"
-  #
-  #       within(".talk-to-us") do
-  #         expect(page).to have_text("Chat is closed")
-  #       end
-  #     end
-  #   end
-  # end
+    context "when agents are not available" do
+      before do
+        stub_request(:get, "http://api.example/")
+          .to_return(status: 200, body: "{\"skillid\": 123456,	\"available\": false, \"status_age\": 123 }")
+      end
+
+      scenario "viewing the chat section of the talk to us component" do
+        puts "TEST A: #{ENV["CHAT_AVAILABILITY_API"]}"
+        # puts "TEST B: #{ENV.fetch("CHAT_AVAILABILITY_API")}"
+        puts "TEST C: #{ENV.fetch("CHAT_AVAILABILITY_API", nil)}"
+        puts "TEST D: #{Chat.new.availability_api_uri}"
+        puts "TEST E: #{Chat.new.availability}"
+
+        puts "TEST1h"
+        visit root_path
+        puts "TEST2h"
+        dismiss_cookies
+        puts "TEST3h"
+
+        within(".talk-to-us") do
+          expect(page).to have_text("Chat is closed")
+        end
+      end
+    end
+  end
 
   # context "when javascript is not enabled", js: false do
   #   scenario "viewing the chat section of the talk to us component" do
@@ -84,9 +84,9 @@ RSpec.feature "Chat", type: :feature do
   #   end
   # end
 
-  # def dismiss_cookies
-  #   click_link "Accept all cookies"
-  # end
+  def dismiss_cookies
+    click_link "Accept all cookies"
+  end
 
 
   # def visit_on_date(path)
