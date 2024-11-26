054cec27350b74013677976fe9ac02a0b0e12337
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-11-26 14:18:37 +0000

    Update chat_spec.rb

--- spec/features/chat_spec.rb
+++ spec/features/chat_spec.rb
@@ -5,10 +5,10 @@ RSpec.feature "Chat", type: :feature do
 
   context "when javascript is enabled", :js do
     before do
-    #   allow(ENV).to receive(:fetch).and_call_original
-    #   allow(ENV).to receive(:fetch).with("CHAT_AVAILABILITY_API", nil).and_return("http://api.example/")
-    #   stub_request(:get, "http://api.example/")
-    #     .to_return(status: 200, body: "{\"skillid\": 123456, \"available\": false, \"status_age\": 123 }")
+      allow(ENV).to receive(:fetch).and_call_original
+      allow(ENV).to receive(:fetch).with("CHAT_AVAILABILITY_API", nil).and_return("http://api.example/")
+      stub_request(:get, "http://api.example/")
+        .to_return(status: 200, body: "{\"skillid\": 123456, \"available\": false, \"status_age\": 123 }")
     end
 
     scenario "test" do
