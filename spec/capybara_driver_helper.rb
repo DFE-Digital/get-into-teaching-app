4aa33f2a8ccf70c14e5cb0ce507cf6a1e6d3dfe1
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-11-26 15:06:46 +0000

    force chrome version

--- spec/capybara_driver_helper.rb
+++ spec/capybara_driver_helper.rb
@@ -9,6 +9,7 @@ Capybara.register_driver JS_DRIVER do |app|
   options.add_argument("--no-sandbox")
   options.add_argument("--disable-dev-shm-usage")
   options.add_argument("--window-size=1400,1400")
+  options.browser_version = "127"
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
--- spec/features/chat_spec.rb
+++ spec/features/chat_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.feature "Chat", type: :feature do
   include_context "when requesting a page with the Get Into Teaching events badge"
 
-  context "when javascript is enabled", :selenium_chrome do
+  context "when javascript is enabled", :js do
     scenario "test" do
       puts "TEST 1"
       visit root_path
