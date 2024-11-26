01a0712fbab52f411161147e9143f4722f278f15
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-11-26 11:50:48 +0000

    Update chat_spec.rb

--- spec/features/chat_spec.rb
+++ spec/features/chat_spec.rb
@@ -3,10 +3,17 @@ require "rails_helper"
 RSpec.feature "Chat", type: :feature do
   include_context "when requesting a page with the Get Into Teaching events badge"
 
+  before do
+    allow(ENV).to receive(:fetch).and_call_original
+    allow(ENV).to receive(:fetch).with("CHAT_AVAILABILITY_API", nil).and_return("http://api.example/")
+    stub_request(:get, "http://api.example/")
+      .to_return(status: 200, body: "{\"skillid\": 123456, \"available\": false, \"status_age\": 123 }")
+  end
+
   scenario "test" do
-    puts "TEST1c"
+    puts "TEST1d"
     visit root_path
-    puts "TEST2c"
+    puts "TEST2d"
   end
 
 
