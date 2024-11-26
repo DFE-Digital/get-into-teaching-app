1d9a1f2484e6256e8bfee6ed99b8dffef617ffa5
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-11-26 14:57:35 +0000

    Update chat_spec.rb

--- spec/features/chat_spec.rb
+++ spec/features/chat_spec.rb
@@ -3,14 +3,7 @@ require "rails_helper"
 RSpec.feature "Chat", type: :feature do
   include_context "when requesting a page with the Get Into Teaching events badge"
 
-  context "when javascript is enabled", :js do
-    before do
-      allow(ENV).to receive(:fetch).and_call_original
-      allow(ENV).to receive(:fetch).with("CHAT_AVAILABILITY_API", nil).and_return("http://api.example/")
-      stub_request(:get, "http://api.example/")
-        .to_return(status: 200, body: "{\"skillid\": 123456, \"available\": false, \"status_age\": 123 }")
-    end
-
+  context "when javascript is enabled", :selenium_chrome do
     scenario "test" do
       puts "TEST 1"
       visit root_path
