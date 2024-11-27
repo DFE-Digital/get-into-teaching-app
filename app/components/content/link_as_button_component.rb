f15e84f3a4676939ce044c84bed0fd75f918eb0f
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2024-11-27 11:56:16 +0000

    updated params in homepage, purple box, cookie acceptance component when rendering LinkAsButton component, updated LinkAsButton test

--- app/components/content/link_as_button_component.rb
+++ app/components/content/link_as_button_component.rb
@@ -1,30 +1,30 @@
 module Content
   class LinkAsButtonComponent < ViewComponent::Base
-    attr_reader :link_text, :url, :options
+    attr_reader :link_text, :href, :tabindex, :data, :id, :classes
 
     include ContentHelper
 
-    def initialize(link_text:, url:, options: {})
+    def initialize(link_text:, href:, tabindex: nil, data: {}, id: nil, css_class: [], **kwargs)
       super
 
+      raise ArgumentError, "Unhandled attributes: #{kwargs.keys.join(', ')}" if kwargs.any?
+
       @link_text = substitute_values(link_text)
-      @url = url
-      @options = options
+      @href = href
+      @tabindex = tabindex
+      @data = data
+      @id = id
+      @classes = css_class.is_a?(Array) ? css_class : [css_class]
     end
 
     def combined_options
-      allowed_keys = %i[class tabindex data id]
-      filtered_options = options.slice(*allowed_keys)
-
-      disallowed_keys = options.keys - allowed_keys
-      if disallowed_keys.any?
-        raise ArgumentError, "Unexpected attributes: #{disallowed_keys.join(', ')}"
-      end
-
-      filtered_options.merge(
-        class: ["button", options[:class]].compact.join(" "),
+      {
+        class: ["button", *classes].compact.join(" "),
+        tabindex: tabindex,
+        data: data,
+        id: id,
         role: "button",
-      )
+      }.compact
     end
   end
 end
