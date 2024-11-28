1a0e8ad39bcaf28715db738886153bd930f15530
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-11-28 17:50:57 +0000

    update colours and styling + add tests

--- app/components/calls_to_action/adviser_component.rb
+++ app/components/calls_to_action/adviser_component.rb
@@ -1,17 +1,19 @@
 class CallsToAction::AdviserComponent < ViewComponent::Base
-  attr_reader :title, :text, :image, :button_text
+  attr_reader :title, :text, :image, :link_text, :link_target
 
   def initialize(
     title: "Get free one-to-one support",
     text: "An adviser with years of teaching experience can help you to become a maths teacher. Chat by phone, text, or email as little or often as you need.",
     image: "static/images/adviser-black.png",
-    button_text: "Find out more about advisers"
+    link_text: "Find out more about advisers",
+    link_target: "/teacher-training-advisers"
   )
     super
 
     @title = title
     @text = text
     @image = image
-    @button_text = button_text
+    @link_text = link_text
+    @link_target = link_target
   end
 end
