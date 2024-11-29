fb6426494741e946482002b44cf7eb705eb14e03
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-11-29 12:09:57 +0000

    Update app/components/calls_to_action/adviser_component.rb
    Co-authored-by: Gemma Dallman <87642394+gemmadallmandfe@users.noreply.github.com>

--- app/components/calls_to_action/adviser_component.rb
+++ app/components/calls_to_action/adviser_component.rb
@@ -3,7 +3,7 @@ class CallsToAction::AdviserComponent < ViewComponent::Base
 
   def initialize(
     title: "Get free one-to-one support",
-    text: "An adviser with years of teaching experience can help you to become a maths teacher. Chat by phone, text, or email as little or often as you need.",
+    text: "An adviser with years of teaching experience can help you to become a teacher. Chat by phone, text, or email as little or often as you need.",
     image: "static/images/adviser-black.png",
     link_text: "Find out more about advisers",
     link_target: "/teacher-training-advisers"
