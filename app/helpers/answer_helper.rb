b3623cb5ba5105576ebc5976addd046e2d12bbf2
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2024-12-02 15:01:18 +0000

    adapted AnswerHelper to include midday and midnight and to reflect changes to time format

--- app/helpers/answer_helper.rb
+++ app/helpers/answer_helper.rb
@@ -8,14 +8,16 @@ module AnswerHelper
     when Time
       formatted_time = answer.in_time_zone.to_formatted_s(:govuk_time_with_period)
 
-      # Convert "12:00pm" to "midday" as per GDS guidance
-      answer = if formatted_time == "12:00pm"
+      # Convert "12:00pm" to "midday" and "12:00am" to "midnight" as per GDS guidance
+      answer = case formatted_time
+               when "12:00pm"
                  "midday"
+               when "12:00am"
+                 "midnight"
                else
                  formatted_time
                end
     end
-
     safe_format(answer.to_s, wrapper_tag: "span")
   end
 
