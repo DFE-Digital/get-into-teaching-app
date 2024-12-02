0297843369bf9b253f2eebcc24ded210fdae168c
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2024-12-02 14:30:49 +0000

    adapted AnswerHelper to change time format as per GDS guidance

--- app/helpers/answer_helper.rb
+++ app/helpers/answer_helper.rb
@@ -6,7 +6,14 @@ module AnswerHelper
     when Date
       answer = answer.to_formatted_s(:govuk_date)
     when Time
-      answer = answer.in_time_zone.to_formatted_s(:govuk_time)
+      formatted_time = answer.in_time_zone.to_formatted_s(:govuk_time_with_period)
+
+      # Convert "12:00pm" to "midday" as per GDS guidance
+      answer = if formatted_time == "12:00pm"
+                 "midday"
+               else
+                 formatted_time
+               end
     end
 
     safe_format(answer.to_s, wrapper_tag: "span")
