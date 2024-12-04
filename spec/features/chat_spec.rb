41bf0cd03dda5e9144276b1d65fb3687dba13844
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-12-04 09:12:27 +0000

    Rolling back Ruby 3.3 again.... (#4401)

--- spec/features/chat_spec.rb
+++ spec/features/chat_spec.rb
@@ -6,44 +6,66 @@ RSpec.feature "Chat", type: :feature do
   before do
     allow(ENV).to receive(:fetch).and_call_original
     allow(ENV).to receive(:fetch).with("CHAT_AVAILABILITY_API", nil).and_return("http://api.example/")
+    stub_request(:get, "http://api.example/")
+      .to_return(status: 200, body: "{\"skillid\": 123456, \"available\": false, \"status_age\": 123 }")
   end
 
-  context "when agents are available" do
-    before do
-      stub_request(:get, "http://api.example/")
-        .to_return(status: 200, body: "{\"skillid\": 123456,	\"available\": true, \"status_age\": 123 }")
-    end
-
-    scenario "viewing the chat section of the talk to us component" do
-      visit root_path
-      dismiss_cookies
-
-      within(".talk-to-us") do
-        click_link("Open chat in new tab")
+  context "when javascript is enabled", :js do
+    context "when agents are available" do
+      before do
+        stub_request(:get, "http://api.example/")
+          .to_return(status: 200, body: "{\"skillid\": 123456,	\"available\": true, \"status_age\": 123 }")
       end
 
-      expect(page.driver.current_url).to end_with("/chat#root")
-      expect(page).to have_css("#root", visible: false)
+      scenario "viewing the chat section of the talk to us component" do
+        visit root_path
+        dismiss_cookies
+
+        within(".talk-to-us") do
+          click_link("Open chat in new tab")
+        end
+
+        popup_window_handle = (page.driver.browser.window_handles - [page.driver.current_window_handle]).first
+        page.driver.switch_to_window(popup_window_handle)
+        expect(page.driver.current_url).to end_with("/chat")
+        expect(page).to have_css("#root", visible: false)
+        page.driver.close_window(popup_window_handle)
+      end
+    end
+
+    context "when agents are not available" do
+      before do
+        stub_request(:get, "http://api.example/")
+          .to_return(status: 200, body: "{\"skillid\": 123456,	\"available\": false, \"status_age\": 123 }")
+      end
+
+      scenario "viewing the chat section of the talk to us component" do
+        visit root_path
+        dismiss_cookies
+
+        within(".talk-to-us") do
+          expect(page).to have_text("Chat is closed")
+        end
+      end
     end
   end
 
-  context "when agents are not available" do
-    before do
-      stub_request(:get, "http://api.example/")
-        .to_return(status: 200, body: "{\"skillid\": 123456, \"available\": false, \"status_age\": 123 }")
-    end
-
+  context "when javascript is not enabled", js: false do
     scenario "viewing the chat section of the talk to us component" do
       visit root_path
       dismiss_cookies
 
       within(".talk-to-us") do
-        expect(page).to have_text("Chat is closed")
+        expect(page).to have_text("Chat not available")
       end
     end
   end
 
   def dismiss_cookies
     click_link "Accept all cookies"
+  end
+
+  def visit_on_date(path)
+    visit "#{path}?fake_browser_time=#{date.to_i * 1000}"
   end
 end
