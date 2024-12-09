5548aa4079b33cbfd4115c68ee0002ee80312d5a
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2024-12-09 13:35:15 +0000

    added border class to ResultsBoxComponent/made title optional to allow for toggling of border colours and title

--- app/components/content/results_box_component.rb
+++ app/components/content/results_box_component.rb
@@ -1,19 +1,28 @@
 module Content
   class ResultsBoxComponent < ViewComponent::Base
-    attr_reader :title, :heading, :fee, :course_length, :funding, :text, :cta
+    attr_reader :title, :heading, :fee, :course_length, :funding, :text, :border_color, :show_title
 
     include ContentHelper
 
-    def initialize(title:, heading:, fee:, course_length:, funding:, text:, cta:)
+    def initialize(heading:, fee:, course_length:, funding:, text:, border_color: :grey, show_title: false, title: nil)
       super
 
-      @title = substitute_values(title)
+      if show_title && title.nil?
+        raise ArgumentError, "Title must be provided when show_title is true"
+      end
+
+      @title = show_title ? substitute_values(title) : nil
       @heading = substitute_values(heading)
       @fee = substitute_values(fee)
       @course_length = substitute_values(course_length)
       @funding = substitute_values(funding)
       @text = substitute_values(text)
-      @cta = cta
+      @border_color = border_color
+      @show_title = show_title
+    end
+
+    def border_class
+      @border_color == :yellow ? "results-box--yellow" : "results-box--grey"
     end
   end
 end
