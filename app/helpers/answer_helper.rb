efd882ed219f528c5987271f321d6c6a056853c8
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2024-12-31 10:29:57 +0000

    reverted back to start_at time formatting and midday/midnight logic for the time being

--- app/helpers/answer_helper.rb
+++ app/helpers/answer_helper.rb
@@ -6,24 +6,17 @@ module AnswerHelper
     when Date
       answer = answer.to_formatted_s(:govuk_date)
     when Time
-      answer
-      # time_slot = find_callback_time_slot(answer)
-      # answer = time_slot[0] || answer.to_formatted_s(:govuk_time_with_period) # if any issues with matching process it will default to the formatted start time e.g. 10:30am
+      answer = if answer.hour.zero? && answer.min.zero?
+                 "midnight"
+               elsif answer.hour == 12 && answer.min.zero?
+                 "midday"
+               else
+                 answer.in_time_zone.to_formatted_s(:govuk_time_with_period)
+               end
     end
 
     safe_format(answer.to_s, wrapper_tag: "span")
   end
-
-  # def find_callback_time_slot(selected_time)
-  #   options = callback_options(Callbacks::Steps::Callback.callback_booking_quotas)
-
-  #   options.each_value do |time_slots|
-  #     time_slots.each do |time_slot|
-  #       return time_slot if time_slot[1] == selected_time
-  #     end
-  #   end
-  #   selected_time
-  # end
 
   def link_to_change_answer(step, question)
     link_to(teacher_training_adviser_step_path(step.key)) do
