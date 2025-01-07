082f8c8fdff0bd5f8563adb7ed35a2434cc49709
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-01-07 09:20:07 +0000

    WIP move logic to routes class

--- app/models/routes_into_teaching/routes.rb
+++ app/models/routes_into_teaching/routes.rb
@@ -9,8 +9,12 @@ module RoutesIntoTeaching
         next false if teaching_route["matches"].blank?
 
         teaching_route["matches"].all? do |matching_rule|
+<<<<<<< HEAD
           question_id = matching_rule["question"]
           matching_answers = matching_rule["answers"]
+=======
+          question_id, matching_answers = matching_rule["question"], matching_rule["answers"]
+>>>>>>> 3b7c2b82c (WIP move logic to routes class)
 
           matching_answers.include?("*") || matching_answers.include?(answers_hash[question_id])
         end
