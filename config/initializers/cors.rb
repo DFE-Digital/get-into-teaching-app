870c52205eb00052a944924b7effd95c8e4d00bc
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-02-06 13:32:38 +0000

    merged code in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,3 +1,18 @@
-# We don't allow CORS but use the Rack::Cors middleware to correctly
-# serve the preflight OPTIONS requests from browsers.
+# Be sure to restart your server when you modify this file.
+
+# Avoid CORS issues when API is called from the frontend app.
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+
+# Read more: https://github.com/cyu/rack-cors
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors
+
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins "example.com"
+#
+#     resource "*",
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#   end
+# end
