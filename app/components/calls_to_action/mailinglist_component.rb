36245f9b4dcbe38d69ebc2d94c01faf94ee960ea
jenhadfield-dfe <119432491+jenhadfield-dfe@users.noreply.github.com>
2025-02-07 12:46:44 +0000

    default title update

--- app/components/calls_to_action/mailinglist_component.rb
+++ app/components/calls_to_action/mailinglist_component.rb
@@ -2,7 +2,7 @@ class CallsToAction::MailinglistComponent < ViewComponent::Base
   attr_reader :title, :text, :image, :link_text, :link_target, :classes, :border
 
   def initialize(
-    title: "Find out more about teaching",
+    title: "Find out more about getting into teaching",
     text: "Find out about nearby events, what teaching is really like, and how to get one-to-one support.",
     image: "images/content/hero-images/geography2.jpg",
     link_text: "Get tailored advice in your inbox",
