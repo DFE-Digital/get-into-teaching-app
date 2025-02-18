ad87be0c8fc3731e02eb26d837b2c3207d7ba275
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-02-18 14:21:50 +0000

    Rubocop linting

--- spec/presenters/picklist_items_api_presenter_spec.rb
+++ spec/presenters/picklist_items_api_presenter_spec.rb
@@ -1,23 +1,24 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe PicklistItemsApiPresenter do
-  let(:mock_api) { double('GetIntoTeachingApiClient::PickListItemsApi') }
+  let(:mock_api) { double("GetIntoTeachingApiClient::PickListItemsApi") }
   let(:presenter) { described_class.new(mock_api) }
 
-  describe '#get_candidate_journey_stages' do
+  describe "#get_candidate_journey_stages" do
     let(:allowed_ids) { [222_750_000, 222_750_003] }
     let(:api_response) do
       [
-        double('Item', id: 222_750_000, name: 'Allowed Stage 1'),
-        double('Item', id: 222_750_003, name: 'Allowed Stage 2'),
-        double('Item', id: 222_750_999, name: 'Not Allowed Stage')
+        double("Item", id: 222_750_000, name: "Allowed Stage 1"),
+        double("Item", id: 222_750_003, name: "Allowed Stage 2"),
+        double("Item", id: 222_750_999, name: "Not Allowed Stage"),
       ]
     end
+
     before do
       allow(mock_api).to receive(:get_candidate_journey_stages).and_return(api_response)
     end
 
-    it 'returns only items with allowed IDs' do
+    it "returns only items with allowed IDs" do
       result = presenter.get_candidate_journey_stages
 
       expect(result.size).to eq(2)
