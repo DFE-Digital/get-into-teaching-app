d98bebf0cee8197ecff6bea0501c3c5935833739
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-02-19 09:46:28 +0000

    Format consideration journey pick list for event steps (#4581)
    * Format consideration journey pick list for event steps
    
    * Added pick list presenter to unify filtering of picklist items from the api
    
    * Rubocop linting
    
    * Added specs
    
    * Rubocop linting
    
    * Rename Picklist to PickList for consistency

--- app/helpers/option_set_helper.rb
+++ app/helpers/option_set_helper.rb
@@ -4,7 +4,7 @@ module OptionSetHelper
     I18n.t("helpers.answer.mailing_list_steps_degree_status.degree_status.#{key}", **{ default: option.value }.merge(Value.data))
   end
 
-  def format_option_value(option, translation_key = nil)
+  def format_option_value(option, translation_key)
     I18n.t("helpers.answer.#{translation_key}.#{option.id}", **{ default: option.value }.merge(Value.data))
   end
 end
--- app/models/mailing_list/steps/teacher_training.rb
+++ app/models/mailing_list/steps/teacher_training.rb
@@ -1,27 +1,17 @@
 module MailingList
   module Steps
     class TeacherTraining < ::GITWizard::Step
-      PICKLIST_ALLOW_IDS = [222_750_000, 222_750_003].freeze
-
       attribute :consideration_journey_stage_id, :integer
       validates :consideration_journey_stage_id,
                 presence: true,
                 inclusion: { in: :consideration_journey_stage_ids }
 
       def consideration_journey_stages
-        @consideration_journey_stages ||= query_consideration_journey_stages
+        @consideration_journey_stages ||= ::PickListItemsApiPresenter.new.get_candidate_journey_stages
       end
 
       def consideration_journey_stage_ids
         consideration_journey_stages.map { |option| option.id.to_i }
-      end
-
-    private
-
-      def query_consideration_journey_stages
-        GetIntoTeachingApiClient::PickListItemsApi.new.get_candidate_journey_stages.select do |option|
-          PICKLIST_ALLOW_IDS.include?(option.id)
-        end
       end
     end
   end
