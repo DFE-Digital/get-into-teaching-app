ae6bd98f56527ffc521e88f6a27cbe47996ae64c
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-02-19 16:15:54 +0000

    removed logger level code for test envrionment from rails_helper as no longer needed

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -52,12 +52,6 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.before(:suite) do
-    # Set the logger level for test environment to WARN; disable ActiveRecord SQL query logging
-    if Rails.env.test?
-      ActiveRecord::Base.logger.level = Logger::WARN
-    end
-  end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
