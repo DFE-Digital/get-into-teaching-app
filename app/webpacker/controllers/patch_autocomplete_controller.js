61d55e150b8d310a55f59ca845f03996b16569a2
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-03-19 15:16:49 +0000

    WIP patch dfe-autocomplete with stimulus controller to link error messages

---
+++ app/webpacker/controllers/patch_autocomplete_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = {
+    fieldId: String,
+    errorId: String
+  }
+
+  connect() {
+    const autocomplete = document.getElementById(this.fieldIdValue);
+
+    if (autocomplete) {
+      const currentDescribedBy = autocomplete.getAttribute('aria-describedby') || '';
+
+      autocomplete.setAttribute('aria-describedby', `${this.errorIdValue} ${currentDescribedBy}`.trim());
+    }
+  }
+}
