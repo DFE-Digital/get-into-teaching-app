1f8abcfe26107ca838e7eaa2d9b10a8659bf782e
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-03-19 14:16:53 +0000

    Remove title from inset component

--- app/components/content/inset_text_component.rb
+++ app/components/content/inset_text_component.rb
@@ -1,15 +1,14 @@
 module Content
   class InsetTextComponent < ViewComponent::Base
-    attr_reader :header, :title, :text, :color, :heading_tag
+    attr_reader :header, :text, :color, :heading_tag
 
     include ContentHelper
 
-    def initialize(text:, title: nil, header: nil, color: "yellow", heading_tag: "h2")
+    def initialize(text:, header: nil, color: "yellow", heading_tag: "h2")
       super
 
       @text = substitute_values(text)
-      @title = substitute_values(title)
-      @header = substitute_values(header.present? && title.present? ? "#{header}:" : header)
+      @header = substitute_values(header)
       @color = color
       @heading_tag = heading_tag
     end
--- spec/components/content/inset_text_component_spec.rb
+++ spec/components/content/inset_text_component_spec.rb
@@ -2,10 +2,9 @@ require "rails_helper"
 
 describe Content::InsetTextComponent, type: :component do
   let(:header) { "Header" }
-  let(:title) { "Title" }
   let(:text) { "Text" }
   let(:color) { "yellow" }
-  let(:component) { described_class.new(text: text, header: header, title: title, color: color) }
+  let(:component) { described_class.new(text: text, header: header, color: color) }
 
   subject do
     render_inline(component)
@@ -15,27 +14,11 @@ describe Content::InsetTextComponent, type: :component do
   it { is_expected.to have_css("section.inset-text.yellow") }
   it { is_expected.to have_css(".inset-text p", text: text) }
   it { is_expected.to have_css(".inset-text h2 span.header", text: "#{header}:") }
-  it { is_expected.to have_css(".inset-text h2 span.title", text: title) }
 
-  context "when there is no title nor header" do
-    let(:title) { nil }
-    let(:header) { nil }
-
-    it { is_expected.not_to have_css(".inset-text h2") }
-  end
-
-  context "when there is only a title but no header" do
-    let(:header) { nil }
-
-    it { is_expected.not_to have_css(".inset-text h2 span.header") }
-    it { is_expected.to have_css(".inset-text h2 span.title", text: title) }
-  end
-
-  context "when there is only a header but no title" do
+  context "when there is a header" do
     let(:title) { nil }
 
     it { is_expected.to have_css(".inset-text h2 span.header", text: header) }
-    it { is_expected.not_to have_css(".inset-text h2 span.title") }
   end
 
   context "when the text contains HTML" do
@@ -64,10 +47,10 @@ describe Content::InsetTextComponent, type: :component do
 
   context "when the heading_tag is overridden" do
     let(:custom_heading_tag) { "h4" }
-    let(:component) { described_class.new(text: text, header: header, title: title, color: color, heading_tag: custom_heading_tag) }
+    let(:component) { described_class.new(text: text, header: header, color: color, heading_tag: custom_heading_tag) }
 
     specify "the custom heading tag is used" do
-      expect(subject).to have_css(".inset-text #{custom_heading_tag}[role=\"text\"]", text: title)
+      expect(subject).to have_css(".inset-text #{custom_heading_tag}[role=\"text\"]", text: header)
     end
   end
 end
