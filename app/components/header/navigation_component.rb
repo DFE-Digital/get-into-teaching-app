dc5ce31d36f28524cebce326323a8ad8cb61a236
Martyn Whitwell <martyn.whitwell@gmail.com>
2025-04-09 14:32:18 +0100

    refactored desktop menu
    work in progress

--- app/components/header/navigation_component.rb
+++ app/components/header/navigation_component.rb
@@ -20,25 +20,27 @@ module Header
 
   private
 
-    def corresponding_mode(mode)
-      mode == :mobile ? :desktop : :mobile
-    end
+    # def corresponding_mode(mode)
+    #   mode == :mobile ? :desktop : :mobile
+    # end
 
     def nav_link(resource, mode, underline_on_hover: true, role: "menuitem")
       title = resource.title
       path = resource.path
       li_id = "#{path.parameterize}-#{mode}"
-      corresponding_li_id = "#{path.parameterize}-#{corresponding_mode(mode)}"
       child_menu_id = category_list_id(resource, mode)
-      corresponding_child_menu_id = category_list_id(resource, corresponding_mode(mode))
-      child_menu_ids = [child_menu_id, corresponding_child_menu_id].join(" ")
+
+      #corresponding_li_id = "#{path.parameterize}-#{corresponding_mode(mode)}"
+      #corresponding_child_menu_id = category_list_id(resource, corresponding_mode(mode))
+      #child_menu_ids = [child_menu_id, corresponding_child_menu_id].join(" ")
+
       li_css = ("active" if uri_is_root?(path) || first_uri_segment_matches_link?(path)).to_s
       show_dropdown = resource.children?
       link_css = "menu-link link link--black link--no-underline #{underline_on_hover ? 'link--underline-on-hover' : ''}"
-      aria_attributes = show_dropdown ? { expanded: false, controls: child_menu_ids } : {}
-      tag.li id: li_id, class: li_css, data: { "corresponding-id": corresponding_li_id, "child-menu-id": child_menu_id, "corresponding-child-menu-id": corresponding_child_menu_id, "direct-link": !show_dropdown, "toggle-secondary-navigation": show_dropdown } do
+      aria_attributes = show_dropdown ? { expanded: false, controls: child_menu_id } : {}
+      tag.li id: li_id, class: li_css, data: { "child-menu-id": child_menu_id, "direct-link": !show_dropdown, "FIXME-toggle-secondary-navigation": show_dropdown } do
         safe_join([
-          link_to(path, class: link_css, role: role, aria: aria_attributes, data: { action: "keydown.enter->navigation#handleNavMenuClick keydown.tab->navigation#handleMenuTab" }) do
+          link_to(path, class: link_css, role: role, aria: aria_attributes, data: { action: "keydown.enter->navigation#handleNavMenuClick keydown.FIXME->navigation#handleMenuTab" }) do
             safe_join([
               tag.span(title, class: "menu-title"),
               contracted_icon(visible: show_dropdown),
@@ -47,7 +49,7 @@ module Header
           if mode == :mobile && resource.children?
             [
               row_break,
-              category_list(resource, mode, css_class: "category-links-list hidden-menu hidden-desktop"),
+              category_list(resource, mode, css_class: "category-links-list hidden-menu hidden-desktopXXX"),
             ]
           end,
         ])
@@ -55,18 +57,20 @@ module Header
     end
 
     def category_list(resource, mode, css_class:, role: "menu")
-      tag.ol(class: css_class, id: category_list_id(resource, mode), role: role) do
-        safe_join(
-          [
-            resource.children_without_subcategory.map do |child_resource|
-              nav_link(child_resource, mode)
-            end,
-            resource.subcategories.map do |category|
-              category_link(category, resource, mode)
-            end,
-            view_all_link(resource, mode),
-          ],
-        )
+      tag.div(class: "desktop-level2-wrapper") do
+        tag.ol(class: css_class, id: category_list_id(resource, mode), role: role) do
+          safe_join(
+            [
+              resource.children_without_subcategory.map do |child_resource|
+                nav_link(child_resource, mode)
+              end,
+              resource.subcategories.map do |category|
+                category_link(category, resource, mode)
+              end,
+              view_all_link(resource, mode),
+            ],
+          )
+        end
       end
     end
 
@@ -77,17 +81,19 @@ module Header
     def category_link(subcategory, resource, mode, role: "menuitem")
       title = subcategory
       li_id = "#{resource.path.parameterize}-#{title.parameterize}-#{mode}"
-      corresponding_li_id = "#{resource.path.parameterize}-#{title.parameterize}-#{corresponding_mode(mode)}"
       child_menu_id = page_list_id(resource, subcategory, mode)
-      corresponding_child_menu_id = page_list_id(resource, subcategory, corresponding_mode(mode))
-      child_menu_ids = [child_menu_id, corresponding_child_menu_id].join(" ")
+
+      #corresponding_li_id = "#{resource.path.parameterize}-#{title.parameterize}-#{corresponding_mode(mode)}"
+      #corresponding_child_menu_id = page_list_id(resource, subcategory, corresponding_mode(mode))
+      #child_menu_ids = [child_menu_id, corresponding_child_menu_id].join(" ")
+
       li_css = ("active" if subcategory == front_matter["subcategory"]).to_s
       link_css = "menu-link link link--black link--no-underline link--underline-on-hover btn-as-link"
-      aria_attributes = { expanded: false, controls: child_menu_ids }
-      tag.li id: li_id, class: li_css, data: { "corresponding-id": corresponding_li_id, "child-menu-id": child_menu_id, "corresponding-child-menu-id": corresponding_child_menu_id, "direct-link": false } do
+      aria_attributes = { expanded: false, controls: child_menu_id }
+      tag.li id: li_id, class: li_css, data: { "child-menu-id": child_menu_id, "direct-link": false } do
         safe_join(
           [
-            tag.button(type: "button", class: link_css, role: role, aria: aria_attributes, data: { action: "keydown.enter->navigation#handleNavMenuClick keydown.tab->navigation#handleMenuTab" }) do
+            tag.button(type: "button", class: link_css, role: role, aria: aria_attributes, data: { action: "keydown.enter->navigation#handleNavMenuClick keydown.FIXME->navigation#handleMenuTab" }) do
               safe_join(
                 [
                   tag.span(title, class: "menu-title"),
@@ -97,7 +103,7 @@ module Header
             end,
             row_break,
             if mode == :mobile
-              page_list(resource, subcategory, mode, css_class: "page-links-list hidden-menu hidden-desktop")
+              page_list(resource, subcategory, mode, css_class: "page-links-list hidden-menu hidden-desktopXXX")
             end,
           ],
         )
@@ -109,14 +115,16 @@ module Header
     end
 
     def page_list(resource, subcategory, mode, css_class:, role: "menu")
-      tag.ol(class: css_class, role: role, id: page_list_id(resource, subcategory, mode)) do
-        safe_join(
-          [
-            resource.children_in_subcategory(subcategory).map do |child_resource|
-              nav_link(child_resource, mode)
-            end,
-          ],
-        )
+      tag.div(class: "desktop-level3-wrapper") do
+        tag.ol(class: css_class, role: role, id: page_list_id(resource, subcategory, mode)) do
+          safe_join(
+            [
+              resource.children_in_subcategory(subcategory).map do |child_resource|
+                nav_link(child_resource, mode)
+              end,
+            ],
+          )
+        end
       end
     end
 
@@ -124,13 +132,14 @@ module Header
       title = "View all in #{resource.title}"
       path = resource.path
       li_id = "menu-view-all-#{path.parameterize}-#{mode}"
-      corresponding_li_id = "menu-view-all-#{path.parameterize}-#{corresponding_mode(mode)}"
       li_css = "view-all #{'active' if uri_is_root?(path)}"
       link_css = "menu-link link link--black"
 
-      tag.li class: li_css, id: li_id, data: { "corresponding-id": corresponding_li_id, "direct-link": true } do
+      #corresponding_li_id = "menu-view-all-#{path.parameterize}-#{corresponding_mode(mode)}"
+
+      tag.li class: li_css, id: li_id, data: { "direct-link": true } do
         safe_join([
-          link_to(path, class: link_css, role: role, data: { action: "keydown.enter->navigation#handleNavMenuClick keydown.tab->navigation#handleMenuTab" }) do
+          link_to(path, class: link_css, role: role, data: { action: "keydown.enter->navigation#handleNavMenuClick keydown.FIXME->navigation#handleMenuTab" }) do
             tag.span(title, class: "menu-title")
           end,
         ])
