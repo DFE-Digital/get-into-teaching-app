85cf69fbc4d892b2faf1802f6056c3f64ba9cb89
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-04-10 11:40:48 +0100

    Restore original options to personalised updates picklist excluding new option

--- app/models/events/steps/personalised_updates.rb
+++ app/models/events/steps/personalised_updates.rb
@@ -1,29 +1,15 @@
 module Events
   module Steps
     class PersonalisedUpdates < ::GITWizard::Step
-      GRADUATION_YEAR_DEPENDENT_OPTION_ID = 222_750_006
-
       attribute :degree_status_id, :integer
       attribute :consideration_journey_stage_id, :integer
       attribute :address_postcode
       attribute :preferred_teaching_subject_id
-      attribute :graduation_year, :integer
 
       validates :degree_status_id, inclusion: { in: :degree_status_option_ids }
       validates :consideration_journey_stage_id, inclusion: { in: :journey_stage_option_ids }
       validates :address_postcode, postcode: { allow_blank: true }
       validates :preferred_teaching_subject_id, inclusion: { in: :teaching_subject_option_ids }
-      validates :graduation_year,
-                format: {
-                  with: /\A\d{4}\z/,
-                  message: "Tell us which year you will graduate",
-                },
-                numericality: { only_integer: true },
-                presence: { message: "Tell us which year you will graduate" },
-                if: :requires_graduation_year?
-
-      validate :graduation_year_not_in_the_past, if: :requires_graduation_year?
-      validate :graduation_year_not_too_far_in_the_future, if: :requires_graduation_year?
 
       before_validation if: :address_postcode do
         self.address_postcode = address_postcode.to_s.strip.upcase.presence
@@ -44,7 +30,9 @@ module Events
       end
 
       def degree_status_options
-        @degree_status_options ||= PickListItemsApiPresenter.new.get_qualification_degree_status
+        @degree_status_options ||= GetIntoTeachingApiClient::PickListItemsApi.new
+          .get_qualification_degree_status
+          .reject { |item| item.id == 222_750_006 }
       end
 
       def degree_status_option_ids
@@ -71,22 +59,6 @@ module Events
 
       def postcode_in_crm
         @store.preexisting(:address_postcode)
-      end
-
-      def requires_graduation_year?
-        degree_status_id == GRADUATION_YEAR_DEPENDENT_OPTION_ID
-      end
-
-      def graduation_year_not_in_the_past
-        if graduation_year.present? && graduation_year < Time.current.year
-          errors.add(:graduation_year, "Enter the current year or a year in the future")
-        end
-      end
-
-      def graduation_year_not_too_far_in_the_future
-        if graduation_year.present? && graduation_year >= Time.current.year + 10
-          errors.add(:graduation_year, "Enter a valid graduation year")
-        end
       end
     end
   end
