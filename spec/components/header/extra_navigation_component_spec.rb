7970ae808e09fe5686d6701e50dc80051c5531d8
Martyn Whitwell <martyn.whitwell@gmail.com>
2025-04-16 11:32:42 +0100

    Remove search from front end (#4741)
    * Remove search
    
    * tweak order in footer
    
    * remove icon-search.svg

--- spec/components/header/extra_navigation_component_spec.rb
+++ spec/components/header/extra_navigation_component_spec.rb
@@ -4,20 +4,15 @@ describe Header::ExtraNavigationComponent, type: "component" do
   subject! { render_inline(component) }
 
   let(:custom_class) { "red-bg" }
-  let(:custom_custom_search_input_id) { "do-a-search" }
-  let(:component) { described_class.new(classes: Array.wrap(custom_class), search_input_id: custom_custom_search_input_id) }
+  let(:component) { described_class.new(classes: Array.wrap(custom_class)) }
 
   specify "renders the extra navigation container with contents" do
     expect(page).to have_css(".extra-navigation") do |en|
-      expect(en).to have_css("ul.extra-navigation__list.extra-navigation__flex > li", count: 3)
+      expect(en).to have_css("ul.extra-navigation__list.extra-navigation__flex > li", count: 2)
     end
   end
 
   specify "applies the custom classes" do
     expect(page).to have_css(".extra-navigation.#{custom_class}")
-  end
-
-  specify "generates a label for the accessible select box with the supplied id" do
-    expect(page).to have_css("label[for='#{custom_custom_search_input_id}']", text: "Search")
   end
 end
