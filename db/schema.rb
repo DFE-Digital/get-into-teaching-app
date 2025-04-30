a55af2044e5d355a0eb0faff47f9ea14eeec1130
emmaachesongray <168454683+emmaachesongray@users.noreply.github.com>
2025-04-30 09:36:00 +0000

    Add english case study to why teach section

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_18_000000) do
+ActiveRecord::Schema[7.2].define(version: 2024_01_18_000000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,5 +29,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_18_000000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
