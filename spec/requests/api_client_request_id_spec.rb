043daacf4e6ce8bca2cc317d0728e4aa1deef8c4
Martyn Whitwell <martyn.whitwell@gmail.com>
2025-05-20 10:43:41 +0100

    fix test

--- spec/requests/api_client_request_id_spec.rb
+++ spec/requests/api_client_request_id_spec.rb
@@ -1,10 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "API client request_id", type: :request do
+  let(:uuid) { "56b2d75b-2407-4131-bb5c-83e2d8ee0cf1" }
+
   it "passes the request_id to the API client" do
-    uuid = "56b2d75b-2407-4131-bb5c-83e2d8ee0cf1"
+    api_client_double = class_double(GetIntoTeachingApiClient::Current, request_id: uuid)
+                          .as_stubbed_const(transfer_nested_constants: true)
     allow_any_instance_of(ActionDispatch::Request).to receive(:uuid) { uuid }
-    expect_any_instance_of(GetIntoTeachingApiClient::Current).to receive(:request_id=).with(uuid).once #FIXME - this approach no longer works
+    expect(api_client_double).to receive(:request_id=).with(uuid).once
     get root_path
   end
 end
