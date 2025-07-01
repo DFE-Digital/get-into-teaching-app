0168b2a7069621d5a2445fd0dbd67878dbcb7e46
emmaachesongray <168454683+emmaachesongray@users.noreply.github.com>
2025-07-01 09:04:15 +0000

    Adviser funnel - degree grade questions

--- spec/features/teacher_training_adviser/sign_up_spec.rb
+++ spec/features/teacher_training_adviser/sign_up_spec.rb
@@ -697,7 +697,7 @@ RSpec.feature "Sign up for a teacher training adviser", type: :feature do
       fill_in "What subject is your degree?", with: "Mathematics"
       click_on "Next step"
 
-      expect(page).to have_css "h1", text: "What degree class are you predicted to get?"
+      expect(page).to have_css "h1", text: "What degree degree are you predicted to get?"
       select "2:2"
       click_on "Next step"
 
@@ -933,7 +933,7 @@ RSpec.feature "Sign up for a teacher training adviser", type: :feature do
       fill_in "What subject is your degree?", with: "Mathematics"
       click_on "Next step"
 
-      expect(page).to have_css "h1", text: "Which class is your degree?"
+      expect(page).to have_css "h1", text: "Which grade is your degree?"
       select "2:2"
       click_on "Next step"
 
