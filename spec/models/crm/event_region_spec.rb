d80c48208f74e4c655052ed002f063f521be96b8
Martyn Whitwell <martyn.whitwell@gmail.com>
2025-09-01 15:46:46 +0100

    Add missing regions to events (#4915)
    * Add missing regions to events
    
    * spec updates

--- spec/models/crm/event_region_spec.rb
+++ spec/models/crm/event_region_spec.rb
@@ -5,7 +5,7 @@ describe Crm::EventRegion do
     describe ".all_ids" do
       subject { described_class.all_ids }
 
-      it { is_expected.to eq((222_750_000..222_750_010).to_a) }
+      it { is_expected.to eq((222_750_000..222_750_013).to_a) }
     end
 
     describe ".lookup_by_id" do
@@ -14,7 +14,7 @@ describe Crm::EventRegion do
       end
 
       specify "errors when an unrecognised id is passed in" do
-        expect { described_class.lookup_by_id(222_750_011) }.to raise_error(KeyError)
+        expect { described_class.lookup_by_id(222_750_099) }.to raise_error(KeyError)
       end
     end
   end
