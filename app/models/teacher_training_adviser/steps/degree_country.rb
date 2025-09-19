e7a75d71d4120f61a16da724be87d7ee46617b0b
Martyn Whitwell <martyn.whitwell@gmail.com>
2025-09-19 16:25:00 +0100

    Update degree_country.rb

--- app/models/teacher_training_adviser/steps/degree_country.rb
+++ app/models/teacher_training_adviser/steps/degree_country.rb
@@ -3,12 +3,20 @@ module TeacherTrainingAdviser::Steps
     include ApiOptions
     UK = "72f5c2e6-74f9-e811-a97a-000d3a2760f2"
     ANOTHER_COUNTRY = "6f9e7b81-e44d-f011-877a-00224886d23e"
+    DEGREE = 222_750_000
+    DEGREE_EQUIVALENT = 222_750_005
 
     attribute :degree_country_id, :string
+    attribute :degree_type_id
 
     validates :degree_country_id, lookup_items: { method: :get_degree_countries }
 
     include FunnelTitle
+
+    def degree_country_id=(value)
+      super
+      set_degree_type
+    end
 
     def options
       generate_api_options(GetIntoTeachingApiClient::LookupItemsApi, :get_degree_countries, [], [UK, ANOTHER_COUNTRY])
@@ -26,6 +34,10 @@ module TeacherTrainingAdviser::Steps
 
     def another_country?
       degree_country_id == ANOTHER_COUNTRY
+    end
+
+    def set_degree_type
+      self.degree_type_id = another_country? ? DEGREE_EQUIVALENT : DEGREE
     end
   end
 end
