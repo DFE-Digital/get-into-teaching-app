bc7278043a15a6a7fc9b537e91694353db70298a
Martyn Whitwell <martyn.whitwell@gmail.com>
2025-09-26 18:06:35 +0100

    Update degree class page to radio buttons

--- app/models/teacher_training_adviser/steps/what_degree_class.rb
+++ app/models/teacher_training_adviser/steps/what_degree_class.rb
@@ -1,18 +1,12 @@
 module TeacherTrainingAdviser::Steps
   class WhatDegreeClass < GITWizard::Step
     extend ApiOptions
-
-    OMIT_GRADE_IDS = [
-      222_750_004, # Third class or below
-      222_750_005, # Unknown
-    ].freeze
-
     attribute :uk_degree_grade_id, :integer
 
     include FunnelTitle
 
     def self.options
-      generate_api_options(GetIntoTeachingApiClient::PickListItemsApi, :get_qualification_uk_degree_grades, OMIT_GRADE_IDS)
+      PickListItemsApiPresenter.new.get_qualification_uk_degree_grades
     end
 
     validates :uk_degree_grade_id, pick_list_items: { method: :get_qualification_uk_degree_grades }
