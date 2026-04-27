aaf74187cc1450c51156e9a3cfcf16f51ea4c30f
Martyn Whitwell <martyn.whitwell@gmail.com>
2026-04-27 09:10:11 +0100

    wip

--- app/components/calls_to_action/adviser_block_component.rb
+++ app/components/calls_to_action/adviser_block_component.rb
@@ -1,11 +1,12 @@
 module CallsToAction
   class AdviserBlockComponent < ViewComponent::Base
-    attr_reader :title, :image, :title_color, :text, :link_text, :link_target
+    attr_reader :title, :image, :image_background, :title_color, :text, :link_text, :link_target
 
     def initialize(
       title: "Get a free adviser",
-      image: "adviser-block-promo.jpg",
-      title_color: "yellow",
+      image: "adviser-black.png",
+      title_color: "gitbluepastel",
+      image_background: "gitbluetint",
       text: "An adviser with years of teaching experience can help you become a teacher.",
       link_text: "Find out about advisers",
       link_target: "/teacher-training-advisers"
@@ -13,6 +14,7 @@ module CallsToAction
       super
       @title = title
       @image = image
+      @image_background = image_background
       @title_color = title_color
       @text = text
       @link_text = link_text
