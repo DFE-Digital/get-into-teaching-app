010d4cc3df64f7aa7c723738634d994ca6b77344
Martyn Whitwell <martyn.whitwell@gmail.com>
2026-06-25 15:13:43 +0100

    add specs

--- spec/features/provider_events/register_new_provider_event_spec.rb
+++ spec/features/provider_events/register_new_provider_event_spec.rb
@@ -3,12 +3,10 @@ require "rails_helper"
 RSpec.feature "Register a new provider event", type: :feature do
   include_context "with wizard data"
 
-
-  around do |example|
-    travel_to(Date.new(2026, 6, 24)) do
-      example.run
+  describe "Start page" do
+    it "has a link to the email step" do
+      visit provider_events_steps_path
+      expect(page).to have_link(href: provider_events_step_path(ProviderEvents::Steps::Email.key))
     end
   end
-
-
 end
