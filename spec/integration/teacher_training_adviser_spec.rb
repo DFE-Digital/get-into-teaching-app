425b34b1f0d6d0d7d541820e53f9c57ef6fbf769
Martyn Whitwell <martyn.whitwell@gmail.com>
2026-09-15 11:06:33 +0100

    Use 'Another country' label in integration test (#5212)
    Update the teacher training adviser integration spec to use the correct label casing for the degree country option ("Another country"). This aligns the test with the application's actual option text and prevents selector/lookup mismatches in the sign-up flow.

--- spec/integration/teacher_training_adviser_spec.rb
+++ spec/integration/teacher_training_adviser_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe "Teacher Training Adviser integration tests", :integration, :mech
   def complete_sign_up
     submit_label_step("No", :returning_teacher)
     submit_label_step("Yes", :degree_status)
-    submit_label_step("Another Country", :degree_country)
+    submit_label_step("Another country", :degree_country)
     submit_date_of_birth_step(Date.new(1974, 3, 16))
     submit_label_step("In the UK", :location)
     submit_uk_callback_step("123456789")
